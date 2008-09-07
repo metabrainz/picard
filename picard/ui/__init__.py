a371d2c277b76de7e31d70fb83cc6114bbf58d11
Lukáš Lalinský <lalinsky@gmail.com>
2008-09-07 15:07:27 +0200

    Get rid of FakeQDialogButtonBox

--- picard/ui/__init__.py
+++ picard/ui/__init__.py
@@ -16,28 +16,3 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-
-from PyQt4 import QtCore, QtGui
-
-# for people who really can't install Qt 4.2 :(
-if not hasattr(QtGui, 'QDialogButtonBox'):
-    class FakeQDialogButtonBox(QtGui.QWidget):
-        AcceptRole = 0
-        RejectRole = 1
-        HelpRole = 4
-        def __init__(self, parent=None):
-            QtGui.QWidget.__init__(self, parent)
-            self.hbox = QtGui.QHBoxLayout(self)
-            self.hbox.setMargin(0)
-            self.hbox.addStretch()
-        def setOrientation(self, orientation):
-            pass
-        def addButton(self, button, role):
-            self.hbox.addWidget(button)
-            if role == self.AcceptRole:
-                self.connect(button, QtCore.SIGNAL('clicked()'), self, QtCore.SIGNAL('accepted()'))
-            elif role == self.RejectRole:
-                self.connect(button, QtCore.SIGNAL('clicked()'), self, QtCore.SIGNAL('rejected()'))
-            elif role == self.HelpRole:
-                self.connect(button, QtCore.SIGNAL('clicked()'), self, QtCore.SIGNAL('helpRequested()'))
-    QtGui.QDialogButtonBox = FakeQDialogButtonBox
