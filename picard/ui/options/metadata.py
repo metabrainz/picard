f70a50fd6a0ca2ad4dbccdae97c2139591cb7048
Philipp Wolfer <ph.wolfer@googlemail.com>
2009-01-24 20:27:06 +0100

    Fix translation of the release country list in the UI

--- picard/ui/options/metadata.py
+++ picard/ui/options/metadata.py
@@ -49,9 +49,9 @@ class MetadataOptionsPage(OptionsPage):
         self.ui.setupUi(self)
         self.connect(self.ui.va_name_default, QtCore.SIGNAL("clicked()"), self.set_va_name_default)
         self.connect(self.ui.nat_name_default, QtCore.SIGNAL("clicked()"), self.set_nat_name_default)
-        self.ui.preferred_release_country.addItem(N_("None"), QtCore.QVariant(""))
+        self.ui.preferred_release_country.addItem(_("None"), QtCore.QVariant(""))
         for country, name in sorted(RELEASE_COUNTRIES.items(), key=operator.itemgetter(1)):
-            self.ui.preferred_release_country.addItem(name, QtCore.QVariant(country))
+            self.ui.preferred_release_country.addItem(_(name), QtCore.QVariant(country))
 
     def load(self):
         self.ui.translate_artist_names.setChecked(self.config.setting["translate_artist_names"])
