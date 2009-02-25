962f1a14b6ee6d7b4f47ab283bcaa97b8f578f19
Philipp Wolfer <ph.wolfer@googlemail.com>
2009-02-25 01:05:37 +0100

    Plugin to get the earliest release date for an album

---
+++ contrib/plugins/originalreleasedate.py
@@ -0,0 +1,58 @@
+""" 
+Use the earliest release to set the original release date.
+"""
+
+PLUGIN_NAME = 'Original Release Date'
+PLUGIN_AUTHOR = 'Philipp Wolfer'
+PLUGIN_DESCRIPTION = '''Set the original release date of a release by using the release's release events and earliest release advanced relationships.'''
+PLUGIN_VERSION = '0.1'
+PLUGIN_API_VERSIONS = ['0.10']
+
+from picard.metadata import register_album_metadata_processor
+from picard.album import Album
+from picard.util import partial
+from picard.mbxml import release_to_metadata
+from PyQt4.QtCore import QUrl
+
+def _earliest_release_downloaded(album, metadata, original_id, document, http, error):
+    try:
+        if error:
+            self.log.error("%r", unicode(http.errorString()))
+        else:
+            try:
+                release_node = document.metadata[0].release[0]
+                original_album = Album(original_id)
+                release_to_metadata(release_node, original_album.metadata,
+                                    config=album.config, album=original_album)
+                get_earliest_release_date(original_album, metadata)
+                for track in album._new_tracks:
+                    track.metadata["originaldate"] = metadata["originaldate"]
+            except:
+                error = True
+                self.log.error(traceback.format_exc())
+    finally:
+        album._requests -= 1
+        album._finalize_loading(None)
+
+def original_release_date(album, metadata, release_node):
+    # First find the earliest release from the release event list
+    get_earliest_release_date(album, metadata)
+    
+    # Check for earliest release ARs and load those
+    for relation_list in release_node.relation_list:
+        if relation_list.target_type == 'Release':
+            for relation in relation_list.relation:
+                if relation.type == 'FirstAlbumRelease' and relation.direction == 'backward':
+                    album._requests += 1
+                    album.tagger.xmlws.get_release_by_id(relation.target,
+                        partial(_earliest_release_downloaded, album, metadata, relation.target),
+                        ['release-events'])
+
+def get_earliest_release_date(album, metadata):
+    earliest_date = metadata["originaldate"]
+    for event in album.release_events:
+        if not earliest_date or event.date < earliest_date:
+            earliest_date = event.date
+    metadata["originaldate"] = earliest_date
+
+register_album_metadata_processor(original_release_date)
