375deb050282400e140646f247ccd2dee9f4a046
Philipp Wolfer <ph.wolfer@googlemail.com>
2009-03-24 01:01:55 +0100

    Added missing import

--- picard/util/mimetype.py
+++ picard/util/mimetype.py
@@ -17,6 +17,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+import os
+
 MIME_TYPE_EXTENSION_MAP = {
     'image/jpeg': '.jpg',
     'image/png': '.png',
