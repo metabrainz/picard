b6b37f3884e27ffea8a564a4a4431f7df6d7f183
Lukáš Lalinský <lalinsky@gmail.com>
2011-01-07 18:22:59 +0100

    *poof*
    This code was rewritten in my Qt port.

--- picard/browser/filelookup.py
+++ picard/browser/filelookup.py
@@ -1,56 +1,22 @@
-# ***** BEGIN LICENSE BLOCK *****
-# Version: RCSL 1.0/RPSL 1.0/GPL 2.0
+# -*- coding: utf-8 -*-
 #
-# Portions Copyright (c) 1995-2002 RealNetworks, Inc. All Rights Reserved.
-# Portions Copyright (c) 2004 Robert Kaye. All Rights Reserved.
+# Picard, the next-generation MusicBrainz tagger
+# Copyright (c) 2004 Robert Kaye
+# Copyright (C) 2006 Lukáš Lalinský
 #
-# The contents of this file, and the files included with this file, are
-# subject to the current version of the RealNetworks Public Source License
-# Version 1.0 (the "RPSL") available at
-# http://www.helixcommunity.org/content/rpsl unless you have licensed
-# the file under the RealNetworks Community Source License Version 1.0
-# (the "RCSL") available at http://www.helixcommunity.org/content/rcsl,
-# in which case the RCSL will apply. You may also obtain the license terms
-# directly from RealNetworks.  You may not use this file except in
-# compliance with the RPSL or, if you have a valid RCSL with RealNetworks
-# applicable to this file, the RCSL.  Please see the applicable RPSL or
-# RCSL for the rights, obligations and limitations governing use of the
-# contents of the file.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
 #
-# This file is part of the Helix DNA Technology. RealNetworks is the
-# developer of the Original Code and owns the copyrights in the portions
-# it created.
-#
-# This file, and the files included with this file, is distributed and made
-# available on an 'AS IS' basis, WITHOUT WARRANTY OF ANY KIND, EITHER
-# EXPRESS OR IMPLIED, AND REALNETWORKS HEREBY DISCLAIMS ALL SUCH WARRANTIES,
-# INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS
-# FOR A PARTICULAR PURPOSE, QUIET ENJOYMENT OR NON-INFRINGEMENT.
-#
-# Technology Compatibility Kit Test Suite(s) Location:
-#    http://www.helixcommunity.org/content/tck
-#
-# --------------------------------------------------------------------
-#
-# picard is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# picard is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with picard; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-# Contributor(s):
-#   Robert Kaye
-#
-#
-# ***** END LICENSE BLOCK *****
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 from PyQt4 import QtCore
 import os.path
