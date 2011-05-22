50e2c1de735e4b7b7992b270ad6b6a61fffd9343
Michael Wiencek <mwtuea@gmail.com>
2011-05-22 12:20:57 -0500

    Remove score column from cdlookup dialog entirely.

--- picard/ui/cdlookup.py
+++ picard/ui/cdlookup.py
@@ -29,17 +29,16 @@ class CDLookupDialog(QtGui.QDialog):
         self.disc = disc
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
-        self.ui.release_list.setHeaderLabels([_(u"Score"), _(u"Album"), _(u"Artist")])
-        self.ui.release_list.header().resizeSection(0, 40)
+        self.ui.release_list.setHeaderLabels([_(u"Album"), _(u"Artist")])
         if self.releases:
             for release in self.releases:
                 item = QtGui.QTreeWidgetItem(self.ui.release_list)
-                #item.setText(0, release.score)
-                item.setText(1, release.title[0].text)
-                item.setText(2, artist_credit_from_node(release.artist_credit[0])[0])
+                item.setText(0, release.title[0].text)
+                item.setText(1, artist_credit_from_node(release.artist_credit[0])[0])
                 item.setData(0, QtCore.Qt.UserRole, QtCore.QVariant(release.id))
             self.ui.release_list.setCurrentItem(self.ui.release_list.topLevelItem(0))
             self.ui.ok_button.setEnabled(True)
+        self.ui.release_list.resizeColumnToContents(0)
         self.connect(self.ui.lookup_button, QtCore.SIGNAL("clicked()"), self.lookup)
 
     def accept(self):
