344a8943f61b0046939d60ec43141cadb2043496
Lukáš Lalinský <lalinsky@gmail.com>
2011-09-16 18:40:04 +0200

    Add a 'Get API key' button in AcoustID's settings

--- picard/ui/ui_options_fingerprinting.py
+++ picard/ui/ui_options_fingerprinting.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'ui/options_fingerprinting.ui'
 #
-# Created: Thu Sep 15 13:48:39 2011
+# Created: Fri Sep 16 18:35:59 2011
 #      by: PyQt4 UI code generator 4.8.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -33,23 +33,32 @@ class Ui_FingerprintingOptionsPage(object):
         self.verticalLayout.addWidget(self.groupBox)
         self.acoustid_settings = QtGui.QGroupBox(FingerprintingOptionsPage)
         self.acoustid_settings.setObjectName(_fromUtf8("acoustid_settings"))
-        self.gridLayout_2 = QtGui.QGridLayout(self.acoustid_settings)
-        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.acoustid_settings)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
         self.label = QtGui.QLabel(self.acoustid_settings)
         self.label.setObjectName(_fromUtf8("label"))
-        self.gridLayout_2.addWidget(self.label, 0, 0, 1, 2)
-        self.acoustid_fpcalc_browse = QtGui.QPushButton(self.acoustid_settings)
-        self.acoustid_fpcalc_browse.setObjectName(_fromUtf8("acoustid_fpcalc_browse"))
-        self.gridLayout_2.addWidget(self.acoustid_fpcalc_browse, 1, 1, 1, 1)
-        self.label_2 = QtGui.QLabel(self.acoustid_settings)
-        self.label_2.setObjectName(_fromUtf8("label_2"))
-        self.gridLayout_2.addWidget(self.label_2, 2, 0, 1, 2)
-        self.acoustid_apikey = QtGui.QLineEdit(self.acoustid_settings)
-        self.acoustid_apikey.setObjectName(_fromUtf8("acoustid_apikey"))
-        self.gridLayout_2.addWidget(self.acoustid_apikey, 3, 0, 1, 2)
+        self.verticalLayout_2.addWidget(self.label)
+        self.horizontalLayout_2 = QtGui.QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
         self.acoustid_fpcalc = QtGui.QLineEdit(self.acoustid_settings)
         self.acoustid_fpcalc.setObjectName(_fromUtf8("acoustid_fpcalc"))
-        self.gridLayout_2.addWidget(self.acoustid_fpcalc, 1, 0, 1, 1)
+        self.horizontalLayout_2.addWidget(self.acoustid_fpcalc)
+        self.acoustid_fpcalc_browse = QtGui.QPushButton(self.acoustid_settings)
+        self.acoustid_fpcalc_browse.setObjectName(_fromUtf8("acoustid_fpcalc_browse"))
+        self.horizontalLayout_2.addWidget(self.acoustid_fpcalc_browse)
+        self.verticalLayout_2.addLayout(self.horizontalLayout_2)
+        self.label_2 = QtGui.QLabel(self.acoustid_settings)
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.verticalLayout_2.addWidget(self.label_2)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.acoustid_apikey = QtGui.QLineEdit(self.acoustid_settings)
+        self.acoustid_apikey.setObjectName(_fromUtf8("acoustid_apikey"))
+        self.horizontalLayout.addWidget(self.acoustid_apikey)
+        self.acoustid_apikey_get = QtGui.QPushButton(self.acoustid_settings)
+        self.acoustid_apikey_get.setObjectName(_fromUtf8("acoustid_apikey_get"))
+        self.horizontalLayout.addWidget(self.acoustid_apikey_get)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
         self.verticalLayout.addWidget(self.acoustid_settings)
         spacerItem = QtGui.QSpacerItem(181, 21, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
         self.verticalLayout.addItem(spacerItem)
@@ -65,4 +74,5 @@ class Ui_FingerprintingOptionsPage(object):
         self.label.setText(_("Fingerprinter:"))
         self.acoustid_fpcalc_browse.setText(_("Browse..."))
         self.label_2.setText(_("API key:"))
+        self.acoustid_apikey_get.setText(_("Get API key..."))
 
