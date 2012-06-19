e565fdbee77f233627d2f150d07cdc6aa15655e8
Frederik "Freso" S. Olesen <freso.dk@gmail.com>
2012-06-19 15:36:51 +0200

    Added license declaration to the sql2py script.

--- scripts/country_sql2py.py
+++ scripts/country_sql2py.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Picard, the next-generation MusicBrainz tagger
+# Copyright (C) 2006 Lukáš Lalinský
+# Copyright (C) 2012 Frederik "Freso" S. Olesen
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 def get_countries(dbuser="musicbrainz", dbname="musicbrainz_db_slave"):
     from subprocess import Popen, PIPE
