metabrainz/picard

commit fc7470eee076c06d925598d3f29e7eca7cb089c6

Make it possible to pass a filename when adding an image

picard/file.py
@@ -197,7 +197,7 @@ class File(LockableObject, Item):
                 pass
         # Save cover art images
         if settings["save_images_to_files"]:
-            self._save_images(new_filename, metadata, settings)
+            self._save_images(os.path.dirname(new_filename), metadata, settings)
         return new_filename
 
     @staticmethod
@@ -312,28 +312,33 @@ class File(LockableObject, Item):
         else:
             return old_filename
 
-    def _save_images(self, filename, metadata, settings):
-        """Save the cover images to disk."""
-        if not metadata.images:
-            return
-        overwrite = settings["save_images_overwrite"]
-        image_filename = self._script_to_filename(
-            settings["cover_image_filename"], metadata, settings)
+    def _make_image_filename(self, image_filename, dirname, metadata, settings):
+        image_filename = self._script_to_filename(image_filename, metadata, settings)
         if not image_filename:
             image_filename = "cover"
         if os.path.isabs(image_filename):
             filename = image_filename
         else:
-            filename = os.path.join(os.path.dirname(filename), image_filename)
+            filename = os.path.join(dirname, image_filename)
         if settings['windows_compatible_filenames'] or sys.platform == 'win32':
             filename = filename.replace('./', '_/').replace('.\\', '_\\')
-        filename = encode_filename(filename)
+        return encode_filename(filename)
+
+    def _save_images(self, dirname, metadata, settings):
+        """Save the cover images to disk."""
+        if not metadata.images:
+            return
+        default_filename = self._make_image_filename(
+            settings["cover_image_filename"], dirname, metadata, settings)
+        overwrite = settings["save_images_overwrite"]
         i = 0
-        for mime, data in metadata.images:
+        for mime, data, filename in metadata.images:
+            if filename is None:
+                filename = default_filename
+            else:
+                filename = self._make_image_filename(filename, dirname, metadata, settings)
             image_filename = filename
             ext = mimetype.get_extension(mime, ".jpg")
-            if i > 0:
-                image_filename = "%s (%d)" % (filename, i)
             i += 1
             while os.path.exists(image_filename + ext) and not overwrite:
                 if os.path.getsize(image_filename + ext) == len(data):
@@ -343,6 +348,9 @@ class File(LockableObject, Item):
                 i += 1
             else:
                 self.log.debug("Saving cover images to %r", image_filename)
+                new_dirname = os.path.dirname(image_filename)
+                if not os.path.isdir(new_dirname):
+                    os.makedirs(new_dirname)
                 f = open(image_filename + ext, "wb")
                 f.write(data)
                 f.close()

picard/formats/apev2.py
@@ -141,7 +141,7 @@ class APEv2File(File):
         for name, values in temp.items():
             tags[str(name)] = values
         if settings['save_images_to_tags']:
-            for mime, data in metadata.images:
+            for mime, data, _ in metadata.images:
                 cover_filename = 'Cover Art (Front)'
                 cover_filename += mimetype.get_extension(mime, '.jpg')
                 tags['Cover Art (Front)'] = cover_filename + '\0' + data

picard/formats/asf.py
@@ -26,7 +26,7 @@ import struct
 def unpack_image(data):
     """
     Helper function to unpack image data from a WM/Picture tag.
-    
+
     The data has the following format:
     1 byte: Picture type (0-20), see ID3 APIC frame specification at http://www.id3.org/id3v2.4.0-frames
     4 bytes: Picture data length in LE format
@@ -152,11 +152,11 @@ class ASFFile(File):
             file.tags.clear()
         if settings['save_images_to_tags']:
             cover = []
-            for mime, data in metadata.images:
+            for mime, data, _ in metadata.images:
                 tag_data = pack_image(mime, data, 3)
                 cover.append(ASFByteArrayAttribute(tag_data))
             if cover:
-                file.tags['WM/Picture'] = cover  
+                file.tags['WM/Picture'] = cover
 
         for name, values in metadata.rawitems():
             if name.startswith('lyrics:'):

picard/formats/id3.py
@@ -247,7 +247,7 @@ class ID3File(File):
             tags.add(id3.TPOS(encoding=0, text=text))
 
         if settings['save_images_to_tags']:
-            for mime, data in metadata.images:
+            for mime, data, _ in metadata.images:
                 tags.add(id3.APIC(encoding=0, mime=mime, type=3, desc='', data=data))
 
         tmcl = mutagen.id3.TMCL(encoding=encoding, people=[])

picard/formats/mp4.py
@@ -186,7 +186,7 @@ class MP4File(File):
 
         if settings['save_images_to_tags']:
             covr = []
-            for mime, data in metadata.images:
+            for mime, data, _ in metadata.images:
                 if mime == "image/jpeg":
                     covr.append(MP4Cover(data, MP4Cover.FORMAT_JPEG))
                 elif mime == "image/png":

picard/formats/vorbis.py
@@ -139,7 +139,7 @@ class VCommentFile(File):
             tags.setdefault(u"DISCTOTAL", []).append(metadata["totaldiscs"])
 
         if settings['save_images_to_tags']:
-            for mime, data in metadata.images:
+            for mime, data, _ in metadata.images:
                 image = mutagen.flac.Picture()
                 image.type = 3 # Cover image
                 image.data = data

picard/metadata.py
@@ -42,8 +42,8 @@ class Metadata(object):
         self.images = []
         self.length = 0
 
-    def add_image(self, mime, data):
-        self.images.append((mime, data))
+    def add_image(self, mime, data, filename=None):
+        self.images.append((mime, data, filename))
 
     def __repr__(self):
         return repr(self._items)

picard/ui/infodialog.py
@@ -70,7 +70,7 @@ class InfoDialog(QtGui.QDialog):
         text = '<br/>'.join(map(lambda i: '<b>%s</b><br/>%s' % i, info))
         self.ui.info.setText(text)
 
-        for mime, data in file.metadata.images:
+        for mime, data, _ in file.metadata.images:
             item = QtGui.QListWidgetItem()
             pixmap = QtGui.QPixmap()
             pixmap.loadFromData(data)

commit 1e8a412edf056dd7cf12606cae8492dba7d7b5cd

Prevent cover images from skipping numbers
This is a bug introduced with fc7470eee076c06d925598d3f29e7eca7cb089c6:
When saving multiple images with the generic filename, only odd numbers
would be appended to the filename, because the "while exists(foo)" check
is always true for every image except the first one at least once,
causing "i" to be incremented twice for every image (except for the
first one).

picard/file.py
@@ -339,6 +339,8 @@ class File(LockableObject, Item):
                 filename = self._make_image_filename(filename, dirname, metadata, settings)
             image_filename = filename
             ext = mimetype.get_extension(mime, ".jpg")
+            if i > 0:
+                image_filename = "%s (%d)" % (filename, i)
             i += 1
             while os.path.exists(image_filename + ext) and not overwrite:
                 if os.path.getsize(image_filename + ext) == len(data):