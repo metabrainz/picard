metabrainz/picard

commit e16b2533c27b738b250e81f7aad003934b6318d5

Fix "vocal vocals" bug in performer: tag

picard/mbxml.py
@@ -77,11 +77,7 @@ def _relations_to_metadata(relation_lists, m, config):
                 attribs = []
                 if 'attribute_list' in relation.children:
                     attribs = [a.text for a in relation.attribute_list[0].attribute]
-                if reltype == 'vocal':
-                    name = 'performer:' + ' '.join([_parse_attributes(attribs), 'vocal']).strip()
-                elif reltype == 'instrument':
-                    name = 'performer:' + _parse_attributes(attribs)
-                elif reltype == 'performer':
+                if reltype in ('vocal', 'instrument', 'performer'):
                     name = 'performer:' + _parse_attributes(attribs)
                 elif reltype == 'mix-DJ' and len(attribs) > 0:
                     if not hasattr(m, "_djmix_ars"):

commit 513b51d73025c8b0347c8fe55e81a8ba53f5f12f

Fix performed vocals attribute with no vocal type set
This fixes a regression since e16b2533c where picard no saves vocal ARs
with no type set as generic "performer" ars; appearing in the UI like
    Performer []: Some Vocalist
    Performer [guest]: Some Guest Vocalist

Add special handling for the vocal relationship type to use a fallback
string if there is no vocal type set. The fallback string is "vocals"
(earlier versions of picard used "vocal"), for consistancy with the
current vocal type strings, which look like "lead vocals", "background
vocals", etc.

picard/mbxml.py
@@ -55,7 +55,8 @@ def _decamelcase(text):
 
 _REPLACE_MAP = {}
 _EXTRA_ATTRS = ['guest', 'additional', 'minor']
-def _parse_attributes(attrs):
+_BLANK_SPECIAL_RELTYPES = {'vocal': 'vocals'}
+def _parse_attributes(attrs, reltype):
     attrs = [_decamelcase(_REPLACE_MAP.get(a, a)) for a in attrs]
     prefix = ' '.join([a for a in attrs if a in _EXTRA_ATTRS])
     attrs = [a for a in attrs if a not in _EXTRA_ATTRS]
@@ -64,7 +65,7 @@ def _parse_attributes(attrs):
     elif len(attrs) == 1:
         attrs = attrs[0]
     else:
-        attrs = ''
+        attrs = _BLANK_SPECIAL_RELTYPES.get(reltype, '')
     return ' '.join([prefix, attrs]).strip().lower()
 
 
@@ -79,7 +80,7 @@ def _relations_to_metadata(relation_lists, m):
                 if 'attribute_list' in relation.children:
                     attribs = [a.text for a in relation.attribute_list[0].attribute]
                 if reltype in ('vocal', 'instrument', 'performer'):
-                    name = 'performer:' + _parse_attributes(attribs)
+                    name = 'performer:' + _parse_attributes(attribs, reltype)
                 elif reltype == 'mix-DJ' and len(attribs) > 0:
                     if not hasattr(m, "_djmix_ars"):
                         m._djmix_ars = {}