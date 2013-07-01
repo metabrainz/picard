329ed8cedb64be6f43cfd9a0ae964e292b1ec695
Laurent Monin <zas@norz.org>
2013-07-01 13:34:53 +0200

    Deprecated failUnlessAlmostEqual() -> assertAlmostEqual()
    http://docs.python.org/2/library/unittest.html#deprecated-aliases

--- test/test_similarity.py
+++ test/test_similarity.py
@@ -6,5 +6,5 @@ class SimilarityTest(unittest.TestCase):
     def test_correct(self):
         self.assertEqual(similarity(u"K!", u"K!"), 1.0)
         self.assertEqual(similarity(u"BBB", u"AAA"), 0.0)
-        self.failUnlessAlmostEqual(similarity(u"ABC", u"ABB"), 0.7, 1)
+        self.assertAlmostEqual(similarity(u"ABC", u"ABB"), 0.7, 1)
 
