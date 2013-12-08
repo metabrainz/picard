8896ea62415e71eefab03e8742ca02ef5abf06d9
Frederik "Freso" S. Olesen <freso.dk@gmail.com>
2013-12-08 14:50:38 +0100

    Support .m4v files.
    http://chatlogs.musicbrainz.org/musicbrainz/2013/2013-12/2013-12-08.html#T12-42-40-561267

--- picard/formats/mp4.py
+++ picard/formats/mp4.py
@@ -25,7 +25,7 @@ from picard.util import encode_filename
 
 
 class MP4File(File):
-    EXTENSIONS = [".m4a", ".m4b", ".m4p", ".mp4"]
+    EXTENSIONS = [".m4a", ".m4b", ".m4p", ".m4v", ".mp4"]
     NAME = "MPEG-4 Audio"
 
     __text_tags = {
