30b166fba11870fff4eaa75651586e8dc79e7a90
Laurent Monin <zas@norz.org>
2013-12-27 21:57:29 +0100

    Fix missing QtCore import

--- picard/ui/util.py
+++ picard/ui/util.py
@@ -18,7 +18,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 import sys
-from PyQt4 import QtGui
+from PyQt4 import QtCore, QtGui
 from picard import config
 from picard.util import find_existing_path
 
