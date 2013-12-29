metabrainz/picard

commit 9092a7abcae5254e581497b346b2167a5e37cb26

Fix lookup encoding for non latin characters

picard/webservice.py
@@ -280,12 +280,12 @@ class XmlWebService(QtNetwork.QHttp):
             else:
                 value = _escape_lucene_query(value).strip().lower()
                 if value:
-                    query.append('%s:(%s)' % (name, value.encode('utf-8')))
+                    query.append('%s:(%s)' % (name, value))
         if query:
             filters.append(('query', ' '.join(query)))
         params = []
         for name, value in filters:
-            value = str(QtCore.QUrl.toPercentEncoding(str(value)))
+            value = str(QtCore.QUrl.toPercentEncoding(QtCore.QString(value)))
             params.append('%s=%s' % (str(name), value))
         path = "/ws/1/%s/?type=xml&%s" % (entitytype, "&".join(params))
         self.get(host, port, path, handler)

commit f79f981ac4d862e6897f73d8dfcda6920b235312

Fix encoding of limit parameter value in webservice query (limit=%19)
This is a very old bug it seems, it was indirectly introduced by
commit 9092a7abcae5254e581497b346b2167a5e37cb26

The missing str() was causing integer value 25 to be encoded as
ascii char 25, then, after percent encoding, limit=%19 in the search url.

picard/webservice.py
@@ -392,7 +392,7 @@ class XmlWebService(QtCore.QObject):
         query = []
         for name, value in kwargs.items():
             if name == 'limit':
-                filters.append((name, value))
+                filters.append((name, str(value)))
             else:
                 value = _escape_lucene_query(value).strip().lower()
                 if value: