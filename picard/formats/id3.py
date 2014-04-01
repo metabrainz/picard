5a84fcd4d1d8bc7ad59cdd29165ea19d291e0b3a
Sophist <github@sodalis.co.uk>
2014-04-01 18:08:37 +0100

    Drop id3v24 tagging timestamp.

--- picard/formats/id3.py
+++ picard/formats/id3.py
@@ -413,8 +413,6 @@ class ID3File(File):
         if tipl.people:
             tags.add(tipl)
 
-        tags.add(id3.TDTG(encoding=encoding, text=[id3.ID3TimeStamp(text=time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime()))]))
-
         if config.setting['write_id3v23']:
             tags.update_to_v23(join_with=config.setting['id3v23_join_with'])
             tags.save(encode_filename(filename), v2=3, v1=v1)
