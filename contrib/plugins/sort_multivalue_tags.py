6f5be01b8b7ea29b301e0d48094467fdeaed7fb6
Sophist <github@sodalis.co.uk>
2014-04-03 07:37:15 +0100

    Fix sort multivalue tags plugin
    Exclude certain multi-value tags from sorting because the order is
    important.
    
    Get raw list because id3metadata sub-class overrides the getall method.
    
    Make a class to avoid polluting the global namespace with the
    exclude_tags variable.

--- contrib/plugins/sort_multivalue_tags.py
+++ contrib/plugins/sort_multivalue_tags.py
@@ -15,22 +15,31 @@
 
 PLUGIN_NAME = u"Sort Multi-Value Tags"
 PLUGIN_AUTHOR = u"Sophist"
-PLUGIN_DESCRIPTION = u'Sort Multi-Value Tags e.g. Release Type, Lyrics alphabetically.'
-PLUGIN_VERSION = "0.1"
-PLUGIN_API_VERSIONS = ["0.15"]
+PLUGIN_DESCRIPTION = u'Sort Multi-Value Tags e.g. Performers alphabetically.'
+PLUGIN_VERSION = "0.2"
+PLUGIN_API_VERSIONS = ["0.15.0", "0.15.1", "0.16.0", "1.0.0", "1.1.0", "1.2.0", "1.3.0"]
 
 from picard.metadata import register_track_metadata_processor
 
-# Define and register the Track Metadata function
+class SortMultiValueTags:
 
+    # Exclude the following tags because the sort order is related to other tags or has a meaning like primary artist
+    exclude_tags = [
+        'artist', 'artistsort', 'musicbrainz_artistid',
+        'albumartist', 'albumartistsort', 'musicbrainz_albumartistid',
+        'releasetype',
+        ]
 
-def sort_multivalue_tags(tagger, metadata, track, release):
+    # Define and register the Track Metadata function
+    def sort_multivalue_tags(self, tagger, metadata, track, release):
 
-    for tag in metadata.keys():
-        data = metadata.getall(tag)
-        if len(data) > 1:
-            sorted_data = sorted(data)
-            if data != sorted_data:
-                metadata.set(tag, sorted_data)
+        for tag in metadata.keys():
+            if tag in SortMultiValueTags.exclude_tags:
+                continue
+            data = dict.get(metadata, tag)
+            if len(data) > 1:
+                sorted_data = sorted(data)
+                if data != sorted_data:
+                    metadata.set(tag,sorted_data)
 
-register_track_metadata_processor(sort_multivalue_tags)
+register_track_metadata_processor(SortMultiValueTags().sort_multivalue_tags)
