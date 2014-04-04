c22f93da50e738165a42c76ea2032d8907ae787b
Sophist <github@sodalis.co.uk>
2014-04-04 13:28:20 +0100

    Drop import time which is never used.

--- picard/formats/id3.py
+++ picard/formats/id3.py
@@ -17,7 +17,6 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-import time
 import mutagen.apev2
 import mutagen.mp3
 import mutagen.trueaudio
