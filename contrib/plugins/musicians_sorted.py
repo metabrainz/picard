ad62489f607dfce18a055ee5f64421c0fb8da156
Sophist <github@sodalis.co.uk>
2014-04-11 12:34:37 +0100

    Make plugin non-class

--- contrib/plugins/musicians_sorted.py
+++ contrib/plugins/musicians_sorted.py
@@ -9,28 +9,31 @@ Sort Multi-Value Tags plugin can be used with or without this plugin.
 If used without this plugin, then musicians tags are sorted by forename.
 If used with this plugin, then musicians tags are sorted by surname.'''
 PLUGIN_VERSION = '0.1'
-PLUGIN_API_VERSIONS = ["0.15.0", "0.15.1", "0.16.0", "1.0.0", "1.1.0", "1.2.0", "1.3.0"]
+PLUGIN_API_VERSIONS = ["1.3.0"]
 
 from picard.metadata import register_track_metadata_processor
 
-class MusiciansSorted:
+def use_musicians_sorted(self, album, metadata, *args):
 
-    def use_musicians_sorted(self, album, metadata, *args):
+    for key in metadata.keys():
+        if key.startswith('~') or key.endswith('sort'):
+            continue
+        if ':' in key:
+            mainkey, subkey = key.split(':', 1)
+        else:
+            mainkey = key
+            subkey = None
+        sortedkey = '~%ssort%s' % (
+            mainkey,
+            (':' + subkey) if subkey else '',
+            )
+        unsortedkey = '~%s%s' % (
+            mainkey,
+            (':' + subkey) if subkey else '',
+            )
+        if not sortedkey in metadata:
+            continue
+        metadata[unsortedkey] = dict.get(metadata,key)
+        metadata[key] = dict.get(metadata,sortedkey)
 
-        for key in metadata.keys():
-            if key.startswith('~') or key.endswith('sort'):
-                continue
-            if ':' in key:
-                mainkey, subkey = key.split(':', 1)
-            else:
-                mainkey = key
-                subkey = None
-            sortedkey = '~%ssort%s' % (
-                mainkey,
-                (':' + subkey) if subkey else '',
-                )
-            if not sortedkey in metadata:
-                continue
-            metadata[key] = dict.get(metadata,sortedkey)
-
-register_track_metadata_processor(MusiciansSorted().use_musicians_sorted)
+register_track_metadata_processor(use_musicians_sorted)
