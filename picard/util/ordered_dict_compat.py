febf18a949bd7d7a8766c194998d85759ad40143
Laurent Monin <zas@norz.org>
2014-04-11 22:42:36 +0200

    Add copyright notice

--- picard/util/ordered_dict_compat.py
+++ picard/util/ordered_dict_compat.py
@@ -1,3 +1,7 @@
+# From http://code.activestate.com/recipes/576693/
+# (c) 2009-2012 Raymond Hettinger
+# licensed under The MIT License (http://opensource.org/licenses/MIT)
+
 # Backport of OrderedDict() class that runs on Python 2.4, 2.5, 2.6, 2.7 and pypy.
 # Passes Python2.7's test suite and incorporates all the latest updates.
 
