80e852ceae49d8569f7fb569fd587a55e2a3459b
Philipp Wolfer <ph.wolfer@gmail.com>
2014-07-14 22:43:32 +0200

    Allow explicit loading of hidden files.
    This will allow explicit loading of hidden files, but still will ignore hidden files and folders on deeper levels if the "show_hidden_files" option is disabled.
    
    This fixes PICARD-589 and improves PICARD-528.

--- picard/tagger.py
+++ picard/tagger.py
@@ -77,7 +77,7 @@ from picard.util import (
     mbid_validate,
     check_io_encoding,
     uniqify,
-    is_hidden_path,
+    is_hidden,
     versions,
 )
 from picard.webservice import XmlWebService
@@ -345,7 +345,7 @@ class Tagger(QtGui.QApplication):
         new_files = []
         for filename in filenames:
             filename = os.path.normpath(os.path.realpath(filename))
-            if ignore_hidden and is_hidden_path(filename):
+            if ignore_hidden and is_hidden(filename):
                 log.debug("File ignored (hidden): %s" % (filename))
                 continue
             if ignoreregex is not None and ignoreregex.search(filename):
@@ -366,11 +366,14 @@ class Tagger(QtGui.QApplication):
                 file.load(partial(self._file_loaded, target=target))
 
     def add_directory(self, path):
+        ignore_hidden = not config.persist["show_hidden_files"]
         walk = os.walk(unicode(path))
 
         def get_files():
             try:
                 root, dirs, files = walk.next()
+                if ignore_hidden:
+                    dirs[:] = [d for d in dirs if not is_hidden(os.path.join(root, d))]
             except StopIteration:
                 return None
             else:
