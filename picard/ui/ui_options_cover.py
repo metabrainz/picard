7de238be9938985012f28777f48466e06bc0a10b
Laurent Monin <github@norz.org>
2014-07-18 22:34:26 +0200

    Improve release group fallback option description

--- picard/ui/ui_options_cover.py
+++ picard/ui/ui_options_cover.py
@@ -159,5 +159,5 @@ class Ui_CoverOptionsPage(object):
         self.label_2.setText(_("Download only images of the following types:"))
         self.cb_approved_only.setText(_("Download only approved images"))
         self.cb_type_as_filename.setText(_("Use the first image type as the filename. This will not change the filename of front images."))
-        self.caprovider_caa_release_group.setText(_("Use image from release group if no image is found for the release"))
+        self.caprovider_caa_release_group.setText(_("Use the image of the release group if no front image is associated with the release"))
 
