17bc0bfc366e5442e1fb734e7c4c8e47fdd21e55
Sophist <github@sodalis.co.uk>
2014-10-22 20:36:47 +0100

    Add primary artist variable
    See http://forums.musicbrainz.org/viewtopic.php?id=5276 for requirement.

--- picard/mbxml.py
+++ picard/mbxml.py
@@ -203,6 +203,7 @@ def artist_credit_to_metadata(node, m, release=False):
         m["artistsort"] = artistsort
         m["artists"] = artists
         m["~artists_sort"] = artistsort
+        m["~primaryartist"] = artists[0]
 
 
 def label_info_from_node(node):
