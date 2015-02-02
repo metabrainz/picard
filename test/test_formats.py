7a7a8c0140eea6c3df1d32f6b8c501538f94968e
NCenerar <ncenerar@free.fr>
2015-02-02 17:32:41 +0100

    Added an entry in the dict settings for 'remove_extra_padding'

--- test/test_formats.py
+++ test/test_formats.py
@@ -15,6 +15,7 @@ from tempfile import mkstemp
 settings = {
     'enabled_plugins': '',
     'clear_existing_tags': False,
+    'remove_extra_padding': False,
     'remove_images_from_tags': False,
     'write_id3v1': True,
     'id3v2_encoding': 'utf-8',
