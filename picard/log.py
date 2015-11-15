3fd7429b189466603db47a389fb57a290d46a58f
Sophist <github@sodalis.co.uk>
2015-11-15 18:07:23 +0000

    Minor tweak to log.py...
    ... to ensure that linesep is not voided by formatted_log_line.

--- picard/log.py
+++ picard/log.py
@@ -116,7 +116,7 @@ def formatted_log_line(level, time, message, timefmt='hh:mm:ss',
 
 def _stderr_receiver(level, time, msg):
     try:
-        sys.stderr.write(formatted_log_line(level, time, msg + os.linesep))
+        sys.stderr.write(formatted_log_line(level, time, msg) + os.linesep)
     except UnicodeDecodeError:
         import traceback
         traceback.print_exc()
