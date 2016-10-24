46dbab97b9f9df693076869b9226120990e6f000
Wieland Hoffmann <themineo@gmail.com>
2016-10-24 17:30:04 +0200

    Fix a missing traceback import

--- picard/coverart/providers/__init__.py
+++ picard/coverart/providers/__init__.py
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+import traceback
 
 from picard import log, config
 from picard.plugin import ExtensionPoint
