1bfbc683d5d580ebdfbf127bc97ab221b0ce6050
Sambhav Kothari <sambhavs.email@gmail.com>
2016-12-29 17:29:45 +0530

    Remove wav specific supports_tag

--- picard/formats/wav.py
+++ picard/formats/wav.py
@@ -43,7 +43,3 @@ class WAVFile(File):
     def _save(self, filename, metadata):
         log.debug("Saving file %r", filename)
         pass
-
-    def supports_tag(self, name):
-        # WAVFile does not support any tag
-        return False
