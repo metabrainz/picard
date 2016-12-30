8264d8d5c78ac27e5a496676a5cd425f4e148caa
Sambhav Kothari <sambhavs.email@gmail.com>
2016-12-30 16:07:24 +0530

    Add accelerator to Reset Defaults button

--- picard/ui/options/dialog.py
+++ picard/ui/options/dialog.py
@@ -80,7 +80,7 @@ class OptionsDialog(PicardDialog):
         self.ui.setupUi(self)
 
         self.ui.reset_all_button = QtGui.QPushButton(_("&Restore all Defaults"))
-        self.ui.reset_button = QtGui.QPushButton(_("Restore Defaults"))
+        self.ui.reset_button = QtGui.QPushButton(_("Restore &Defaults"))
 
         self.ui.buttonbox.addButton(StandardButton(StandardButton.OK), QtGui.QDialogButtonBox.AcceptRole)
         self.ui.buttonbox.addButton(StandardButton(StandardButton.CANCEL), QtGui.QDialogButtonBox.RejectRole)
