142387a21f70fc16750a999729ac2fd2673e2fe1
Lukáš Lalinský <lukas@oxygene.sk>
2017-01-20 12:22:51 +0100

    Fix windows build

--- scripts/package-win.bat
+++ scripts/package-win.bat
@@ -7,17 +7,17 @@ copy /Y "C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC
 copy /Y "C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\msvcp90.dll" .
 
 wget --no-check-certificate https://github.com/acoustid/chromaprint/releases/download/v%CHROMAPRINT_FPCALC_VERSION%/chromaprint-fpcalc-%CHROMAPRINT_FPCALC_VERSION%-windows-i686.zip -O fpcalc.zip
-7z e fpcalc.zip
+7z x fpcalc.zip -y
 copy /Y chromaprint-fpcalc-%CHROMAPRINT_FPCALC_VERSION%-windows-i686\fpcalc.exe .
 
 wget --no-check-certificate http://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/libdiscid-%DISCID_VERSION%-win32.zip -O libdiscid.zip
-7z e libdiscid.zip
+7z x libdiscid.zip -y
 copy /Y libdiscid-%DISCID_VERSION%-win32\discid.dll .
 
 wget --no-check-certificate https://github.com/metabrainz/picard-plugins/archive/master.zip -O plugins.zip
-7z e plugins.zip -y
-mkdir .\contrib\plugins\
-xcopy /E /Y .\picard-plugins-master\plugins\ .\contrib\plugins\
+7z x plugins.zip -y
+mkdir .\contrib\
+move .\picard-plugins-master\plugins .\contrib\plugins
 
 rmdir /S /Q e
 virtualenv --system-site-packages e
