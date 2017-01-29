4b519bf2f99702f56eec34634d9cdce7e9151de5
Sophist <github@sodalis.co.uk>
2017-01-29 20:23:54 +0000

    New fix addressing only writing to stderr

--- picard/log.py
+++ picard/log.py
@@ -113,7 +113,7 @@ def formatted_log_line(level, time, message, timefmt='hh:mm:ss',
 
 def _stderr_receiver(level, time, msg):
     try:
-        sys.stderr.write(formatted_log_line(level, time, msg + os.linesep))
+        sys.stderr.write("%r%s" % (formatted_log_line(level, time, msg), os.linesep))
     except UnicodeDecodeError:
         import traceback
         traceback.print_exc()
