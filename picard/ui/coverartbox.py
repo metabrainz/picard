0c7e2a0ecd27ec78e2c81b0010932871ff5f13af
Antonio Larrosa <larrosa@kde.org>
2017-03-05 19:09:21 +0100

    PEP8 fixes

--- picard/ui/coverartbox.py
+++ picard/ui/coverartbox.py
@@ -133,7 +133,7 @@ class CoverArtThumbnail(ActiveLabel):
                     pixmap = QtGui.QPixmap()
                     pixmap.loadFromData(self.data[0].data)
                 else:
-                    stack_width, stack_height = (w+displacements*(len(self.data)-1), h+displacements*(len(self.data)-1))
+                    stack_width, stack_height = (w + displacements * (len(self.data) - 1), h + displacements * (len(self.data) - 1))
                     pixmap = QtGui.QPixmap(stack_width, stack_height)
                     bgcolor = self.palette().color(QtGui.QPalette.Window)
                     painter = QtGui.QPainter(pixmap)
@@ -143,8 +143,8 @@ class CoverArtThumbnail(ActiveLabel):
                     for image in self.data:
                         thumb = QtGui.QPixmap()
                         thumb.loadFromData(image.data)
-                        thumb = self.decorate_cover( thumb )
-                        painter.drawPixmap(x - thumb.width()/2, y - thumb.height()/2, thumb)
+                        thumb = self.decorate_cover(thumb)
+                        painter.drawPixmap(x - thumb.width() / 2, y - thumb.height() / 2, thumb)
                         x += displacements
                         y += displacements
                     painter.end()
@@ -153,7 +153,7 @@ class CoverArtThumbnail(ActiveLabel):
                     pixmap = None
 
             if pixmap and not pixmap.isNull():
-                cover = self.decorate_cover( pixmap )
+                cover = self.decorate_cover(pixmap)
                 self.setPixmap(cover)
 
     def set_metadata(self, metadata):
@@ -162,11 +162,10 @@ class CoverArtThumbnail(ActiveLabel):
         if metadata and metadata.images:
             self.related_images = metadata.images
             log.debug("%s using images:" % (self.name), metadata.images)
-            # TODO: Combine all images to show there are different images in use instead of getting the first one
-            data = [ image for image in metadata.images if image.is_front_image() ]
+            data = [image for image in metadata.images if image.is_front_image()]
             if not data:
                 # There's no front image, choose the first one available
-                data = [ metadata.images[0] ]
+                data = [metadata.images[0]]
         self.set_data(data)
         release = None
         if metadata:
