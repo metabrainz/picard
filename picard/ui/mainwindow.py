0dd1e8d6aa078f6c55cd5a2dcb0b8e0495339c95
Antonio Larrosa <larrosa@kde.org>
2017-03-08 10:13:31 +0100

    Start the correct InfoDialog
    I forgot to add the changes from this file to the previous commit
    (654490053ab038c27a628510c9f921ba2bf1be02)

--- picard/ui/mainwindow.py
+++ picard/ui/mainwindow.py
@@ -841,13 +841,12 @@ class MainWindow(QtGui.QMainWindow):
         elif isinstance(self.selected_objects[0], Cluster):
             cluster = self.selected_objects[0]
             dialog = ClusterInfoDialog(cluster, self)
+        elif isinstance(self.selected_objects[0], Track):
+            track = self.selected_objects[0]
+            dialog = TrackInfoDialog(track, self)
         else:
-            files = self.tagger.get_files_from_objects(self.selected_objects)
-            if not files and isinstance(self.selected_objects[0], Track):
-                track = self.selected_objects[0]
-                dialog = TrackInfoDialog(track, self)
-            else:
-                dialog = FileInfoDialog(files[0], self)
+            file = self.tagger.get_files_from_objects(self.selected_objects)[0]
+            dialog = FileInfoDialog(file, self)
         dialog.ui.tabWidget.setCurrentIndex(default_tab)
         dialog.exec_()
 
