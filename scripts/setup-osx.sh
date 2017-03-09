43848a2c9161746ff33648f94f420d0c96a7eadb
Sambhav Kothari <sambhavs.email@gmail.com>
2017-03-09 15:10:49 +0530

    Add OSX tests and a build matrix for different linux envs

---
+++ scripts/setup-osx.sh
@@ -0,0 +1,11 @@
+brew tap cartr/qt4
+brew tap-pin cartr/qt4
+brew install gettext
+brew link gettext --force
+brew install qt
+brew install pyqt
+brew install libdiscid 
+pip install mutagen
+pip install discid
+pip install py2app
+pip install pyobjc-framework-Cocoa
