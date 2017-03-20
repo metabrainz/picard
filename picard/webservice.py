f38d20012878abbbdb901400b1bbecc962855838
Wieland Hoffmann <themineo@gmail.com>
2017-03-20 18:52:35 +0100

    _find: Move keyword arguments that are never changed to the call to self.get

--- picard/webservice.py
+++ picard/webservice.py
@@ -453,9 +453,7 @@ class XmlWebService(QtCore.QObject):
         return self._get_by_id('discid', discid, handler, inc, queryargs={"cdstubs": "no"},
                                priority=priority, important=important, refresh=refresh)
 
-    def _find(self, entitytype, handler,
-              xml=True, priority=True, important=True, mblogin=False,
-              cacheloadcontrol=None, refresh=False, **kwargs):
+    def _find(self, entitytype, handler, **kwargs):
         host = config.setting["server_host"]
         port = config.setting["server_port"]
         filters = []
@@ -487,8 +485,8 @@ class XmlWebService(QtCore.QObject):
             queryargs[str(name)] = value
         path = "/ws/2/%s" % (entitytype)
         return self.get(host, port, path, handler, queryargs=queryargs,
-                        xml=xml, priority=priority, important=important, mblogin=mblogin,
-                        cacheloadcontrol=cacheloadcontrol, refresh=refresh)
+                        xml=True, priority=True, important=True, mblogin=False,
+                        cacheloadcontrol=None, refresh=False)
 
     def find_releases(self, handler, **kwargs):
         return self._find('release', handler, **kwargs)
