54dd5616bb92881e8857b81e78a1cee1658a0b3c
Antonio Larrosa <larrosa@kde.org>
2017-03-24 14:04:53 +0100

    Emit the images changed signal when keeping original images
    This fixes the album cover art to update properly when the user
    selects to keep the original image of only one file in the album

--- picard/file.py
+++ picard/file.py
@@ -178,6 +178,7 @@ class File(QtCore.QObject, Item):
     def keep_original_images(self):
         self.metadata.images = self.orig_metadata.images[:]
         self.update()
+        self.metadata_images_changed.emit()
 
     def has_error(self):
         return self.state == File.ERROR
