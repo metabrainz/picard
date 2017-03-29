8b5a6ae07e09d526e351a7ad9d51c5dd14259ef9
Sophist <github@sodalis.co.uk>
2017-03-29 22:13:24 +0100

    Add threading notes

--- picard/util/thread.py
+++ picard/util/thread.py
@@ -17,6 +17,57 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+# NOTES:
+# Threads in Python do NOT operate as you would expect because of Python's Global Interpreter Lock (GIL).
+# 1. The GIL essentially allows only one Python thread to execute at any time -
+#    threads do NOT execute in parallel on a multi-processor system.
+#    (Indeed because of the way this works in Py2 & Py3.1, multi-threading actually
+#    works better on a single processor system - or with Python execution constrained
+#    to a single processor using affinity - than on a multi-processor system.
+#    O/S thread priorities also make little or no difference to which thread is executed.
+#    In Py3.2, the GIL was changed to make the choice of thread to execute a little better
+#    but Python is still constrained by the GIL to executing a single thread at any time.)
+# 2. Threads give up CPU in only 3 circumstances:
+#    a. After a certain number of Python byte-code instructions - but it is possible
+#       that the same thread will get control again (or even likely or even highly likely -
+#       because this thread is already running whilst the O/S has to schedule other threads).
+#    b. When a thread has to wait for disk I/O (or other blocking activities)
+#    c. When a thread explicitly sleeps.
+# 3. Qt runs in an event loop on the main thread - the UI can only respond to user actions
+#    and other queued events (like log / status messages) when the main thread is:
+#    a. not executing any other code
+#    b. is not executing a blacking operation (like I/O) and
+#    c. has the GIL (i.e. another thread is not executing)
+#    and can therefore process the event queue.
+#
+# See http://www.dabeaz.com/python/UnderstandingGIL.pdf for an explanation.
+#
+# As a consequence, the following rules are proposed to help keep the UI responsive:
+# 1. All disk I/O and other blocking operations should run in worker threads.
+# 2. The number of simultaneous threads should be kept as small as possible as the
+#    overheads for switching threads grow exponentially with the number of threads.
+#    Ideally we want the minimum number of threads to:
+#    a. Maximise but not overload disk I/O
+#    b. Allow UI to show multiple activities happening in parallel e.g. directory walking
+#       in parallel with guess format in parallel with file loading
+#    c. Save files (can only be one file save thread to avoid conflicting writes).
+#    d. Process CPU-only workload
+#    e. Plus main-thread for UI.
+# 3. Qt requires any UI operations need to run in the main thread.
+# 4. All CPU intensive work or potentially high-volume activities (i.e. if the user asks
+#    for an activity on a large number of files) should run in worker-threads.
+# 5. Worker-thread activity that results in a callback for UI changes should be broken into
+#    the smallest possible pieces to execute on the worker-thread so that call-back activity is small.
+#    If necessary the work to break this into small pieces can itself be run in a worker-thread.
+# 6. Worker-threads should sleep for 1ms every so often to give main thread a chance to run
+#    to keep UI responsive. Worker threads should also sleep before exit to avoid next thread-pool
+#    running without giving chance for other threads to execute.
+# 7. Direct main-thread processing of a CPU-intensive UI action can call QCoreApplication.processEvents to
+#    keep UI responsive (though CPUintensive code is better run in a worker thread if possible).
+#    BUT QCoreApplication.processEvents MUST NOT BE RUN FROM A WORKER-THREAD CALL-BACK as
+#    a new worker-thread and its callback may be executed before the remainder of this call-back
+#    leading to Python Recursion Level Exceeded errors.
+
 import sys
 import traceback
 from PyQt4.QtCore import QRunnable, QCoreApplication, QEvent, QThread
@@ -59,41 +110,12 @@ def run_task(func, next, priority=0, thread_pool=None):
 
 
 def to_main(func, *args, **kwargs):
-    # TODO Sophist
-    # At present we use processEvents which both processes events sent to the generic window
-    # and processes UI events as well.
-    # We should consider whether we would have greater control over returning here if we either
-    # a. Use processEvents with flags to exclude user events or socket notifiers; or
-    # b. Use a specific event receiver for to_main rather than the generic receiver and use
-    # sendPostedEvents to execute this code.
-    # c.Use signals rather than events to execute code on the main thread
-    # see http://www.qtcentre.org/threads/7167-Main-thread-worker-thread-communication
-    # My current thoughts are that these are no more likely to switch back to this task,
-    # and just avoid processing UI events which we want processed to keep the UI responsive.
-    # TODO Sophist
-    if func is not None:
-        QCoreApplication.postEvent(QCoreApplication.instance(),
-                                   ProxyToMainEvent(func, *args, **kwargs))
+    QCoreApplication.postEvent(QCoreApplication.instance(),
+                               ProxyToMainEvent(func, *args, **kwargs))
     processEvents()
 
 
 def processEvents():
    if QCoreApplication.instance() and QCoreApplication.instance().thread() != QThread.currentThread():
-        # If we are in a worker thread, use QApplication.processEvents to pass control to the
-        # main thread to execute the event we just posted. If we don't do this
-        # the main thread may not get CPU to process the function for a long time.
-        # See http://www.dabeaz.com/python/UnderstandingGIL.pdf for details.
-        #
-        # If we are in the main thread and post the event and call processEvents then,
-        # IF there are worker threads queued, it is possible (or based on experience likely)
-        # that instead of returning here and finish executing to_main, instead
-        # Python will dispatch a worker thread which will finish by running to_main again.
-        # The multiple instances of to_main will only finish executing when there is no other work.
-        # With sufficient worker thread tasks queued, we will get Recursion Level Exceeded errors.
-        #
-        # Calling from within a worker thread is OK because the number of threads
-        # in thread pools is small and well below Python's recursion limit.
         QCoreApplication.instance().processEvents()
-        # Sleep this thread for a few ms to allow main thread to get CPU.
-        # TODO Python 3 has a different thread dispatch mechanism which needs to be tested
         QThread.currentThread().msleep(10)
