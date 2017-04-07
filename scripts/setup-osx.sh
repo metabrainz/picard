be395cd9f85289b3d7106ba0b87675abb6e40698
Sophist <github@sodalis.co.uk>
2017-04-07 10:05:27 +0100

    Fix Travis for Mac after recent Homebrew update
    With help from @ilovezfs

--- scripts/setup-osx.sh
+++ scripts/setup-osx.sh
@@ -2,9 +2,9 @@ brew tap cartr/qt4
 brew tap-pin cartr/qt4
 brew install gettext
 brew link gettext --force
-brew install qt
-brew install pyqt
-brew install libdiscid 
+brew install cartr/qt4/qt@4
+brew install cartr/qt4/pyqt@4
+brew install libdiscid
 pip install mutagen
 pip install discid
 pip install py2app
