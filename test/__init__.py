9992ade39d0f0e4fc752cf13af01a371120fda56
Sambhav Kothari <sambhavs.email@gmail.com>
2017-04-07 16:45:35 +0530

    Fix py3 compatibility
    sip.setapi is no longer needed with PyQt5 and py3.
    Thus it was removed.

--- test/__init__.py
+++ test/__init__.py
@@ -1,10 +1,5 @@
 import glob
 import os.path
 
-import sip
-
-sip.setapi("QString", 2)
-sip.setapi("QVariant", 2)
-
 for filename in glob.glob(os.path.join(os.path.dirname(__file__), "test_*.py")):
     __import__("test." + os.path.basename(filename)[:-3])
