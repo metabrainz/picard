f672df46fcec78094fba7e4c7a316df5a916a5f8
Sambhav Kothari <sambhavs.email@gmail.com>
2017-04-09 00:01:32 +0530

    Fix crash on Browser lookups
    Crash due to unexpected type string.
    Bytes/str issue with QT

--- picard/browser/browser.py
+++ picard/browser/browser.py
@@ -59,14 +59,14 @@ class BrowserIntegration(QtNetwork.QTcpServer):
     def _process_request(self):
         conn = self.sender()
         line = string_(conn.readLine())
-        conn.write("HTTP/1.1 200 OK\r\nCache-Control: max-age=0\r\n\r\nNothing to see here.")
+        conn.write(b"HTTP/1.1 200 OK\r\nCache-Control: max-age=0\r\n\r\nNothing to see here.")
         conn.disconnectFromHost()
         line = line.split()
         log.debug("Browser integration request: %r", line)
         if line[0] == "GET" and "?" in line[1]:
             action, args = line[1].split("?")
             args = [a.split("=", 1) for a in args.split("&")]
-            args = dict((a, string_(QtCore.QUrl.fromPercentEncoding(b))) for (a, b) in args)
+            args = dict((a, string_(QtCore.QUrl.fromPercentEncoding(b.encode('ascii')))) for (a, b) in args)
             self.tagger.bring_tagger_front()
             if action == "/openalbum":
                 self.tagger.load_album(args["id"])
