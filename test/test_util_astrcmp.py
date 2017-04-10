9e4ffcd5769b408e6e87492e4a2e753c2c7d5df5
Laurent Monin <github@norz.org>
2017-04-11 00:15:57 +0200

    Fix astrcmp tests when C version of astrcmp() isn't available

--- test/test_util_astrcmp.py
+++ test/test_util_astrcmp.py
@@ -3,7 +3,12 @@
 import os
 import os.path
 import unittest
-from picard.util.astrcmp import astrcmp_c, astrcmp_py
+from picard.util.astrcmp import astrcmp_py
+
+try:
+    from picard.util._astrcmp import astrcmp as astrcmp_c
+except ImportError:
+    astrcmp_c = "astrcmp_c() not available"
 
 
 class AstrcmpBase(object):
@@ -11,6 +16,10 @@ class AstrcmpBase(object):
 
     def test_astrcmp(self):
         astrcmp = self.__class__.func
+        # if C version of astrcmp() isn't available, astrcmp_c contains a
+        # string, which is used as a reason for the skip
+        if isinstance(astrcmp, str):
+            raise unittest.SkipTest(astrcmp)
         self.assertAlmostEqual(0.0, astrcmp(u"", u""))
         self.assertAlmostEqual(0.0, astrcmp(u"a", u""))
         self.assertAlmostEqual(0.0, astrcmp(u"", u"a"))
