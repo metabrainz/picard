2b9096a45ed60916297b5f372496335227278595
Sophist <github@sodalis.co.uk>
2017-04-14 18:33:42 +0100

    Move itunesgrouping test to ID3 where it belongs

--- test/test_formats.py
+++ test/test_formats.py
@@ -493,6 +493,7 @@ class MP3Test(ID3Test):
         'movementname': 'Bar',
         'movementnumber': '1',
         'movementtotal': '2',
+        'itunesgrouping': 'Foo',
     }
 
 
@@ -772,7 +773,6 @@ class MP4Test(FormatsTest):
         'movementnumber': '1',
         'movementtotal': '2',
         '~movementshow': '1',
-        'itunesgrouping': 'Foo',
     }
 
 
