22586506cc1b7255f4e6bb9b62c82ef4e334c25c
Sambhav Kothari <sambhavs.email@gmail.com>
2017-07-15 15:14:11 +0530

    Fix tests and add test data

--- test/test_releaseversions.py
+++ test/test_releaseversions.py
@@ -1,3 +1,4 @@
+import json
 import os.path
 import unittest
 import shutil
@@ -16,34 +17,13 @@ settings = {
 }
 
 
-class XmlNode(object):
-
-    def __init__(self, text=u'', children=None, attribs=None):
-        if children is None:
-            self.children = {}
-        else:
-            self.children = children
-        if attribs is None:
-            self.attribs = {}
-        else:
-            self.attribs = attribs
-        self.text = text
-
-    def __repr__(self):
-        return repr(self.__dict__)
-
-    def __getattr__(self, name):
-        try:
-            return self.children[name]
-        except KeyError:
-            try:
-                return self.attribs[name]
-            except KeyError:
-                raise
-                #raise AttributeError, name
-
-
 class ReleaseTest(unittest.TestCase):
+
+    @staticmethod
+    def load_data(filename):
+        with open(os.path.join('test', 'data', 'ws_data', filename)) as f:
+            return json.load(f)
+
     def setUp(self):
         # we are using temporary locales for tests
         self.tmp_path = tempfile.mkdtemp()
@@ -58,98 +38,7 @@ class ReleaseTest(unittest.TestCase):
 
     def test_1(self):
         config.setting = settings
-        rlist = XmlNode(children={
-            'metadata': [XmlNode(children={
-                'release_list': [XmlNode(attribs={'count': '3'}, children={
-                    'release': [
-                        XmlNode(attribs={'id': '123'}, children={
-                            'title': [XmlNode(text='Foo')],
-                            'status': [XmlNode(text='Official')],
-                            'packaging': [XmlNode(text='Jewel Case')],
-                            'disambiguation': [XmlNode(text='special')],
-                            'text_representation': [XmlNode(children={
-                                'language': [XmlNode(text='eng')],
-                                'script': [XmlNode(text='Latn')]
-                                })],
-                            'date': [XmlNode(text='2009-08-07')],
-                            'country': [XmlNode(text='GB')],
-                            'barcode': [XmlNode(text='012345678929')],
-                            'medium_list': [XmlNode(attribs={'count': '1'}, children={
-                                'medium': [XmlNode(children={
-                                    'position': [XmlNode(text='1')],
-                                    'format': [XmlNode(text='CD')],
-                                    'track_list': [XmlNode(attribs={'count': '5'})],
-                                })]
-                            })],
-                            'label_info_list': [XmlNode(attribs={'count': '1'}, children={
-                                'label_info': [XmlNode(children={
-                                    'catalog_number': [XmlNode(text='cat 123')],
-                                    'label': [XmlNode(children={
-                                        'name': [XmlNode(text='label A')]
-                                    })]
-                                })]
-                            })]
-                        }),
-                        XmlNode(attribs={'id': '456'}, children={
-                            'title': [XmlNode(text='Foo')],
-                            'status': [XmlNode(text='Official')],
-                            'packaging': [XmlNode(text='Digipak')],
-                            'disambiguation': [XmlNode(text='special')],
-                            'text_representation': [XmlNode(children={
-                                'language': [XmlNode(text='eng')],
-                                'script': [XmlNode(text='Latn')]
-                                })],
-                            'date': [XmlNode(text='2009-08-07')],
-                            'country': [XmlNode(text='GB')],
-                            'barcode': [XmlNode(text='012345678929')],
-                            'medium_list': [XmlNode(attribs={'count': '1'}, children={
-                                'medium': [XmlNode(children={
-                                    'position': [XmlNode(text='1')],
-                                    'format': [XmlNode(text='CD')],
-                                    'track_list': [XmlNode(attribs={'count': '5'})],
-                                })]
-                            })],
-                            'label_info_list': [XmlNode(attribs={'count': '1'}, children={
-                                'label_info': [XmlNode(children={
-                                    'catalog_number': [XmlNode(text='cat 123')],
-                                    'label': [XmlNode(children={
-                                        'name': [XmlNode(text='label A')]
-                                    })]
-                                })]
-                            })]
-                        }),
-                        XmlNode(attribs={'id': '789'}, children={
-                            'title': [XmlNode(text='Foo')],
-                            'status': [XmlNode(text='Official')],
-                            'packaging': [XmlNode(text='Digipak')],
-                            'disambiguation': [XmlNode(text='specialx')],
-                            'text_representation': [XmlNode(children={
-                                'language': [XmlNode(text='eng')],
-                                'script': [XmlNode(text='Latn')]
-                                })],
-                            'date': [XmlNode(text='2009-08-07')],
-                            'country': [XmlNode(text='GB')],
-                            'barcode': [XmlNode(text='012345678929')],
-                            'medium_list': [XmlNode(attribs={'count': '1'}, children={
-                                'medium': [XmlNode(children={
-                                    'position': [XmlNode(text='1')],
-                                    'format': [XmlNode(text='CD')],
-                                    'track_list': [XmlNode(attribs={'count': '5'})],
-                                })]
-                            })],
-                            'label_info_list': [XmlNode(attribs={'count': '1'}, children={
-                                'label_info': [XmlNode(children={
-                                    'catalog_number': [XmlNode(text='cat 123')],
-                                    'label': [XmlNode(children={
-                                        'name': [XmlNode(text='label A')]
-                                    })]
-                                })]
-                            })]
-                        }),
-                        ]
-                })]
-            })]
-        })
+        rlist = self.load_data('release_group_2.json')
         r = ReleaseGroup(1)
         r._parse_versions(rlist)
         self.assertEqual(r.versions[0]['name'],
@@ -161,65 +50,7 @@ class ReleaseTest(unittest.TestCase):
 
     def test_2(self):
         config.setting = settings
-        rlist = XmlNode(children={
-            'metadata': [XmlNode(children={
-                'release_list': [XmlNode(attribs={'count': '2'}, children={
-                    'release': [
-                        XmlNode(attribs={'id': '789'}, children={
-                            'title': [XmlNode(text='Foox')],
-                            'status': [XmlNode(text='Official')],
-                            'disambiguation': [XmlNode(text='special A')],
-                            'text_representation': [XmlNode(children={
-                                'language': [XmlNode(text='eng')],
-                                'script': [XmlNode(text='Latn')]
-                                })],
-                            'date': [XmlNode(text='2011-08-07')],
-                            'country': [XmlNode(text='FR')],
-                            'medium_list': [XmlNode(attribs={'count': '1'}, children={
-                                'medium': [XmlNode(children={
-                                    'position': [XmlNode(text='1')],
-                                    'format': [XmlNode(text='CD')],
-                                    'track_list': [XmlNode(attribs={'count': '5'})],
-                                })]
-                            })],
-                            'label_info_list': [XmlNode(attribs={'count': '1'}, children={
-                                'label_info': [XmlNode(children={
-                                    'catalog_number': [XmlNode(text='cat 123')],
-                                    'label': [XmlNode(children={
-                                        'name': [XmlNode(text='label A')]
-                                    })]
-                                })]
-                            })]
-                        }),
-                        XmlNode(attribs={'id': '789'}, children={
-                            'title': [XmlNode(text='Foox')],
-                            'status': [XmlNode(text='Official')],
-                            'text_representation': [XmlNode(children={
-                                'language': [XmlNode(text='eng')],
-                                'script': [XmlNode(text='Latn')]
-                                })],
-                            'date': [XmlNode(text='2011-08-07')],
-                            'country': [XmlNode(text='FR')],
-                            'medium_list': [XmlNode(attribs={'count': '1'}, children={
-                                'medium': [XmlNode(children={
-                                    'position': [XmlNode(text='1')],
-                                    'format': [XmlNode(text='CD')],
-                                    'track_list': [XmlNode(attribs={'count': '5'})],
-                                })]
-                            })],
-                            'label_info_list': [XmlNode(attribs={'count': '1'}, children={
-                                'label_info': [XmlNode(children={
-                                    'catalog_number': [XmlNode(text='cat 123')],
-                                    'label': [XmlNode(children={
-                                        'name': [XmlNode(text='label A')]
-                                    })]
-                                })]
-                            })]
-                        }),
-                        ]
-                })]
-            })]
-        })
+        rlist = self.load_data('release_group_3.json')
         r = ReleaseGroup(1)
         r._parse_versions(rlist)
         self.assertEqual(r.versions[0]['name'],
@@ -229,73 +60,10 @@ class ReleaseTest(unittest.TestCase):
 
     def test_3(self):
         config.setting = settings
-        rlist = XmlNode(children={
-            'metadata': [XmlNode(children={
-                'release_list': [XmlNode(attribs={'count': '2'}, children={
-                    'release': [
-                        XmlNode(attribs={'id': '789'}, children={
-                            'title': [XmlNode(text='Foox')],
-                            'status': [XmlNode(text='Official')],
-                            'packaging': [XmlNode(text='Digipak')],
-                            'disambiguation': [XmlNode(text='specialx')],
-                            'text_representation': [XmlNode(children={
-                                'language': [XmlNode(text='eng')],
-                                'script': [XmlNode(text='Latn')]
-                                })],
-                            'date': [XmlNode(text='2009-08-07')],
-                            'country': [XmlNode(text='FR')],
-                            'barcode': [XmlNode(text='012345678929')],
-                            'medium_list': [XmlNode(attribs={'count': '1'}, children={
-                                'medium': [XmlNode(children={
-                                    'position': [XmlNode(text='1')],
-                                    'format': [XmlNode(text='CD')],
-                                    'track_list': [XmlNode(attribs={'count': '5'})],
-                                })]
-                            })],
-                            'label_info_list': [XmlNode(attribs={'count': '1'}, children={
-                                'label_info': [XmlNode(children={
-                                    'catalog_number': [XmlNode(text='cat 123')],
-                                    'label': [XmlNode(children={
-                                        'name': [XmlNode(text='label A')]
-                                    })]
-                                })]
-                            })]
-                        }),
-                        XmlNode(attribs={'id': '789'}, children={
-                            'title': [XmlNode(text='Foox')],
-                            'status': [XmlNode(text='Official')],
-                            'packaging': [XmlNode(text='Digipak')],
-                            'disambiguation': [XmlNode(text='specialx')],
-                            'text_representation': [XmlNode(children={
-                                'language': [XmlNode(text='eng')],
-                                'script': [XmlNode(text='Latn')]
-                                })],
-                            'date': [XmlNode(text='2009-08-07')],
-                            'country': [XmlNode(text='FR')],
-                            'barcode': [XmlNode(text='')],
-                            'medium_list': [XmlNode(attribs={'count': '1'}, children={
-                                'medium': [XmlNode(children={
-                                    'position': [XmlNode(text='1')],
-                                    'format': [XmlNode(text='CD')],
-                                    'track_list': [XmlNode(attribs={'count': '5'})],
-                                })]
-                            })],
-                            'label_info_list': [XmlNode(attribs={'count': '1'}, children={
-                                'label_info': [XmlNode(children={
-                                    'catalog_number': [XmlNode(text='cat 123')],
-                                    'label': [XmlNode(children={
-                                        'name': [XmlNode(text='label A')]
-                                    })]
-                                })]
-                            })]
-                        }),
-                        ]
-                })]
-            })]
-        })
+        rlist = self.load_data('release_group_4.json')
         r = ReleaseGroup(1)
         r._parse_versions(rlist)
         self.assertEqual(r.versions[0]['name'],
-                         '5 / 2009 / FR / CD / label A / cat 123 / 012345678929')
+                         '5 / 2009 / FR / CD / label A / cat 123 / 0123456789')
         self.assertEqual(r.versions[1]['name'],
                          '5 / 2009 / FR / CD / label A / cat 123 / [no barcode]')
