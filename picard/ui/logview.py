1f82facb97df26cd6c5dac61697039b40b0d1d07
Laurent Monin <github@norz.org>
2018-02-04 15:15:22 +0100

    Add verbosity control to log view

--- picard/ui/logview.py
+++ picard/ui/logview.py
@@ -17,6 +17,8 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+from collections import OrderedDict
+from functools import partial
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from picard import config, log
@@ -24,6 +26,8 @@ from picard.ui import PicardDialog
 
 
 class LogViewCommon(PicardDialog):
+
+    default_verbosity = {log.LOG_INFO, log.LOG_WARNING, log.LOG_ERROR, log.LOG_DEBUG}
 
     def __init__(self, title, logger, w=740, h=340, parent=None):
         PicardDialog.__init__(self, parent)
@@ -37,7 +41,8 @@ class LogViewCommon(PicardDialog):
         self.browser.setDocument(self.doc)
         self.vbox = QtWidgets.QVBoxLayout(self)
         self.vbox.addWidget(self.browser)
-        self._display()
+        self._setup_formats()
+        self.verbosity = self.default_verbosity
 
     def _setup_formats(self):
         font = QtGui.QFont()
@@ -64,13 +69,20 @@ class LogViewCommon(PicardDialog):
     def _format(self, level):
         return self.formats[level]
 
-    def _display(self):
-        self._setup_formats()
+    def display(self):
+        try:
+            self.logger.unregister_receiver(self._add_entry)
+        except ValueError:
+            pass
+        self.doc.clear()
+        self.textCursor.movePosition(QtGui.QTextCursor.Start)
         for message_obj in self.logger.entries:
             self._add_entry(message_obj)
         self.logger.register_receiver(self._add_entry)
 
     def _add_entry(self, message_obj):
+        if message_obj.level not in self.verbosity:
+            return
         self.textCursor.movePosition(QtGui.QTextCursor.End)
         self.textCursor.insertText(self._formatted_log_line(message_obj),
                                    self._format(message_obj.level))
@@ -103,24 +115,58 @@ class LogView(LogViewCommon):
     options = [
         config.Option("persist", "logview_position", QtCore.QPoint()),
         config.Option("persist", "logview_size", QtCore.QSize(560, 400)),
+        config.Option("persist", "logview_verbosity", LogViewCommon.default_verbosity)
     ]
+
+    _log_level_names = OrderedDict([
+        (log.LOG_INFO,      N_('Info')),
+        (log.LOG_WARNING,   N_('Warning')),
+        (log.LOG_ERROR,     N_('Error')),
+        (log.LOG_DEBUG,     N_('Debug')),
+    ])
 
     def __init__(self, parent=None):
         title = _("Log")
         logger = log.main_logger
         LogViewCommon.__init__(self, title, logger, parent=parent)
+        self.verbosity = config.persist['logview_verbosity']
         self.restoreWindowState("logview_position", "logview_size")
+        self.hbox = QtWidgets.QHBoxLayout(self)
+        self.vbox.addLayout(self.hbox)
+
         cb = QtWidgets.QCheckBox(_('Debug mode'), self)
         cb.setChecked(QtCore.QObject.tagger._debug)
         cb.stateChanged.connect(self.toggleDebug)
-        self.vbox.addWidget(cb)
+        self.hbox.addWidget(cb)
+
+        self.verbosity_menu = QtWidgets.QMenu(self)
+        for level, label in self._log_level_names.items():
+            act = QtWidgets.QAction(_(label), self.verbosity_menu)
+            act.setCheckable(True)
+            act.setChecked(level in self.verbosity)
+            act.triggered.connect(partial(self._verbosity_changed, level))
+            self.verbosity_menu.addAction(act)
+
+        self.verbosity_menu_button = QtWidgets.QPushButton(_("Verbosity"))
+        self.verbosity_menu_button.setMenu(self.verbosity_menu)
+        self.hbox.addWidget(self.verbosity_menu_button)
+
+        self.display()
 
     def toggleDebug(self, state):
         QtCore.QObject.tagger.debug(state == QtCore.Qt.Checked)
 
     def closeEvent(self, event):
+        config.persist['logview_verbosity'] = self.verbosity
         self.saveWindowState("logview_position", "logview_size")
         event.accept()
+
+    def _verbosity_changed(self, level, checked):
+        if checked:
+            self.verbosity.add(level)
+        else:
+            self.verbosity.discard(level)
+        self.display()
 
 
 class HistoryView(LogViewCommon):
@@ -135,6 +181,7 @@ class HistoryView(LogViewCommon):
         logger = log.history_logger
         LogViewCommon.__init__(self, title, logger, parent=parent)
         self.restoreWindowState("historyview_position", "historyview_size")
+        self.display()
 
     def _formatted_log_line(self, message_obj):
         return log.formatted_log_line(message_obj, level_prefixes=False)
