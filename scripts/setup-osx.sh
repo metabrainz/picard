7843124c9e2a216b85c54d979bada685a59ba219
Sambhav Kothari <sambhavs.email@gmail.com>
2018-02-06 23:23:34 +0530

    Build discid from source

--- scripts/setup-osx.sh
+++ scripts/setup-osx.sh
@@ -4,8 +4,13 @@ brew install python3
 brew link python3 --force
 brew install gettext
 brew link gettext --force
-brew install libdiscid
-cp "/usr/local/Cellar/libdiscid/$DISCID_VERSION/lib/libdiscid.0.dylib" .
+wget "ftp://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/libdiscid-$DISCID_VERSION.tar.gz"
+tar -xf "libdiscid-$DISCID_VERSION.tar.gz"
+cd "libdiscid-$DISCID_VERSION"
+./configure --prefix="$HOME/libdiscid"
+make install
+cd ..
+cp "$HOME/libdiscid/lib/libdiscid.0.dylib" .
 pip3 install --upgrade pip setuptools wheel
 pip3 install virtualenv
 virtualenv -p python3 .
