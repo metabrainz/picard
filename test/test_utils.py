b679e72ce3f52a69e47fb357d07576de3ab440e4
Yagyansh Bhatia <yagyansh1999@gmail.com>
2018-02-23 12:48:42 +0530

    WIP: reviews needed

--- test/test_utils.py
+++ test/test_utils.py
@@ -242,11 +242,10 @@ class TestEncodeFilename(unittest.TestCase):
     def test_EncodeFilenames_support(self):
         self.assertEqual("小室", util.encode_filename("小室"))
 
-# @unittest.skipUnless(sys.getdefaultencoding() == 'utf-8' , "requires utf-8 encoding")
+@unittest.skipUnless(sys.getdefaultencoding() == 'utf-8' , "requires utf-8 encoding")
 class TestDecodeFilename(unittest.TestCase):
-    if sys.getdefaultencoding() == 'utf-8':
+
         def test_AnySystem(self):
-            print(sys.getdefaultencoding())
             self.assertEqual('Пётр Ильич Чайковский', util.decode_filename('Пётр Ильич Чайковский'))
             self.assertEqual("小室哲哉", util.decode_filename("小室哲哉".encode()))
 
@@ -257,3 +256,4 @@ class TestDecodeFilename(unittest.TestCase):
         @unittest.skipIf(os.path.supports_unicode_filenames , "unicode is supported")
         def test_WithoutUnicodeScript(self):
             self.assertEqual('\u9061abc', util.decode_filename(b'\xe9\x81\xa1abc'))
+
