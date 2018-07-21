b8478bb5952abe5932ba45ec866181daaf4e33d3
Sambhav Kothari <sambhavs.email@gmail.com>
2018-07-22 00:23:40 +0530

    Bump version

--- picard/__init__.py
+++ picard/__init__.py
@@ -22,7 +22,7 @@ import re
 
 PICARD_ORG_NAME = "MusicBrainz"
 PICARD_APP_NAME = "Picard"
-PICARD_VERSION = (2, 0, 0, 'final', 0)
+PICARD_VERSION = (2, 0, 1, 'final', 0)
 
 
 # optional build version
