c8f763661f92b612a5810b653568399150bf3cd5
Philipp Wolfer <phil@parolu.io>
2018-08-28 13:29:06 +0200

    PICARD-1216: Do not allocate new console

--- tagger.py
+++ tagger.py
@@ -10,7 +10,7 @@ import sys
 # See https://stackoverflow.com/questions/54536/win32-gui-app-that-writes-usage-text-to-stdout-when-invoked-as-app-exe-help
 if sys.platform == "win32":
     from ctypes import windll
-    if windll.kernel32.AttachConsole(-1) or windll.kernel32.AllocConsole(-1):
+    if windll.kernel32.AttachConsole(-1):
         sys.stdout = open('CON', 'w')
         sys.stderr = open('CON', 'w')
 
