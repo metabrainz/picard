dd49a77ee26172489e513800dca63edf95df108a
Philipp Wolfer <phil@parolu.io>
2018-09-04 07:34:19 +0200

    PICARD-1216: Attempt to attach to console on Windows
    # Conflicts:
    #	.isort.cfg
    #	tagger.py

--- tagger.py
+++ tagger.py
@@ -3,11 +3,25 @@
 import os.path
 import sys
 
-sys.path.insert(0, '.')
+
+# On Windows try to attach to the console as early as possible in order
+# to get stdout / stderr logged to console. This needs to happen before
+# logging gets imported.
+# See https://stackoverflow.com/questions/54536/win32-gui-app-that-writes-usage-text-to-stdout-when-invoked-as-app-exe-help
+if sys.platform == "win32":
+    from ctypes import windll
+    if windll.kernel32.AttachConsole(-1) or windll.kernel32.AllocConsole(-1):
+        sys.stdout = open('CON', 'w')
+        sys.stderr = open('CON', 'w')
+
 
 from picard.tagger import main
-from picard.util import is_frozen, frozen_temp_path
+from picard.util import (
+    frozen_temp_path,
+    is_frozen,
+)
 
+sys.path.insert(0, '.')
 
 # This is needed to find resources when using pyinstaller
 if is_frozen:
