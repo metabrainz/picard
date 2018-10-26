fdaea7bce1453514bc8b277af3dd4a742b58136e
virusMac <mszulsm@gmail.com>
2018-10-26 20:18:10 +0200

    Add func_title in script.py and tests for it in test_script.py
    + unit tests for func_test
    
    Add func_title and tests for it in test_script.py
    added 2 tests to func_title
    
    added 2 tests to func_title

--- test/test_script.py
+++ test/test_script.py
@@ -318,10 +318,12 @@ class ScriptParserTest(PicardTestCase):
         self.assertScriptResultEquals("$ne_any(abc,abc,def,ghi)", "1")
 
     def test_cmd_title(self):
-        self.assertScriptResultEquals("$title(abc Def g)","Abc Def G")
-        self.assertScriptResultEquals("$title(Abc Def G)","Abc Def G")
-        self.assertScriptResultEquals("$title(abc def g)","Abc Def G")
-        self.assertScriptResultEquals("$title(#1abc 4def - g)","#1abc 4def - G")
+        self.assertScriptResultEquals("$title(abc Def g)", "Abc Def G")
+        self.assertScriptResultEquals("$title(Abc Def G)", "Abc Def G")
+        self.assertScriptResultEquals("$title(abc def g)", "Abc Def G")
+        self.assertScriptResultEquals("$title(#1abc 4def - g)", "#1abc 4def - G")
+        self.assertScriptResultEquals("$title(abcd \\(efg hi jkl mno\\))","Abcd (Efg Hi Jkl Mno)")
+        self.assertScriptResultEquals("$title(...abcd)", "...Abcd")
 
     def test_cmd_swapprefix(self):
         self.assertScriptResultEquals("$swapprefix(A stitch in time)", "stitch in time, A")
