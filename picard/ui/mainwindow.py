8bcc01920a4d129357925e2ada65c41c6bd7cd08
Philipp Wolfer <phil@parolu.io>
2018-11-15 11:53:22 +0100

    PICARD-1415: Fix opening UNC paths on Windows
    Removes an older workaround that is no longer applicable.

--- picard/ui/mainwindow.py
+++ picard/ui/mainwindow.py
@@ -889,12 +889,7 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
         return self.tagger.analyze(self.selected_objects)
 
     def _openUrl(self, url):
-        # Resolves a bug in Qt opening remote URLs - QTBUG-13359
-        # See https://bugreports.qt.io/browse/QTBUG-13359
-        if url.startswith("\\\\") or url.startswith("//"):
-            return QtCore.QUrl(QtCore.QDir.toNativeSeparators(url))
-        else:
-            return QtCore.QUrl.fromLocalFile(url)
+        return QtCore.QUrl.fromLocalFile(url)
 
     def play_file(self):
         files = self.tagger.get_files_from_objects(self.selected_objects)
