4f9c72f11c1d0a6d91b94cd608ae9afef7aa694d
Laurent Monin <github@norz.org>
2018-11-16 11:38:29 +0100

    Split About text in smaller parts to prevent unneeded re-translation.

--- picard/ui/options/about.py
+++ picard/ui/options/about.py
@@ -53,7 +53,7 @@ class AboutOptionsPage(OptionsPage):
         # TR: Replace this with your name to have it appear in the "About" dialog.
         args["translator-credits"] = _("translator-credits")
         if args["translator-credits"] != "translator-credits":
-            # TR: Replace LANG with language you are translatig to.
+            # TR: Replace LANG with language you are translating to.
             args["translator-credits"] = _("<br/>Translated to LANG by %s") % args["translator-credits"].replace("\n", "<br/>")
         else:
             args["translator-credits"] = ""
@@ -62,6 +62,21 @@ class AboutOptionsPage(OptionsPage):
                                                         (versions.version_name(name), value) for name, value
                                                         in versions.as_dict(i18n=True).items()
                                                         if name != 'version'])
+        args['authors-credits'] = ", ".join([
+            'Robert Kaye',
+            'Lukáš Lalinský',
+            'Laurent Monin',
+            'Sambhav Kothari',
+            'Philipp Wolfer',
+        ])
+        args['copyright-years'] = '2004-2018'
+        args['flaticons-credits'] = _("""Icons made by Sambhav Kothari <sambhavs.email@gmail.com>
+and <a href="http://www.flaticon.com/authors/madebyoliver">Madebyoliver</a>,
+<a href="http://www.flaticon.com/authors/pixel-buddha">Pixel Buddha</a>,
+<a href="http://www.flaticon.com/authors/nikita-golubev">Nikita Golubev</a>,
+<a href="http://www.flaticon.com/authors/maxim-basinski">Maxim Basinski</a>
+from <a href="www.flaticon.com">www.flaticon.com</a>""")
+
         text = _("""<p align="center"><span style="font-size:15px;font-weight:bold;">MusicBrainz Picard</span><br/>
 Version %(version)s</p>
 <p align="center"><small>
@@ -72,14 +87,8 @@ Version %(version)s</p>
 Thank you for using Picard. Picard relies on the MusicBrainz database, which is operated by the MetaBrainz Foundation with the help of thousands of volunteers. If you like this application please consider donating to the MetaBrainz Foundation to keep the service running.</p>
 <p align="center"><a href="%(picard-donate-url)s">Donate now!</a></p>
 <p align="center"><strong>Credits</strong><br/>
-<small>Copyright © 2004-2018 Robert Kaye, Lukáš Lalinský, Laurent Monin, Sambhav Kothari, Philipp Wolfer and others%(translator-credits)s</small></p>
-<p align="center"><small>Icons made by Sambhav Kothari <sambhavs.email@gmail.com>
-and <a href="http://www.flaticon.com/authors/madebyoliver">Madebyoliver</a>,
-<a href="http://www.flaticon.com/authors/pixel-buddha">Pixel Buddha</a>,
-<a href="http://www.flaticon.com/authors/nikita-golubev">Nikita Golubev</a>,
-<a href="http://www.flaticon.com/authors/maxim-basinski">Maxim Basinski</a>
-from <a href="www.flaticon.com">www.flaticon.com</a>
-</small></p>
+<small>Copyright © %(copyright-years)s %(authors-credits)s and others%(translator-credits)s</small></p>
+<p align="center"><small>%(flaticons-credits)s</small></p>
 <p align="center"><strong>Official website</strong><br/><a href="%(picard-doc-url)s">%(picard-doc-url)s</a></p>
 """) % args
         self.ui.label.setOpenExternalLinks(True)
