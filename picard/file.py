d53e46af1763ded3a67482773ab8f3122e545656
Philipp Wolfer <phil@parolu.io>
2018-12-10 16:23:58 +0100

    PICARD-1437: Fix any deleted tags in new metadata mark file as changed

--- picard/file.py
+++ picard/file.py
@@ -499,7 +499,8 @@ class File(QtCore.QObject, Item):
         for name in names:
             if not name.startswith('~') and self.supports_tag(name):
                 new_values = new_metadata.getall(name)
-                if not (new_values or clear_existing_tags or new_metadata.deleted_tags):
+                if not (new_values or clear_existing_tags or
+                    name in new_metadata.deleted_tags):
                     continue
                 orig_values = self.orig_metadata.getall(name)
                 if orig_values != new_values:
