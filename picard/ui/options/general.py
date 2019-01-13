5391db12e775545a5361d9a51b607a2be4ddcf47
Wieland Hoffmann <themineo@gmail.com>
2019-01-13 10:22:25 +0100

    Remove unused imports

--- picard/ui/options/general.py
+++ picard/ui/options/general.py
@@ -18,7 +18,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 from PyQt5 import QtCore
-from PyQt5.QtWidgets import QInputDialog
 
 from picard import config
 from picard.const import (
