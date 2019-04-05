194935a6ff94d2adbc506b02145c47ac7579128d
Laurent Monin <github@norz.org>
2019-04-05 15:28:10 +0200

    Invert test and branches, to be consistent with code above

--- picard/plugin.py
+++ picard/plugin.py
@@ -392,10 +392,10 @@ class PluginManager(QtCore.QObject):
                             existing_plugin.version,
                             existing_plugin.file)
                 _unregister_module_extensions(name)
-            if not zip_importer:
-                plugin_module = imp.load_module(_PLUGIN_MODULE_PREFIX + name, *info)
-            else:
+            if zip_importer:
                 plugin_module = zip_importer.load_module(_PLUGIN_MODULE_PREFIX + name)
+            else:
+                plugin_module = imp.load_module(_PLUGIN_MODULE_PREFIX + name, *info)
             plugin = PluginWrapper(plugin_module, plugindir,
                                    file=module_pathname, manifest_data=manifest_data)
             compatible_versions = self._compatible_api_versions(plugin.api_versions)
