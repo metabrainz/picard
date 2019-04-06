d8eecb76ae53a7f8c2ccb6cee20a22f5f3b66613
Laurent Monin <github@norz.org>
2019-04-06 15:24:27 +0200

    Fix and simplify error message

--- picard/plugin.py
+++ picard/plugin.py
@@ -540,7 +540,7 @@ class PluginManager(QtCore.QObject):
                 try:
                     installed_plugin = self._load_plugin_from_directory(plugin_name, self.plugins_directory)
                 except Exception as e:
-                    log.error('Unable to load plugin: %s.\nError occured: %s', plugin_name, e)
+                    log.error("Unable to load plugin '%s': %s", plugin_name, e)
                 else:
                     self.plugin_installed.emit(installed_plugin, False)
             else:
