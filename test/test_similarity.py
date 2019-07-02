cf4ac13d9af039f85de00dfea21b29cd4ebe85f7
Laurent Monin <github@norz.org>
2019-07-02 14:41:06 +0200

    Add few tests for similarity2() function

--- test/test_similarity.py
+++ test/test_similarity.py
@@ -2,7 +2,10 @@
 
 from test.picardtestcase import PicardTestCase
 
-from picard.similarity import similarity
+from picard.similarity import (
+    similarity,
+    similarity2,
+)
 
 
 class SimilarityTest(PicardTestCase):
@@ -11,3 +14,39 @@ class SimilarityTest(PicardTestCase):
         self.assertEqual(similarity("K!", "K!"), 1.0)
         self.assertEqual(similarity("BBB", "AAA"), 0.0)
         self.assertAlmostEqual(similarity("ABC", "ABB"), 0.7, 1)
+
+
+class Similarity2Test(PicardTestCase):
+    def test_1(self):
+        a = b = "a b c"
+        self.assertEqual(similarity2(a, b), 1.0)
+
+    def test_2(self):
+        a = "a b c"
+        b = "A,B•C"
+        self.assertEqual(similarity2(a, b), 1.0)
+
+    def test_3(self):
+        a = "a b c"
+        b = ",A, B •C•"
+        self.assertEqual(similarity2(a, b), 1.0)
+
+    def test_4(self):
+        a = "a b c"
+        b = "c a b"
+        self.assertEqual(similarity2(a, b), 1.0)
+
+    def test_5(self):
+        a = "a b c"
+        b = "a b d"
+        self.assertAlmostEqual(similarity2(a, b), 0.6, 1)
+
+    def test_6(self):
+        a = "a b c"
+        b = "a f d"
+        self.assertAlmostEqual(similarity2(a, b), 0.3, 1)
+
+    def test_7(self):
+        a = "abc"
+        b = "def"
+        self.assertEqual(similarity2(a, b), 0.0)
