3f5018f42360c327d49ceb423632b357b7b03f85
Kurt Mosiejczuk <kurt@cranky.work>
2019-07-06 12:37:18 -0400

    Switching to importing install from setuptools to support --single-version-externally-managed so I can update the OpenBSD picard port

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 import datetime
 from distutils import log
 from distutils.command.build import build
-from distutils.command.install import install as install
+from setuptools.command.install import install as install
 from distutils.dep_util import newer
 from distutils.spawn import find_executable
 import glob
