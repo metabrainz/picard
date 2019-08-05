e1b8ba37c45e142f28cd0c5fb16adc90bc0f3245
Philipp Wolfer <phil@parolu.io>
2019-08-05 19:39:41 +0200

    PICARD-1559: Refactor renaming options UI
    The renaming script and compatibility settings are now independent of rename_files being enabled or not.

--- picard/ui/ui_options_renaming.py
+++ picard/ui/ui_options_renaming.py
@@ -3,7 +3,9 @@
 # Automatically generated - don't edit.
 # Use `python setup.py build_ui` to update it.
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_RenamingOptionsPage(object):
     def setupUi(self, RenamingOptionsPage):
@@ -52,24 +54,16 @@ class Ui_RenamingOptionsPage(object):
         self.delete_empty_dirs.setObjectName("delete_empty_dirs")
         self.verticalLayout_4.addWidget(self.delete_empty_dirs)
         self.verticalLayout_5.addWidget(self.move_files)
-        self.rename_files = QtWidgets.QGroupBox(RenamingOptionsPage)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.rename_files.sizePolicy().hasHeightForWidth())
-        self.rename_files.setSizePolicy(sizePolicy)
-        self.rename_files.setCheckable(True)
-        self.rename_files.setChecked(False)
+        self.rename_files = QtWidgets.QCheckBox(RenamingOptionsPage)
         self.rename_files.setObjectName("rename_files")
-        self.verticalLayout_3 = QtWidgets.QVBoxLayout(self.rename_files)
-        self.verticalLayout_3.setObjectName("verticalLayout_3")
-        self.ascii_filenames = QtWidgets.QCheckBox(self.rename_files)
+        self.verticalLayout_5.addWidget(self.rename_files)
+        self.ascii_filenames = QtWidgets.QCheckBox(RenamingOptionsPage)
         self.ascii_filenames.setObjectName("ascii_filenames")
-        self.verticalLayout_3.addWidget(self.ascii_filenames)
-        self.windows_compatibility = QtWidgets.QCheckBox(self.rename_files)
+        self.verticalLayout_5.addWidget(self.ascii_filenames)
+        self.windows_compatibility = QtWidgets.QCheckBox(RenamingOptionsPage)
         self.windows_compatibility.setObjectName("windows_compatibility")
-        self.verticalLayout_3.addWidget(self.windows_compatibility)
-        self.file_naming_format_group = QtWidgets.QGroupBox(self.rename_files)
+        self.verticalLayout_5.addWidget(self.windows_compatibility)
+        self.file_naming_format_group = QtWidgets.QGroupBox(RenamingOptionsPage)
         self.file_naming_format_group.setObjectName("file_naming_format_group")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.file_naming_format_group)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
@@ -110,15 +104,14 @@ class Ui_RenamingOptionsPage(object):
         self.file_naming_format_default.setObjectName("file_naming_format_default")
         self.horizontalLayout.addWidget(self.file_naming_format_default)
         self.verticalLayout_2.addLayout(self.horizontalLayout)
-        self.verticalLayout_3.addWidget(self.file_naming_format_group)
-        self.file_naming_format_documentation = QtWidgets.QLabel(self.rename_files)
+        self.verticalLayout_5.addWidget(self.file_naming_format_group)
+        self.file_naming_format_documentation = QtWidgets.QLabel(RenamingOptionsPage)
         self.file_naming_format_documentation.setText("")
         self.file_naming_format_documentation.setTextFormat(QtCore.Qt.RichText)
         self.file_naming_format_documentation.setWordWrap(True)
         self.file_naming_format_documentation.setOpenExternalLinks(True)
         self.file_naming_format_documentation.setObjectName("file_naming_format_documentation")
-        self.verticalLayout_3.addWidget(self.file_naming_format_documentation)
-        self.verticalLayout_5.addWidget(self.rename_files)
+        self.verticalLayout_5.addWidget(self.file_naming_format_documentation)
         self.groupBox = QtWidgets.QGroupBox(RenamingOptionsPage)
         self.groupBox.setObjectName("groupBox")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.groupBox)
@@ -149,10 +142,7 @@ class Ui_RenamingOptionsPage(object):
         RenamingOptionsPage.setTabOrder(self.move_files_to_browse, self.move_additional_files)
         RenamingOptionsPage.setTabOrder(self.move_additional_files, self.move_additional_files_pattern)
         RenamingOptionsPage.setTabOrder(self.move_additional_files_pattern, self.delete_empty_dirs)
-        RenamingOptionsPage.setTabOrder(self.delete_empty_dirs, self.rename_files)
-        RenamingOptionsPage.setTabOrder(self.rename_files, self.ascii_filenames)
-        RenamingOptionsPage.setTabOrder(self.ascii_filenames, self.windows_compatibility)
-        RenamingOptionsPage.setTabOrder(self.windows_compatibility, self.file_naming_format)
+        RenamingOptionsPage.setTabOrder(self.delete_empty_dirs, self.file_naming_format)
         RenamingOptionsPage.setTabOrder(self.file_naming_format, self.file_naming_format_default)
 
     def retranslateUi(self, RenamingOptionsPage):
@@ -162,7 +152,7 @@ class Ui_RenamingOptionsPage(object):
         self.move_files_to_browse.setText(_("Browse..."))
         self.move_additional_files.setText(_("Move additional files (case insensitive):"))
         self.delete_empty_dirs.setText(_("Delete empty directories"))
-        self.rename_files.setTitle(_("Rename files when saving"))
+        self.rename_files.setText(_("Rename files when saving"))
         self.ascii_filenames.setText(_("Replace non-ASCII characters"))
         self.windows_compatibility.setText(_("Windows compatibility"))
         self.file_naming_format_group.setTitle(_("Name files like this"))
@@ -173,4 +163,3 @@ class Ui_RenamingOptionsPage(object):
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>"))
         self.file_naming_format_default.setText(_("Default"))
         self.groupBox.setTitle(_("Examples"))
-
