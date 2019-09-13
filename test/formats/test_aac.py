663d04f5cf5403a82f6d827eee4d8b25950ecfd2
Philipp Wolfer <phil@parolu.io>
2019-09-13 09:37:45 +0200

    Run full APE tests for AAC files

--- test/formats/test_aac.py
+++ test/formats/test_aac.py
@@ -1,4 +1,5 @@
 from .common import CommonTests
+from .test_apev2 import CommonApeTests
 
 
 class AACTest(CommonTests.SimpleFormatsTestCase):
@@ -12,6 +13,13 @@ class AACTest(CommonTests.SimpleFormatsTestCase):
     unexpected_info = ['~video']
 
 
-class AACWithAPETest(CommonTests.TagFormatsTestCase):
+class AACWithAPETest(CommonApeTests.ApeTestCase):
     testfile = 'test-apev2.aac'
     supports_ratings = False
+    expected_info = {
+        'length': 120,
+        '~channels': '2',
+        '~sample_rate': '44100',
+        '~bitrate': '123.824',
+    }
+    unexpected_info = ['~video']
