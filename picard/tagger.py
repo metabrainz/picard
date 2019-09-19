metabrainz/picard

commit c6815c4fada5f6f006673d21cecf870f88cc932c

Fix exception caused by an attempt to remove already removed NATS album
When removing an album and a non-album track, remove_nat() will call remove_album().
remove() loop over objects to remove, and call remove_album() again on the same object

This causes a KeyError: 'NATS' exception on del self.albums[album.id] execution

Solution: do not call remove_album() for NAT albums, as remove_nat() will take care of NATS pseudo album removal.

picard/tagger.py
@@ -722,7 +722,7 @@ class Tagger(QtWidgets.QApplication):
                 self.remove_nat(obj)
             elif isinstance(obj, Track):
                 files.extend(obj.linked_files)
-            elif isinstance(obj, Album):
+            elif isinstance(obj, Album) and not isinstance(obj, NatAlbum):
                 self.window.set_statusbar_message(
                     N_("Removing album %(id)s: %(artist)s - %(album)s"),
                     {

commit 1847080df2f231314a8530a1404abb4c39546bc1

PICARD-1608: Allow removal of "non-album tracks" pseudo-album again
Directly removing the NAT pseudo-album broke with c6815c4fada5f6f006673d21cecf870f88cc932c. This commit fixed the issue, that removing both the non-album tracks album and the last NAT track would cause a crash, since removal of the NAT album was attempted twice.

Solution: Allow direct removal of the NAT album, but ensure duplicate calls to remove_album() do not crash.

picard/tagger.py
@@ -681,6 +681,8 @@ class Tagger(QtWidgets.QApplication):
     def remove_album(self, album):
         """Remove the specified album."""
         log.debug("Removing %r", album)
+        if album.id not in self.albums:
+            return
         album.stop_loading()
         self.remove_files(self.get_files_from_objects([album]))
         del self.albums[album.id]
@@ -722,7 +724,7 @@ class Tagger(QtWidgets.QApplication):
                 self.remove_nat(obj)
             elif isinstance(obj, Track):
                 files.extend(obj.linked_files)
-            elif isinstance(obj, Album) and not isinstance(obj, NatAlbum):
+            elif isinstance(obj, Album):
                 self.window.set_statusbar_message(
                     N_("Removing album %(id)s: %(artist)s - %(album)s"),
                     {