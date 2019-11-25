2cc127cb74abcc28e01b533cfff6e463e899fec4
Philipp Wolfer <ph.wolfer@gmail.com>
2019-11-25 21:17:12 +0100

    Github Actions: Package Windows app

---
+++ scripts/package/win-setup.ps1
@@ -0,0 +1,39 @@
+Param(
+  [Parameter(Mandatory=$true)]
+  [String]
+  $DiscidVersion,
+  [Parameter(Mandatory=$true)]
+  [String]
+  $FpcalVersion
+)
+
+$ErrorActionPreference = "Stop"
+
+Function DownloadFile {
+  Param(
+    [Parameter(Mandatory=$true)]
+    [String]
+    $FileName,
+    [Parameter(Mandatory=$true)]
+    [String]
+    $Url
+  )
+  $OutputPath = (Join-Path (Resolve-Path .) $FileName)
+  (New-Object System.Net.WebClient).DownloadFile($Url, "$OutputPath")
+}
+
+New-Item -Name .\build -ItemType Directory -ErrorAction Ignore
+
+$ArchiveFile = ".\build\libdiscid.zip"
+Write-Output "Downloading libdiscid to $ArchiveFile..."
+DownloadFile -Url "https://github.com/metabrainz/libdiscid/releases/download/v$DiscidVersion/libdiscid-$DiscidVersion-win64.zip" `
+  -FileName $ArchiveFile
+Expand-Archive -Path $ArchiveFile -DestinationPath .\build\libdiscid -Force
+Copy-Item .\build\libdiscid\discid.dll .
+
+$ArchiveFile = ".\build\fpcalc.zip"
+Write-Output "Downloading chromaprint-fpcalc to $ArchiveFile..."
+DownloadFile -Url "https://github.com/acoustid/chromaprint/releases/download/v$FpcalVersion/chromaprint-fpcalc-$FpcalVersion-windows-x86_64.zip" `
+    -FileName $ArchiveFile
+Expand-Archive -Path $ArchiveFile -DestinationPath .\build\fpcalc -Force
+Copy-Item .\build\fpcalc\chromaprint-fpcalc-$FpcalVersion-windows-x86_64\fpcalc.exe .
