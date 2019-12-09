c5d9291c1733b3fb8e84b6b1557a9dfcdd1c906c
Philipp Wolfer <ph.wolfer@gmail.com>
2019-12-09 18:01:21 +0100

    Refactored AdvancedScriptItem layout
    Generated QHBoxLayout based layout in Qt Designer. This layout is simpler and provides better resize behavior.

---
+++ picard/ui/widgets/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Picard, the next-generation MusicBrainz tagger
+# Copyright (C) 2019 Philipp Wolfer
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+from PyQt5 import (
+    QtCore,
+    QtGui,
+    QtWidgets,
+)
+
+
+class ElidedLabel(QtWidgets.QLabel):
+    """A QLabel that elides the displayed text with ellipsis when resized."""
+
+    def __init__(self, parent=None):
+        self._full_label = ""
+        super().__init__(parent)
+
+    def setText(self, text):
+        self._full_label = text
+        self._update_text()
+
+    def resizeEvent(self, event):
+        self._update_text()
+
+    def _update_text(self):
+        metrics = QtGui.QFontMetrics(self.font())
+        elided_label = metrics.elidedText(self._full_label,
+                                          QtCore.Qt.ElideRight,
+                                          self.width())
+        super().setText(elided_label)
