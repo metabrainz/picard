f293223f70613693feac21d89cde194f79dcd2d0
Philipp Wolfer <ph.wolfer@gmail.com>
2019-12-17 08:51:48 +0100

    Consistently place list editors in options inside of QGroupBox

--- picard/ui/ui_options_interface_top_tags.py
+++ picard/ui/ui_options_interface_top_tags.py
@@ -15,22 +15,24 @@ class Ui_InterfaceTopTagsOptionsPage(object):
         self.vboxlayout.setContentsMargins(9, 9, 9, 9)
         self.vboxlayout.setSpacing(6)
         self.vboxlayout.setObjectName("vboxlayout")
-        self.label = QtWidgets.QLabel(InterfaceTopTagsOptionsPage)
-        self.label.setObjectName("label")
-        self.vboxlayout.addWidget(self.label)
-        self.top_tags_list = TagListEditor(InterfaceTopTagsOptionsPage)
+        self.groupBox = QtWidgets.QGroupBox(InterfaceTopTagsOptionsPage)
+        self.groupBox.setObjectName("groupBox")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.groupBox)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.top_tags_list = TagListEditor(self.groupBox)
         sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.top_tags_list.sizePolicy().hasHeightForWidth())
         self.top_tags_list.setSizePolicy(sizePolicy)
         self.top_tags_list.setObjectName("top_tags_list")
-        self.vboxlayout.addWidget(self.top_tags_list)
+        self.verticalLayout.addWidget(self.top_tags_list)
+        self.vboxlayout.addWidget(self.groupBox)
 
         self.retranslateUi(InterfaceTopTagsOptionsPage)
         QtCore.QMetaObject.connectSlotsByName(InterfaceTopTagsOptionsPage)
 
     def retranslateUi(self, InterfaceTopTagsOptionsPage):
         _translate = QtCore.QCoreApplication.translate
-        self.label.setText(_("Show the below tags above all other tags in the metadata view"))
+        self.groupBox.setTitle(_("Show the below tags above all other tags in the metadata view"))
 from picard.ui.widgets.taglisteditor import TagListEditor
