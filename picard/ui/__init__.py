metabrainz/picard

commit f8eb75596e8090d2f08e9290608dbe7a9f4964d4

PICARD-1571: Allow opening only a single options dialog
Fixes issues on macOS where it was possible to have more than one
options dialog open.

picard/ui/mainwindow.py
@@ -177,6 +177,7 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
 
         self.logDialog = LogView(self)
         self.historyDialog = HistoryView(self)
+        self.optionsDialog = None
 
         bottomLayout = QtWidgets.QHBoxLayout()
         bottomLayout.setContentsMargins(0, 0, 0, 0)
@@ -928,8 +929,15 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
         self.show_options("about")
 
     def show_options(self, page=None):
-        dialog = OptionsDialog(page, self)
-        dialog.exec_()
+        if not self.optionsDialog:
+            self.optionsDialog = OptionsDialog(page, self)
+            self.optionsDialog.finished.connect(self.on_options_closed)
+        self.optionsDialog.show()
+        self.optionsDialog.raise_()
+        self.optionsDialog.activateWindow()
+
+    def on_options_closed(self):
+        self.optionsDialog = None
 
     def show_help(self):
         webbrowser2.goto('documentation')

picard/ui/options/dialog.py
@@ -86,6 +86,7 @@ class OptionsDialog(PicardDialog):
 
     def __init__(self, default_page=None, parent=None):
         super().__init__(parent)
+        self.setWindowModality(QtCore.Qt.ApplicationModal)
 
         from picard.ui.ui_options import Ui_Dialog
         self.ui = Ui_Dialog()

commit ed924b1c65e09d2de8c8c5a096949cbbd140557e

PICARD-1699: Fix run fingerprinting after asking user to enable it
When fingerprinting is disabled and the user tries to use scan, Picard asks the user if they want to open the options to configure fingerprinting. After the user has enabled fingerprinting Picard is supposed to perform the action. This commit restores this functionaility which broke in f8eb755.

picard/ui/__init__.py
@@ -73,6 +73,7 @@ class SingletonDialog:
         instance.show()
         instance.raise_()
         instance.activateWindow()
+        return instance
 
     @classmethod
     def _on_dialog_finished(cls):

picard/ui/mainwindow.py
@@ -910,10 +910,10 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
         self.tagger.activeWindow().close()
 
     def show_about(self):
-        self.show_options("about")
+        return self.show_options("about")
 
     def show_options(self, page=None):
-        OptionsDialog.show_instance(page, self)
+        return OptionsDialog.show_instance(page, self)
 
     def show_help(self):
         webbrowser2.goto('documentation')
@@ -946,20 +946,16 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
         self.panel.remove(self.selected_objects)
 
     def analyze(self):
-        if not config.setting['fingerprinting_system']:
-            if self.show_analyze_settings_info():
-                self.show_options("fingerprinting")
-            if not config.setting['fingerprinting_system']:
-                return
-        return self.tagger.analyze(self.selected_objects)
+        def callback(fingerprinting_system):
+            if fingerprinting_system:
+                self.tagger.analyze(self.selected_objects)
+        self._ensure_fingerprinting_configured(callback)
 
     def generate_fingerprints(self):
-        if not config.setting['fingerprinting_system']:
-            if self.show_analyze_settings_info():
-                self.show_options("fingerprinting")
-            if not config.setting['fingerprinting_system']:
-                return
-        return self.tagger.generate_fingerprints(self.selected_objects)
+        def callback(fingerprinting_system):
+            if fingerprinting_system:
+                self.tagger.generate_fingerprints(self.selected_objects)
+        self._ensure_fingerprinting_configured(callback)
 
     def _openUrl(self, url):
         return QtCore.QUrl.fromLocalFile(url)
@@ -978,7 +974,17 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
         for folder in folders:
             QtGui.QDesktopServices.openUrl(self._openUrl(folder))
 
-    def show_analyze_settings_info(self):
+    def _ensure_fingerprinting_configured(self, callback):
+        def on_finished(result):
+            callback(config.setting['fingerprinting_system'])
+        if not config.setting['fingerprinting_system']:
+            if self._show_analyze_settings_info():
+                dialog = self.show_options("fingerprinting")
+                dialog.finished.connect(on_finished)
+        else:
+            callback(config.setting['fingerprinting_system'])
+
+    def _show_analyze_settings_info(self):
         ret = QtWidgets.QMessageBox.question(self,
             _("Configuration Required"),
             _("Audio fingerprinting is not yet configured. Would you like to configure it now?"),