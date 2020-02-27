03c6aa08396fa7abdd5aebcae843dc34f942ba1d
Laurent Monin <github@norz.org>
2020-02-27 19:21:39 +0100

    Fix import order

--- picard/coverart/image.py
+++ picard/coverart/image.py
@@ -28,8 +28,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 
-from hashlib import md5
 import filecmp
+from hashlib import md5
 import os
 import shutil
 import tempfile
