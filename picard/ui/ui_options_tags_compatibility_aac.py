e8affd73177f789fc68acbfabde0037c85ae0705
Philipp Wolfer <ph.wolfer@gmail.com>
2020-06-20 09:32:13 +0200

    PICARD-1847: Show additional help for AC3, AAC and WAVE options

--- picard/ui/ui_options_tags_compatibility_aac.py
+++ picard/ui/ui_options_tags_compatibility_aac.py
@@ -17,6 +17,12 @@ class Ui_TagsCompatibilityOptionsPage(object):
         self.aac_tags.setObjectName("aac_tags")
         self.verticalLayout = QtWidgets.QVBoxLayout(self.aac_tags)
         self.verticalLayout.setObjectName("verticalLayout")
+        self.info_label = QtWidgets.QLabel(self.aac_tags)
+        self.info_label.setWordWrap(True)
+        self.info_label.setObjectName("info_label")
+        self.verticalLayout.addWidget(self.info_label)
+        spacerItem = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.verticalLayout.addItem(spacerItem)
         self.aac_save_ape = QtWidgets.QRadioButton(self.aac_tags)
         self.aac_save_ape.setChecked(True)
         self.aac_save_ape.setObjectName("aac_save_ape")
@@ -28,8 +34,8 @@ class Ui_TagsCompatibilityOptionsPage(object):
         self.remove_ape_from_aac.setObjectName("remove_ape_from_aac")
         self.verticalLayout.addWidget(self.remove_ape_from_aac)
         self.vboxlayout.addWidget(self.aac_tags)
-        spacerItem = QtWidgets.QSpacerItem(274, 41, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.vboxlayout.addItem(spacerItem)
+        spacerItem1 = QtWidgets.QSpacerItem(274, 41, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.vboxlayout.addItem(spacerItem1)
 
         self.retranslateUi(TagsCompatibilityOptionsPage)
         QtCore.QMetaObject.connectSlotsByName(TagsCompatibilityOptionsPage)
@@ -37,6 +43,7 @@ class Ui_TagsCompatibilityOptionsPage(object):
     def retranslateUi(self, TagsCompatibilityOptionsPage):
         _translate = QtCore.QCoreApplication.translate
         self.aac_tags.setTitle(_("AAC files"))
+        self.info_label.setText(_("Picard can save APEv2 tags to pure AAC files, which by default do not support tagging. APEv2 tags in AAC are supported by some players, but players not supporting AAC files with APEv2 tags can have issues loading and playing those files. To deal with this you can choose whether to save tags to those files."))
         self.aac_save_ape.setText(_("Save APEv2 tags"))
         self.aac_no_tags.setText(_("Do not save tags"))
         self.remove_ape_from_aac.setText(_("Remove APEv2 tags from AAC files"))
--- picard/ui/ui_options_tags_compatibility_ac3.py
+++ picard/ui/ui_options_tags_compatibility_ac3.py
@@ -17,6 +17,12 @@ class Ui_TagsCompatibilityOptionsPage(object):
         self.ac3_files.setObjectName("ac3_files")
         self.verticalLayout_2 = QtWidgets.QVBoxLayout(self.ac3_files)
         self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.info_label = QtWidgets.QLabel(self.ac3_files)
+        self.info_label.setWordWrap(True)
+        self.info_label.setObjectName("info_label")
+        self.verticalLayout_2.addWidget(self.info_label)
+        spacerItem = QtWidgets.QSpacerItem(20, 20, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.verticalLayout_2.addItem(spacerItem)
         self.ac3_save_ape = QtWidgets.QRadioButton(self.ac3_files)
         self.ac3_save_ape.setChecked(True)
         self.ac3_save_ape.setObjectName("ac3_save_ape")
@@ -28,8 +34,8 @@ class Ui_TagsCompatibilityOptionsPage(object):
         self.remove_ape_from_ac3.setObjectName("remove_ape_from_ac3")
         self.verticalLayout_2.addWidget(self.remove_ape_from_ac3)
         self.vboxlayout.addWidget(self.ac3_files)
-        spacerItem = QtWidgets.QSpacerItem(274, 41, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
-        self.vboxlayout.addItem(spacerItem)
+        spacerItem1 = QtWidgets.QSpacerItem(274, 41, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        self.vboxlayout.addItem(spacerItem1)
 
         self.retranslateUi(TagsCompatibilityOptionsPage)
         QtCore.QMetaObject.connectSlotsByName(TagsCompatibilityOptionsPage)
@@ -37,6 +43,7 @@ class Ui_TagsCompatibilityOptionsPage(object):
     def retranslateUi(self, TagsCompatibilityOptionsPage):
         _translate = QtCore.QCoreApplication.translate
         self.ac3_files.setTitle(_("AC3 files"))
+        self.info_label.setText(_("Picard can save APEv2 tags to pure AC3 files, which by default do not support tagging. APEv2 tags in AC3 are supported by some players, but players not supporting AC3 files with APEv2 tags can have issues loading and playing those files. To deal with this you can choose whether to save tags to those files."))
         self.ac3_save_ape.setText(_("Save APEv2 tags"))
         self.ac3_no_tags.setText(_("Do not save tags"))
         self.remove_ape_from_ac3.setText(_("Remove APEv2 tags from AC3 files"))
