metabrainz/picard

commit fac2b37d8cf8dad8e821de541b9c40ee2ae0a0b8

Postpone file moves until tracks have been loaded

picard/tagger.py
@@ -361,7 +361,7 @@ class Tagger(QtWidgets.QApplication):
         """Move `file` to recording `recordingid` on album `albumid`."""
         self.window.setUpdatesEnabled(False)
         album = self.load_album(albumid)
-        file.move(album.unmatched_files)
+        album.run_when_loaded(partial(file.move, album.unmatched_files))
         album.run_when_loaded(partial(album.match_files, [file],
                                       recordingid=recordingid))
         self.window.setUpdatesEnabled(True)

commit ecaf367c685a82fceb73d9573f912b67ad45ff2c

PICARD-1931: Ensure file gets moved to album after load
Fixes the issue introduced in fac2b37 where files did not get moved to the album after a loading error.

picard/album.py
@@ -286,6 +286,10 @@ class Album(DataObject, Item):
             del self._new_metadata
             del self._new_tracks
             self.update()
+            if not self._requests:
+                for func, always in self._after_load_callbacks:
+                    if always:
+                        func()
             return
 
         if self._requests > 0:
@@ -393,7 +397,7 @@ class Album(DataObject, Item):
                 },
                 timeout=3000
             )
-            for func in self._after_load_callbacks:
+            for func, always in self._after_load_callbacks:
                 func()
             self._after_load_callbacks = []
             if self.item.isSelected():
@@ -475,11 +479,11 @@ class Album(DataObject, Item):
             self.id, self._release_request_finished, inc=inc,
             mblogin=require_authentication, priority=priority, refresh=refresh)
 
-    def run_when_loaded(self, func):
+    def run_when_loaded(self, func, always=False):
         if self.loaded:
             func()
         else:
-            self._after_load_callbacks.append(func)
+            self._after_load_callbacks.append((func, always))
 
     def stop_loading(self):
         if self.load_task:

picard/tagger.py
@@ -349,7 +349,7 @@ class Tagger(QtWidgets.QApplication):
     def move_file_to_track(self, file, albumid, recordingid):
         """Move `file` to recording `recordingid` on album `albumid`."""
         album = self.load_album(albumid)
-        album.run_when_loaded(partial(file.move, album.unmatched_files))
+        album.run_when_loaded(partial(file.move, album.unmatched_files), always=True)
         album.run_when_loaded(partial(album.match_files, [file],
                                       recordingid=recordingid))
 