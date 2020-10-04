metabrainz/picard

commit 09c382f9469f475618307fbaf35698705cdca78e

PICARD-259: Execute tagger script when file gets moved to track
This allows using file specific metadata and variables (such as e.g. %_bitrate%) to be used in tagger script.

Also fixes PICARD-23

picard/track.py
@@ -170,7 +170,12 @@ class Track(DataObject, Item):
     def update_file_metadata(self, file):
         if file not in self.linked_files:
             return
-        file.copy_metadata(self.metadata)
+        # Run the scripts for the file to allow usage of
+        # file specific metadata and variables
+        metadata = Metadata(file.metadata)
+        metadata.update(self.orig_metadata)
+        self.run_scripts(metadata)
+        file.copy_metadata(metadata)
         file.metadata['~extension'] = file.orig_metadata['~extension']
         file.update(signal=False)
         self.update()
@@ -188,6 +193,16 @@ class Track(DataObject, Item):
         self.update()
         if self.item.isSelected():
             self.tagger.window.refresh_metadatabox()
+
+    @staticmethod
+    def run_scripts(metadata):
+        for s_name, s_text in enabled_tagger_scripts_texts():
+            parser = ScriptParser()
+            try:
+                parser.eval(s_text, metadata)
+            except ScriptError:
+                log.exception("Failed to run tagger script %s on track", s_name)
+            metadata.strip_whitespace()
 
     def update(self):
         if self.item:
@@ -403,14 +418,7 @@ class NonAlbumTrack(Track):
         self.orig_metadata.copy(m)
         self._customize_metadata()
         run_track_metadata_processors(self.album, m, recording)
-        for s_name, s_text in enabled_tagger_scripts_texts():
-            parser = ScriptParser()
-            try:
-                parser.eval(s_text, m)
-            except ScriptError:
-                log.exception("Failed to run tagger script %s on track", s_name)
-            m.strip_whitespace()
-
+        self.run_scripts(m)
         self.loaded = True
         self.status = None
         if self.callback:

commit 6b723d5f097860e027aadca7e3f3759eb85caa24

PICARD-259: Fix clear_existing_tags
This broke in 09c382f when scripting for file metadata was enabled.

picard/file.py
@@ -87,6 +87,9 @@ from picard.util.tags import PRESERVED_TAGS
 from picard.ui.item import Item
 
 
+FILE_INFO_TAGS = ('~bitrate', '~sample_rate', '~channels', '~bits_per_sample', '~format')
+
+
 class File(QtCore.QObject, Item):
 
     metadata_images_changed = QtCore.pyqtSignal()
@@ -354,8 +357,7 @@ class File(QtCore.QObject, Item):
             self.base_filename = os.path.basename(new_filename)
             length = self.orig_metadata.length
             temp_info = {}
-            for info in ('~bitrate', '~sample_rate', '~channels',
-                         '~bits_per_sample', '~format'):
+            for info in FILE_INFO_TAGS:
                 temp_info[info] = self.orig_metadata[info]
             # Data is copied from New to Original because New may be
             # a subclass to handle id3v23

picard/track.py
@@ -55,6 +55,7 @@ from picard.const import (
 )
 from picard.dataobj import DataObject
 from picard.file import (
+    FILE_INFO_TAGS,
     run_file_post_addition_to_track_processors,
     run_file_post_removal_from_track_processors,
 )
@@ -173,7 +174,13 @@ class Track(DataObject, Item):
             return
         # Run the scripts for the file to allow usage of
         # file specific metadata and variables
-        metadata = Metadata(file.metadata)
+        metadata = Metadata()
+        if config.setting["clear_existing_tags"]:
+            # Only use technical variables from existing file, but ignore actual tags
+            for info in FILE_INFO_TAGS:
+                metadata[info] = file.metadata[info]
+        else:
+            metadata.update(file.metadata)
         metadata.update(self.orig_metadata)
         self.run_scripts(metadata)
         # Apply changes to the track's metadata done manually after the scripts ran