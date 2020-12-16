metabrainz/picard

commit b0ee61568c928661f3b5d1978ccad8562327436e

Minor code simplification
Use yield from in iterfiles, removed redundant logic when calling album.match_files

picard/album.py
@@ -142,11 +142,9 @@ class Album(DataObject, Item):
 
     def iterfiles(self, save=False):
         for track in self.tracks:
-            for file in track.iterfiles():
-                yield file
+            yield from track.iterfiles()
         if not save:
-            for file in self.unmatched_files.iterfiles():
-                yield file
+            yield from self.unmatched_files.iterfiles()
 
     def enable_update_metadata_images(self, enabled):
         self.update_metadata_images_enabled = enabled

picard/cluster.py
@@ -420,8 +420,7 @@ class ClusterList(list, Item):
 
     def iterfiles(self, save=False):
         for cluster in self:
-            for file in cluster.iterfiles(save):
-                yield file
+            yield from cluster.iterfiles(save)
 
     def can_save(self):
         return len(self) > 0

picard/tagger.py
@@ -336,11 +336,7 @@ class Tagger(QtWidgets.QApplication):
         """Move `files` to tracks on album `albumid`."""
         if album is None:
             album = self.load_album(albumid)
-        if album.loaded:
-            album.match_files(files)
-        else:
-            for file in list(files):
-                file.move(album.unmatched_files)
+        album.match_files(files)
 
     def move_file_to_album(self, file, albumid):
         """Move `file` to a track on album `albumid`."""

commit 5f80f68005b8f79237ca76aa41f623991c026c2d

PICARD-2070: Fixed lookup only moving half the files
Iterating over files to then move them can change the original list. Iterate over a copy. This is a regression from b0ee615

picard/album.py
@@ -577,7 +577,7 @@ class Album(DataObject, Item):
             for file, target in moves:
                 file.move(target)
         else:
-            for file in files:
+            for file in list(files):
                 file.move(self.unmatched_files)
 
     def can_save(self):