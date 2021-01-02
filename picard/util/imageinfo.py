0b7522f84d334517090574fc1ae79248db782362
Philipp Wolfer <ph.wolfer@gmail.com>
2021-01-02 23:50:24 +0100

    Restructured util.imageinfo

--- picard/util/imageinfo.py
+++ picard/util/imageinfo.py
@@ -44,7 +44,7 @@ class UnexpectedError(IdentificationError):
 
 
 def identify(data):
-    """Parse data for jpg, gif, png, webp and pdf metadata
+    """Parse data for jpg, gif, png, webp, tiff and pdf metadata
     If successfully recognized, it returns a tuple with:
         - width
         - height
@@ -69,133 +69,28 @@ def identify(data):
 
     # http://en.wikipedia.org/wiki/Graphics_Interchange_Format
     if data[:6] in (b'GIF87a', b'GIF89a'):
-        w, h = struct.unpack('<HH', data[6:10])
-        mime = 'image/gif'
-        extension = '.gif'
+        w, h, mime, extension = _gif_read(data)
 
     # http://en.wikipedia.org/wiki/Portable_Network_Graphics
     # http://www.w3.org/TR/PNG/#11IHDR
     elif data[:8] == b'\x89PNG\x0D\x0A\x1A\x0A' and data[12:16] == b'IHDR':
-        w, h = struct.unpack('>LL', data[16:24])
-        mime = 'image/png'
-        extension = '.png'
+        w, h, mime, extension = _png_read(data)
 
     # http://en.wikipedia.org/wiki/JPEG
     elif data[:2] == b'\xFF\xD8':  # Start Of Image (SOI) marker
-        jpeg = BytesIO(data)
-        # skip SOI
-        jpeg.read(2)
-        b = jpeg.read(1)
-        try:
-            while b and ord(b) != 0xDA:  # Start Of Scan (SOS)
-                while ord(b) != 0xFF:
-                    b = jpeg.read(1)
-                while ord(b) == 0xFF:
-                    b = jpeg.read(1)
-                if ord(b) in (0xC0, 0xC1, 0xC2, 0xC5, 0xC6, 0xC7,
-                              0xC9, 0xCA, 0xCB, 0xCD, 0xCE, 0xCF):
-                    jpeg.read(2)  # parameter length (2 bytes)
-                    jpeg.read(1)  # data precision (1 byte)
-                    h, w = struct.unpack('>HH', jpeg.read(4))
-                    mime = 'image/jpeg'
-                    extension = '.jpg'
-                    break
-                else:
-                    # read 2 bytes as integer
-                    length = int(struct.unpack('>H', jpeg.read(2))[0])
-                    # skip data
-                    jpeg.read(length - 2)
-                b = jpeg.read(1)
-        except struct.error:
-            pass
-        except ValueError:
-            pass
+        w, h, mime, extension = _jpeg_read(data)
 
     # WebP
     elif data[:4] == b'RIFF' and data[8:12] == b'WEBP':
-        # See https://developers.google.com/speed/webp/docs/riff_container
-        format = data[12:16]
-        # Simple File Format (Lossy)
-        if format == b'VP8 ':
-            # See https://tools.ietf.org/html/rfc6386#section-9.1
-            index = data.find(b'\x9d\x01\x2a')
-            if index != -1:
-                if len(data) < index + 7:
-                    raise NotEnoughData('Not enough data for WebP VP8')
-                w, h = struct.unpack('<HH', data[index + 3:index + 7])
-                # Width and height are encoded as 14 bit integers, ignore the first 2 bits
-                w &= 0x3fff
-                h &= 0x3fff
-            else:
-                w, h = 0, 0
-        # Simple File Format (Lossless)
-        elif format == b'VP8L':
-            if len(data) < 25:
-                raise NotEnoughData('Not enough data for WebP VP8L')
-            reader = LSBBitReader(BytesIO(data[21:25]))
-            w = reader.bits(14) + 1
-            h = reader.bits(14) + 1
-        # Extended File Format
-        elif format == b'VP8X':
-            if len(data) < 30:
-                raise NotEnoughData('Not enough data for WebP VP8X')
-            reader = LSBBitReader(BytesIO(data[24:30]))
-            w = reader.bits(24) + 1
-            h = reader.bits(24) + 1
-        else:
-            h, w = 0, 0
-        mime = 'image/webp'
-        extension = '.webp'
+        w, h, mime, extension = _webp_read(data)
 
     # TIFF
     elif data[:4] == b'II*\x00' or data[:4] == b'MM\x00*':
-        TIFF_BYTE_ORDER_LSB = b'II'
-        TIFF_BYTE_ORDER_MSB = b'MM'
-        TIFF_TAG_IMAGE_LENGTH = 257
-        TIFF_TAG_IMAGE_WIDTH = 256
-        TIFF_TYPE_SHORT = 3
-        TIFF_TYPE_LONG = 4
-
-        def read_value(type, data):
-            if type == TIFF_TYPE_LONG:
-                value = data[:4]
-                format = order + 'I'
-            elif type == TIFF_TYPE_SHORT:
-                value = data[:2]
-                format = order + 'H'
-            return struct.unpack(format, value)[0]
-
-        byte_order = data[:2]
-        if byte_order == TIFF_BYTE_ORDER_LSB:
-            order = '<'
-        elif byte_order == TIFF_BYTE_ORDER_MSB:
-            order = '>'
-        try:
-            offset, = struct.unpack(order + 'I', data[4:8])
-            entry_count, = struct.unpack(order + 'H', data[offset:offset + 2])
-            pos = offset + 2
-            for i in range(entry_count):
-                field = data[pos:pos + 12]
-                tag, type = struct.unpack(order + 'HH', field[:4])
-                if tag == TIFF_TAG_IMAGE_WIDTH:
-                    w = read_value(type, field[8:12])
-                elif tag == TIFF_TAG_IMAGE_LENGTH:
-                    h = read_value(type, field[8:12])
-                if h > -1 and w > -1:  # Found both width and height, abort
-                    break
-                pos += 12
-            else:
-                w, h = 0, 0
-        except struct.error:
-            w, h = 0, 0
-        mime = 'image/tiff'
-        extension = '.tiff'
+        w, h, mime, extension = _tiff_read(data)
 
     # PDF
     elif data[:4] == b'%PDF':
-        h, w = 0, 0
-        mime = 'application/pdf'
-        extension = '.pdf'
+        w, h, mime, extension = _pdf_read(data)
 
     else:
         raise UnrecognizedFormat('Unrecognized image data')
@@ -206,3 +101,131 @@ def identify(data):
                               % (w, h, mime, extension))
 
     return (int(w), int(h), mime, extension, datalen)
+
+
+def _gif_read(data):
+    w, h = struct.unpack('<HH', data[6:10])
+    return (w, h, 'image/gif', '.gif')
+
+
+def _png_read(data):
+    w, h = struct.unpack('>LL', data[16:24])
+    return (w, h, 'image/png', '.png')
+
+
+def _jpeg_read(data):
+    jpeg = BytesIO(data)
+    # skip SOI
+    jpeg.read(2)
+    b = jpeg.read(1)
+    try:
+        while b and ord(b) != 0xDA:  # Start Of Scan (SOS)
+            while ord(b) != 0xFF:
+                b = jpeg.read(1)
+            while ord(b) == 0xFF:
+                b = jpeg.read(1)
+            if ord(b) in (0xC0, 0xC1, 0xC2, 0xC5, 0xC6, 0xC7,
+                            0xC9, 0xCA, 0xCB, 0xCD, 0xCE, 0xCF):
+                jpeg.read(2)  # parameter length (2 bytes)
+                jpeg.read(1)  # data precision (1 byte)
+                h, w = struct.unpack('>HH', jpeg.read(4))
+                break
+            else:
+                # read 2 bytes as integer
+                length = int(struct.unpack('>H', jpeg.read(2))[0])
+                # skip data
+                jpeg.read(length - 2)
+            b = jpeg.read(1)
+    except struct.error:
+        pass
+    except ValueError:
+        pass
+    return (w, h, 'image/jpeg', '.jpg')
+
+
+def _webp_read(data):
+    # See https://developers.google.com/speed/webp/docs/riff_container
+    format = data[12:16]
+    # Simple File Format (Lossy)
+    if format == b'VP8 ':
+        # See https://tools.ietf.org/html/rfc6386#section-9.1
+        index = data.find(b'\x9d\x01\x2a')
+        if index != -1:
+            if len(data) < index + 7:
+                raise NotEnoughData('Not enough data for WebP VP8')
+            w, h = struct.unpack('<HH', data[index + 3:index + 7])
+            # Width and height are encoded as 14 bit integers, ignore the first 2 bits
+            w &= 0x3fff
+            h &= 0x3fff
+        else:
+            w, h = 0, 0
+    # Simple File Format (Lossless)
+    elif format == b'VP8L':
+        if len(data) < 25:
+            raise NotEnoughData('Not enough data for WebP VP8L')
+        reader = LSBBitReader(BytesIO(data[21:25]))
+        w = reader.bits(14) + 1
+        h = reader.bits(14) + 1
+    # Extended File Format
+    elif format == b'VP8X':
+        if len(data) < 30:
+            raise NotEnoughData('Not enough data for WebP VP8X')
+        reader = LSBBitReader(BytesIO(data[24:30]))
+        w = reader.bits(24) + 1
+        h = reader.bits(24) + 1
+    else:
+        h, w = 0, 0
+    return (w, h, 'image/webp', '.webp')
+
+
+TIFF_BYTE_ORDER_LSB = b'II'
+TIFF_BYTE_ORDER_MSB = b'MM'
+TIFF_TAG_IMAGE_LENGTH = 257
+TIFF_TAG_IMAGE_WIDTH = 256
+TIFF_TYPE_SHORT = 3
+TIFF_TYPE_LONG = 4
+
+
+def _tiff_read_value(type, order, data):
+    if type == TIFF_TYPE_LONG:
+        value = data[:4]
+        format = order + 'I'
+    elif type == TIFF_TYPE_SHORT:
+        value = data[:2]
+        format = order + 'H'
+    else:
+        raise UnexpectedError('TIFF: unexpected field type %s' % type)
+    return struct.unpack(format, value)[0]
+
+
+def _tiff_read(data):
+    # See https://www.adobe.io/content/dam/udp/en/open/standards/tiff/TIFF6.pdf
+    w, h = 0, 0
+    byte_order = data[:2]
+    if byte_order == TIFF_BYTE_ORDER_LSB:
+        order = '<'
+    elif byte_order == TIFF_BYTE_ORDER_MSB:
+        order = '>'
+    else:
+        raise UnexpectedError('TIFF: unexpected byte order %r' % byte_order)
+    try:
+        offset, = struct.unpack(order + 'I', data[4:8])
+        entry_count, = struct.unpack(order + 'H', data[offset:offset + 2])
+        pos = offset + 2
+        for i in range(entry_count):
+            field = data[pos:pos + 12]
+            tag, type = struct.unpack(order + 'HH', field[:4])
+            if tag == TIFF_TAG_IMAGE_WIDTH:
+                w = _tiff_read_value(type, order, field[8:12])
+            elif tag == TIFF_TAG_IMAGE_LENGTH:
+                h = _tiff_read_value(type, order, field[8:12])
+            if w and h:  # Found both width and height, abort
+                break
+            pos += 12
+    except struct.error:
+        pass
+    return (w, h, 'image/tiff', '.tiff')
+
+
+def _pdf_read(data):
+    return (0, 0, 'application/pdf', '.pdf')
