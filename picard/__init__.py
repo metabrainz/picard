b1aafb9df32615591aeebbb8dc352b217946342d
Philipp Wolfer <ph.wolfer@gmail.com>
2021-01-05 15:20:40 +0100

    Release 2.5.6

--- picard/__init__.py
+++ picard/__init__.py
@@ -41,7 +41,7 @@ PICARD_APP_NAME = "Picard"
 PICARD_DISPLAY_NAME = "MusicBrainz Picard"
 PICARD_APP_ID = "org.musicbrainz.Picard"
 PICARD_DESKTOP_NAME = PICARD_APP_ID + ".desktop"
-PICARD_VERSION = Version(2, 5, 6, 'dev', 2)
+PICARD_VERSION = Version(2, 5, 6, 'final', 0)
 
 
 # optional build version
