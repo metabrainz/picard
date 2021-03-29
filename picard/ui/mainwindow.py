2a0b0fcdebbcc2bc956c2b132774dc9576428b62
Philipp Wolfer <ph.wolfer@gmail.com>
2021-03-29 09:40:06 +0200

    PICARD-2164: Make "Search for similar tracks" available globally
    This is needed to make the shortcut accessible.

--- picard/ui/mainwindow.py
+++ picard/ui/mainwindow.py
@@ -787,6 +787,7 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
         menu.addAction(self.analyze_action)
         menu.addAction(self.cluster_action)
         menu.addAction(self.browser_lookup_action)
+        menu.addAction(self.track_search_action)
         menu.addSeparator()
         menu.addAction(self.generate_fingerprints_action)
         menu.addAction(self.tags_from_filenames_action)
@@ -1187,6 +1188,7 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
         single = self.selected_objects[0] if len(self.selected_objects) == 1 else None
         can_view_info = bool(single and single.can_view_info())
         can_browser_lookup = bool(single and single.can_browser_lookup())
+        is_file = bool(single and isinstance(single, (File, Track)))
         have_files = bool(self.tagger.get_files_from_objects(self.selected_objects))
         have_objects = bool(self.selected_objects)
         for obj in self.selected_objects:
@@ -1218,7 +1220,7 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
         self.cut_action.setEnabled(have_objects)
         files = self.get_selected_or_unmatched_files()
         self.tags_from_filenames_action.setEnabled(bool(files))
-        self.track_search_action.setEnabled(have_objects)
+        self.track_search_action.setEnabled(is_file)
 
     def update_selection(self, objects=None, new_selection=True, drop_album_caches=False):
         if self.ignore_selection_changes:
