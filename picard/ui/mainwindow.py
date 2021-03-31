4c84600b24351a5eebe78b9ee6b7c5c58a3c881f
Philipp Wolfer <ph.wolfer@gmail.com>
2021-03-31 19:59:26 +0200

    PICARD-2173: Disable "Search for similar tracks" until items get selected

--- picard/ui/mainwindow.py
+++ picard/ui/mainwindow.py
@@ -549,8 +549,9 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
 
         self.track_search_action = QtWidgets.QAction(icontheme.lookup('system-search'), _("Search for similar tracks..."), self)
         self.track_search_action.setStatusTip(_("View similar tracks and optionally choose a different release"))
-        self.track_search_action.triggered.connect(self.show_more_tracks)
+        self.track_search_action.setEnabled(False)
         self.track_search_action.setShortcut(QtGui.QKeySequence(_("Ctrl+T")))
+        self.track_search_action.triggered.connect(self.show_more_tracks)
 
         self.show_file_browser_action = QtWidgets.QAction(_("File &Browser"), self)
         self.show_file_browser_action.setCheckable(True)
