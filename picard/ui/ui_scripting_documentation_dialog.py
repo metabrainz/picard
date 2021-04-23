99f829493cdc9e9db92e8a01dcf9bb3c70431db8
Bob Swift <bswift@rsds.ca>
2021-04-23 12:22:55 -0600

    Use custom widget for documentation display from scriping options page

--- picard/ui/ui_scripting_documentation_dialog.py
+++ picard/ui/ui_scripting_documentation_dialog.py
@@ -3,7 +3,9 @@
 # Automatically generated - don't edit.
 # Use `python setup.py build_ui` to update it.
 
+
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 
 class Ui_ScriptingDocumentationDialog(object):
     def setupUi(self, ScriptingDocumentationDialog):
@@ -15,32 +17,16 @@ class Ui_ScriptingDocumentationDialog(object):
         sizePolicy.setHeightForWidth(ScriptingDocumentationDialog.sizePolicy().hasHeightForWidth())
         ScriptingDocumentationDialog.setSizePolicy(sizePolicy)
         ScriptingDocumentationDialog.setModal(False)
-        self.gridLayout = QtWidgets.QGridLayout(ScriptingDocumentationDialog)
-        self.gridLayout.setObjectName("gridLayout")
-        self.textBrowser = QtWidgets.QTextBrowser(ScriptingDocumentationDialog)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        sizePolicy.setHorizontalStretch(1)
-        sizePolicy.setVerticalStretch(1)
-        sizePolicy.setHeightForWidth(self.textBrowser.sizePolicy().hasHeightForWidth())
-        self.textBrowser.setSizePolicy(sizePolicy)
-        self.textBrowser.setOpenExternalLinks(True)
-        self.textBrowser.setObjectName("textBrowser")
-        self.gridLayout.addWidget(self.textBrowser, 0, 0, 1, 2)
-        self.scripting_doc_link = QtWidgets.QLabel(ScriptingDocumentationDialog)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.scripting_doc_link.sizePolicy().hasHeightForWidth())
-        self.scripting_doc_link.setSizePolicy(sizePolicy)
-        self.scripting_doc_link.setWordWrap(True)
-        self.scripting_doc_link.setOpenExternalLinks(True)
-        self.scripting_doc_link.setObjectName("scripting_doc_link")
-        self.gridLayout.addWidget(self.scripting_doc_link, 1, 0, 1, 1)
+        self.verticalLayout = QtWidgets.QVBoxLayout(ScriptingDocumentationDialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.documentation_layout = QtWidgets.QVBoxLayout()
+        self.documentation_layout.setObjectName("documentation_layout")
+        self.verticalLayout.addLayout(self.documentation_layout)
         self.buttonBox = QtWidgets.QDialogButtonBox(ScriptingDocumentationDialog)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Close)
         self.buttonBox.setObjectName("buttonBox")
-        self.gridLayout.addWidget(self.buttonBox, 1, 1, 1, 1)
+        self.verticalLayout.addWidget(self.buttonBox)
 
         self.retranslateUi(ScriptingDocumentationDialog)
         QtCore.QMetaObject.connectSlotsByName(ScriptingDocumentationDialog)
@@ -48,5 +34,3 @@ class Ui_ScriptingDocumentationDialog(object):
     def retranslateUi(self, ScriptingDocumentationDialog):
         _translate = QtCore.QCoreApplication.translate
         ScriptingDocumentationDialog.setWindowTitle(_("Scripting Documentation"))
-        self.scripting_doc_link.setText(_("Open documentation in browser"))
-
