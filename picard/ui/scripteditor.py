2fb82fdc5c653848f9cb8f0ecc41be7a6acfdd2d
Philipp Wolfer <ph.wolfer@gmail.com>
2021-05-28 11:41:59 +0200

    PICARD-2179: Use standard dialog button box for script editor

--- picard/ui/scripteditor.py
+++ picard/ui/scripteditor.py
@@ -301,18 +301,21 @@ class ScriptEditorDialog(PicardDialog):
         self.ui.setupUi(self)
         self.make_menu()
 
-        # Button tooltips
-        self.ui.file_naming_editor_close.setToolTip(self.close_action.toolTip())
-        self.ui.file_naming_editor_save.setToolTip(self.save_action.toolTip())
-        self.ui.file_naming_editor_reset.setToolTip(self.reset_action.toolTip())
-
         self.ui.label.setWordWrap(False)
 
         self.installEventFilter(self)
 
-        self.ui.file_naming_editor_save.clicked.connect(self.save_script)
-        self.ui.file_naming_editor_close.clicked.connect(self.close_window)
-        self.ui.file_naming_editor_reset.clicked.connect(self.reset_script)
+        # Dialog buttons
+        self.reset_button = QtWidgets.QPushButton(_('Revert'))
+        self.reset_button.setToolTip(self.reset_action.toolTip())
+        self.reset_button.clicked.connect(self.reset_script)
+        self.ui.buttonbox.addButton(self.reset_button, QtWidgets.QDialogButtonBox.ActionRole)
+        self.save_button = self.ui.buttonbox.addButton(QtWidgets.QDialogButtonBox.Save)
+        self.save_button.setToolTip(self.save_action.toolTip())
+        self.ui.buttonbox.accepted.connect(self.save_script)
+        self.close_button = self.ui.buttonbox.addButton(QtWidgets.QDialogButtonBox.Close)
+        self.close_button.setToolTip(self.close_action.toolTip())
+        self.ui.buttonbox.rejected.connect(self.close_window)
 
         self.ui.file_naming_format.setEnabled(True)
 
@@ -345,11 +348,6 @@ class ScriptEditorDialog(PicardDialog):
         """
         config = get_config()
         main_menu = QtWidgets.QMenuBar()
-        base_font = self.ui.file_naming_editor_close.font()
-        main_menu.setStyleSheet(
-            "QMenuBar { font-family: %s; font-size: %upt; }" %
-            (base_font.family(), base_font.pointSize())
-        )
 
         # File menu settings
         file_menu = main_menu.addMenu(_('&File'))
@@ -705,8 +703,8 @@ class ScriptEditorDialog(PicardDialog):
 
         # Buttons
         self.ui.file_naming_format.setReadOnly(readonly)
-        self.ui.file_naming_editor_save.setEnabled(save_enabled and not readonly)
-        self.ui.file_naming_editor_reset.setEnabled(not readonly)
+        self.save_button.setEnabled(save_enabled and not readonly)
+        self.reset_button.setEnabled(not readonly)
 
         # Menu items
         self.save_action.setEnabled(save_enabled and not readonly)
