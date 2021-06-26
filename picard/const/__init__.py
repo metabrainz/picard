e01b97844132e01c7f5463eef91653cc064c369e
Bob Swift <bswift@rsds.ca>
2021-06-26 12:35:12 -0600

    Correct the url to the help page

--- picard/const/__init__.py
+++ picard/const/__init__.py
@@ -88,7 +88,7 @@ PICARD_URLS = {
     'doc_scripting':           DOCS_BASE_URL + '/extending/scripting.html',
     'doc_tags_from_filenames': DOCS_BASE_URL + '/usage/tags_from_file_names.html',
     'doc_naming_script_edit':  DOCS_BASE_URL + '/config/options_filerenaming_editor.html',
-    'doc_profile_edit':        DOCS_BASE_URL + '/config/options_profile_editor.html',
+    'doc_profile_edit':        DOCS_BASE_URL + '/config/usage/user_profiles.html',
     'doc_cover_art_types':     "https://musicbrainz.org/doc/Cover_Art/Types",
     'plugins':                 "https://picard.musicbrainz.org/plugins/",
     'forum':                   "https://community.metabrainz.org/c/picard",
