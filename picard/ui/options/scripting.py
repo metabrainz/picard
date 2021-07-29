46957c54426dc2d9becad3e316e2d2e06fd62da8
Bob Swift <bswift@rsds.ca>
2021-07-29 13:42:58 -0600

    Add basic tagging script import/export functions:
    - Add buttons to UI and rearrange buttons
    - Make all button sizes consistent within the UI (using QToolButton)
    - Add import and export functions
    - Add missing button tooltips

--- picard/ui/options/scripting.py
+++ picard/ui/options/scripting.py
@@ -28,8 +28,14 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 
-from PyQt5 import QtCore
+import os
 
+from PyQt5 import (
+    QtCore,
+    QtWidgets,
+)
+
+from picard import log
 from picard.config import (
     BoolOption,
     IntOption,
@@ -38,6 +44,10 @@ from picard.config import (
 )
 from picard.const.sys import IS_MACOS
 from picard.script import ScriptParser
+from picard.script.serializer import (
+    PicardScript,
+    ScriptImportError,
+)
 
 from picard.ui import (
     PicardDialog,
@@ -58,6 +68,10 @@ from picard.ui.widgets.scriptlistwidget import ScriptListWidgetItem
 
 
 class ScriptCheckError(OptionsCheckError):
+    pass
+
+
+class ScriptFileError(OptionsCheckError):
     pass
 
 
@@ -97,6 +111,9 @@ class ScriptingOptionsPage(OptionsPage):
         IntOption("persist", "last_selected_script_pos", 0),
     ]
 
+    default_script_directory = os.path.normpath(QtCore.QStandardPaths.writableLocation(QtCore.QStandardPaths.DocumentsLocation))
+    default_script_filename = "picard_tagging_script.ptsp"
+
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_ScriptingOptionsPage()
@@ -106,9 +123,140 @@ class ScriptingOptionsPage(OptionsPage):
         self.move_view = MoveableListView(self.ui.script_list, self.ui.move_up_button,
                                           self.ui.move_down_button)
         self.ui.scripting_documentation_button.clicked.connect(self.show_scripting_documentation)
+        self.ui.scripting_documentation_button.setToolTip(_("Show scripting documentation in new window."))
+
+        self.ui.import_button.clicked.connect(self.import_script)
+        self.ui.import_button.setToolTip(_("Import a script file as a new script."))
+
+        self.ui.export_button.clicked.connect(self.export_script)
+        self.ui.export_button.setToolTip(_("Export the current script to a file."))
+
+        self.FILE_TYPE_ALL = _("All Files") + " (*)"
+        self.FILE_TYPE_SCRIPT = _("Picard Script Files") + " (*.pts *.txt)"
+        self.FILE_TYPE_PACKAGE = _("Picard Tagging Script Package") + " (*.ptsp *.yaml)"
 
     def show_scripting_documentation(self):
         ScriptingDocumentationDialog.show_instance(parent=self)
+
+    def output_error(self, title, fmt, filename, msg):
+        """Log error and display error message dialog.
+
+        Args:
+            title (str): Title to display on the error dialog box
+            fmt (str): Format for the error type being displayed
+            filename (str): Name of the file being imported or exported
+            msg (str): Error message to display
+        """
+        log.error(fmt, filename, msg)
+        error_message = _(fmt) % (filename, _(msg))
+        self.display_error(ScriptFileError(_(title), error_message))
+
+    def output_file_error(self, fmt, filename, msg):
+        """Log file error and display error message dialog.
+
+        Args:
+            fmt (str): Format for the error type being displayed
+            filename (str): Name of the file being imported or exported
+            msg (str): Error message to display
+        """
+        self.output_error(_("File Error"), fmt, filename, msg)
+
+    def import_script(self):
+        """Import from an external text file to a new script. Import can be either a plain text script or
+        a Picard script package.
+        """
+        FILE_ERROR_IMPORT = N_('Error importing "%s". %s.')
+        FILE_ERROR_DECODE = N_('Error decoding "%s". %s.')
+
+        dialog_title = _("Import Script File")
+        dialog_file_types = self._get_dialog_filetypes()
+        options = QtWidgets.QFileDialog.Options()
+        options |= QtWidgets.QFileDialog.DontUseNativeDialog
+        filename, file_type = QtWidgets.QFileDialog.getOpenFileName(self, dialog_title, self.default_script_directory, dialog_file_types, options=options)
+        if filename:
+            log.debug('Importing tagging script file: %s' % filename)
+            try:
+                with open(filename, 'r', encoding='utf8') as i_file:
+                    file_content = i_file.read()
+            except OSError as error:
+                self.output_file_error(FILE_ERROR_IMPORT, filename, error.strerror)
+                return
+            if not file_content.strip():
+                self.output_file_error(FILE_ERROR_IMPORT, filename, _('The file was empty'))
+                return
+            if file_type == self.FILE_TYPE_PACKAGE:
+                try:
+                    script_item = PicardScript().create_from_yaml(file_content)
+                except ScriptImportError as error:
+                    self.output_file_error(FILE_ERROR_DECODE, filename, error)
+                    return
+            else:
+                script_item = PicardScript(
+                    title=_("Imported from %s") % filename,
+                    script=file_content.strip()
+                )
+            title = _("%s (imported)") % script_item["title"]
+            list_item = ScriptListWidgetItem(title, False, script_item["script"])
+            self.ui.script_list.addItem(list_item)
+            self.ui.script_list.setCurrentRow(self.ui.script_list.count() - 1)
+
+    def export_script(self):
+        """Export the current script to an external file. Export can be either as a plain text
+        script or a naming script package.
+        """
+        FILE_ERROR_EXPORT = N_('Error exporting file "%s". %s.')
+
+        items = self.ui.script_list.selectedItems()
+        if items:
+            item = items[0]
+            script_text = item.script
+            script_title = item.name
+        else:
+            script_text = ""
+            script_title = ""
+
+        if script_text:
+            script_item = PicardScript(title=script_title, script=script_text)
+            default_path = os.path.normpath(os.path.join(self.default_script_directory, self.default_script_filename))
+            dialog_title = _("Export Script File")
+            dialog_file_types = self._get_dialog_filetypes()
+            options = QtWidgets.QFileDialog.Options()
+            options |= QtWidgets.QFileDialog.DontUseNativeDialog
+            filename, file_type = QtWidgets.QFileDialog.getSaveFileName(self, dialog_title, default_path, dialog_file_types, options=options)
+            if filename:
+                # Fix issue where Qt may set the extension twice
+                (name, ext) = os.path.splitext(filename)
+                if ext and str(name).endswith('.' + ext):
+                    filename = name
+                log.debug('Exporting naming script file: %s' % filename)
+                if file_type == self.FILE_TYPE_PACKAGE:
+                    script_text = script_item.to_yaml()
+                try:
+                    with open(filename, 'w', encoding='utf8') as o_file:
+                        o_file.write(script_text)
+                except OSError as error:
+                    self.output_file_error(FILE_ERROR_EXPORT, filename, error.strerror)
+                else:
+                    dialog = QtWidgets.QMessageBox(
+                        QtWidgets.QMessageBox.Information,
+                        _("Export Script"),
+                        _("Script successfully exported to %s") % filename,
+                        QtWidgets.QMessageBox.Ok,
+                        self
+                    )
+                    dialog.exec_()
+
+    def _get_dialog_filetypes(self):
+        """Helper function to build file type string used in the file dialogs.
+
+        Returns:
+            str: File type selection string
+        """
+        return ";;".join((
+            self.FILE_TYPE_PACKAGE,
+            self.FILE_TYPE_SCRIPT,
+            self.FILE_TYPE_ALL,
+        ))
 
     def enable_tagger_scripts_toggled(self, on):
         if on and self.ui.script_list.count() == 0:
@@ -121,9 +269,11 @@ class ScriptingOptionsPage(OptionsPage):
             self.ui.tagger_script.setEnabled(True)
             self.ui.tagger_script.setText(item.script)
             self.ui.tagger_script.setFocus(QtCore.Qt.OtherFocusReason)
+            self.ui.export_button.setEnabled(True)
         else:
             self.ui.tagger_script.setEnabled(False)
             self.ui.tagger_script.setText("")
+            self.ui.export_button.setEnabled(False)
 
     def live_update_and_check(self):
         items = self.ui.script_list.selectedItems()
