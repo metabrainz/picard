d9d748e423c37b18095d8340573c1a2b49253677
Philipp Wolfer <ph.wolfer@gmail.com>
2021-08-23 17:25:01 +0200

    Skip bundling essentia-extractor in macOS builds for now
    Workaround to fix macOS notarization until we get an essentia extractor build with macOS SDK 10.9 or later

--- scripts/package/macos-setup.sh
+++ scripts/package/macos-setup.sh
@@ -24,10 +24,14 @@ fi
 cp "$HOME/libdiscid/lib/libdiscid.0.dylib" .
 
 # Install fpcalc
-wget "https://github.com/acoustid/chromaprint/releases/download/v$FPCALC_VERSION/chromaprint-fpcalc-$FPCALC_VERSION-macos-x86_64.tar.gz"
-tar -xf "chromaprint-fpcalc-$FPCALC_VERSION-macos-x86_64.tar.gz"
-cp "chromaprint-fpcalc-$FPCALC_VERSION-macos-x86_64/fpcalc" .
+if [ -n "$FPCALC_VERSION" ]; then
+  wget "https://github.com/acoustid/chromaprint/releases/download/v$FPCALC_VERSION/chromaprint-fpcalc-$FPCALC_VERSION-macos-x86_64.tar.gz"
+  tar -xf "chromaprint-fpcalc-$FPCALC_VERSION-macos-x86_64.tar.gz"
+  cp "chromaprint-fpcalc-$FPCALC_VERSION-macos-x86_64/fpcalc" .
+fi
 
 # Install AcousticBrainz extractor
-wget "https://ftp.acousticbrainz.org/pub/acousticbrainz/essentia-extractor-$ABEXTRACTOR_VERSION-osx.tar.gz"
-tar -xf "essentia-extractor-$ABEXTRACTOR_VERSION-osx.tar.gz"
+if [ -n "$ABEXTRACTOR_VERSION" ]; then
+  wget "https://ftp.acousticbrainz.org/pub/acousticbrainz/essentia-extractor-$ABEXTRACTOR_VERSION-osx.tar.gz"
+  tar -xf "essentia-extractor-$ABEXTRACTOR_VERSION-osx.tar.gz"
+fi
