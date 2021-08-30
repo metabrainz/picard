1dc50490e93a7d9cb07e8070d0d7ecb8fc4f7e61
Bob Swift <bswift@rsds.ca>
2021-08-30 15:39:18 -0600

    Add Options page to allow removing unused settings from INI file.

---
+++ picard/ui/options/maintenance.py
@@ -0,0 +1,168 @@
+# -*- coding: utf-8 -*-
+#
+# Picard, the next-generation MusicBrainz tagger
+#
+# Copyright (C) 2021 Bob Swift
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+
+from PyQt5 import (
+    QtCore,
+    QtWidgets,
+)
+
+from picard import log
+from picard.config import (
+    Option,
+    get_config,
+)
+
+from picard.ui.options import (
+    OptionsPage,
+    register_options_page,
+)
+from picard.ui.ui_options_maintenance import Ui_MaintenanceOptionsPage
+
+
+class MaintenanceOptionsPage(OptionsPage):
+
+    NAME = "maintenance"
+    TITLE = N_("Maintenance")
+    PARENT = None
+    SORT_ORDER = 99
+    ACTIVE = True
+    HELP_URL = '/config/options_maintenance.html'
+
+    options = []
+
+    INSTRUCTIONS = N_(
+        "This allows you to remove unused option settings from the configuration.\n\n"
+        "Settings that are found in the configuration that do not appear on any option "
+        "settings page will be listed below. If your configuration does not contain any "
+        "unused option settings, then the list will be empty.\n\n"
+        "Note that unused option settings could come from plugins that have been removed, "
+        "so please be careful to not remove settings that you may want to use later when "
+        "the plugin is reloaded.  Options belonging to plugins that are loaded but "
+        "currently disabled will not be listed for possible removal.\n\n"
+        "To remove one or more settings, select the settings to remove by checking the "
+        "box next to the settings, and then enable the removal by checking the \"Remove "
+        "selected options\" box. When you choose \"Make It So!\" to save your option "
+        "settings, the selected items will be removed."
+    )
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MaintenanceOptionsPage()
+        self.ui.setupUi(self)
+        self.ui.description.setText(_(self.INSTRUCTIONS))
+        self.ui.tableWidget.setHorizontalHeaderLabels([_("Option"), _("Value")])
+
+    def load(self):
+        from picard.plugin import _extension_points
+        config = get_config()
+
+        # Setting options from all option pages and loaded plugins (including plugins currently disabled).
+        key_options = set(config.setting.as_dict().keys())
+
+        # All setting options included in the INI file.
+        file_options = set([key[8:] for key in config.allKeys() if key[:8] == 'setting/'])
+
+        # Combine all page and plugin settings with required options not appearing in option pages.
+        current_options = set([
+            # Include options that are required but are not entered directly from the options pages.
+            'file_renaming_scripts',
+            'selected_file_naming_script_id',
+            'log_verbosity',
+            # Items missed if TagsCompatibilityWaveOptionsPage does not register.
+            'remove_wave_riff_info',
+            'wave_riff_info_encoding',
+            'write_wave_riff_info',
+        ]).union(key_options)
+
+        def _add_options_from_page(page, current_options):
+            if not hasattr(page, 'options'):
+                return
+            for option in page.options:
+                if option.section == "setting":
+                    current_options.add(option.name)
+
+        for ep in _extension_points:
+            if ep.label == 'pages':
+                for page in ep:
+                    _add_options_from_page(page, current_options)
+
+        orphan_options = file_options.difference(current_options)
+
+        self.ui.description.setText(
+            _(self.INSTRUCTIONS)
+            + _("\n\nThe configuration file currently contains {0} option settings, {1} which are unused.").format(
+                len(file_options),
+                len(orphan_options)
+            )
+        )
+        self.ui.enable_cleanup.setChecked(False)
+        self.ui.tableWidget.clearContents()
+        self.ui.tableWidget.setRowCount(len(orphan_options))
+        for row, item in enumerate(sorted(orphan_options)):
+            tableitem = QtWidgets.QTableWidgetItem(item)
+            tableitem.setFlags(tableitem.flags() | QtCore.Qt.ItemIsUserCheckable)
+            tableitem.setCheckState(False)
+            self.ui.tableWidget.setItem(row, 0, tableitem)
+            text = self.make_setting_value_text(item)
+            line_height = max(25, 18 * min(5, text.count("\n") + 1))
+            tableitem = QtWidgets.QTextEdit()
+            tableitem.setText(text)
+            self.ui.tableWidget.setRowHeight(row, line_height)
+            self.ui.tableWidget.setCellWidget(row, 1, tableitem)
+        self.ui.tableWidget.resizeColumnsToContents()
+
+    def save(self):
+        if not self.ui.enable_cleanup.checkState() == QtCore.Qt.Checked:
+            return
+        to_remove = set()
+        for idx in range(self.ui.tableWidget.rowCount()):
+            item = self.ui.tableWidget.item(idx, 0)
+            if item.checkState() == QtCore.Qt.Checked:
+                to_remove.add(item.text())
+        if to_remove and QtWidgets.QMessageBox.question(
+            self,
+            _('Confirm Remove'),
+            _("Are you sure you want to remove the {0} selected option settings?").format(len(to_remove)),
+            QtWidgets.QMessageBox.Yes,
+            QtWidgets.QMessageBox.No
+        ) == QtWidgets.QMessageBox.Yes:
+            config = get_config()
+            for item in to_remove:
+                Option.add_if_missing('setting', item, None)
+                log.warning("Removing option setting '%s' from the INI file.", item)
+                config.setting.remove(item)
+
+    def make_setting_value_text(self, key):
+        NONE_TEXT = _("None")
+        config = get_config()
+        value = config.setting.raw_value(key)
+        if value is None:
+            return NONE_TEXT
+        if isinstance(value, str):
+            return '"%s"' % value
+        if type(value) in {bool, int, float}:
+            return str(value)
+        if type(value) in {set, tuple, list, dict}:
+            return _("List of %i items") % len(value)
+        return _("Unknown value format")
+
+
+register_options_page(MaintenanceOptionsPage)
---
+++ picard/ui/ui_options_maintenance.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Automatically generated - don't edit.
+# Use `python setup.py build_ui` to update it.
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MaintenanceOptionsPage(object):
+    def setupUi(self, MaintenanceOptionsPage):
+        MaintenanceOptionsPage.setObjectName("MaintenanceOptionsPage")
+        MaintenanceOptionsPage.resize(334, 397)
+        self.vboxlayout = QtWidgets.QVBoxLayout(MaintenanceOptionsPage)
+        self.vboxlayout.setObjectName("vboxlayout")
+        self.description = QtWidgets.QLabel(MaintenanceOptionsPage)
+        self.description.setText("")
+        self.description.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.description.setWordWrap(True)
+        self.description.setIndent(0)
+        self.description.setObjectName("description")
+        self.vboxlayout.addWidget(self.description)
+        spacerItem = QtWidgets.QSpacerItem(20, 8, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.vboxlayout.addItem(spacerItem)
+        self.line = QtWidgets.QFrame(MaintenanceOptionsPage)
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        self.line.setObjectName("line")
+        self.vboxlayout.addWidget(self.line)
+        spacerItem1 = QtWidgets.QSpacerItem(20, 8, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Fixed)
+        self.vboxlayout.addItem(spacerItem1)
+        self.enable_cleanup = QtWidgets.QCheckBox(MaintenanceOptionsPage)
+        self.enable_cleanup.setObjectName("enable_cleanup")
+        self.vboxlayout.addWidget(self.enable_cleanup)
+        self.tableWidget = QtWidgets.QTableWidget(MaintenanceOptionsPage)
+        self.tableWidget.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustToContents)
+        self.tableWidget.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
+        self.tableWidget.setColumnCount(2)
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.horizontalHeader().setStretchLastSection(True)
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.vboxlayout.addWidget(self.tableWidget)
+
+        self.retranslateUi(MaintenanceOptionsPage)
+        QtCore.QMetaObject.connectSlotsByName(MaintenanceOptionsPage)
+
+    def retranslateUi(self, MaintenanceOptionsPage):
+        _translate = QtCore.QCoreApplication.translate
+        self.enable_cleanup.setText(_("Remove selected options"))
