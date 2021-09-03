10dfab29d932e5f2f77513dee7d0a9c459141a2b
Bob Swift <bswift@rsds.ca>
2021-09-03 08:16:31 -0600

    Disable removal processing checkbox if there are no orphaned options

--- picard/ui/options/maintenance.py
+++ picard/ui/options/maintenance.py
@@ -127,6 +127,9 @@ class MaintenanceOptionsPage(OptionsPage):
             self.ui.tableWidget.setCellWidget(row, 1, tableitem)
         self.ui.tableWidget.resizeColumnsToContents()
         self.ui.select_all.setCheckState(False)
+        # Disable removal processing checkbox if there are no orphaned options found.
+        if not len(orphan_options):
+            self.ui.enable_cleanup.setEnabled(False)
         self.enable_cleanup_changed()
 
     def column_items(self, column):
