471065964ca277f5cce05f5d9e7f5e9b35a8607f
Philipp Wolfer <ph.wolfer@gmail.com>
2021-12-24 12:14:32 +0100

    packaging: verify checksums for macOS downloaded dependencies

--- scripts/package/macos-setup.sh
+++ scripts/package/macos-setup.sh
@@ -7,15 +7,19 @@ brew link gettext --force
 
 # Install requested Python version
 if [ -n "$PYTHON_VERSION" ]; then
-  wget "https://www.python.org/ftp/python/${PYTHON_VERSION}/python-${PYTHON_VERSION}-macosx10.9.pkg"
-  sudo installer -pkg "python-${PYTHON_VERSION}-macosx10.9.pkg" -target /
+  PYTHON_FILENAME=python-$PYTHON_VERSION-macosx10.9.pkg
+  wget "https://www.python.org/ftp/python/$PYTHON_VERSION/$PYTHON_FILENAME"
+  echo "$PYTHON_SHA256SUM  $PYTHON_FILENAME" | shasum --algorithm 256 --check --status
+  sudo installer -pkg "$PYTHON_FILENAME" -target /
   sudo python3 -m ensurepip
 fi
 
 # Install libdiscid
 if [ ! -f "$HOME/libdiscid/lib/libdiscid.0.dylib" ]; then
-  wget "ftp://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/libdiscid-$DISCID_VERSION.tar.gz"
-  tar -xf "libdiscid-$DISCID_VERSION.tar.gz"
+  DISCID_FILENAME="libdiscid-$DISCID_VERSION.tar.gz"
+  wget "ftp://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/$DISCID_FILENAME"
+  echo "$DISCID_SHA256SUM  $DISCID_FILENAME" | shasum --algorithm 256 --check --status
+  tar -xf $DISCID_FILENAME
   cd "libdiscid-$DISCID_VERSION"
   ./configure --prefix="$HOME/libdiscid"
   make install
@@ -25,14 +29,18 @@ cp "$HOME/libdiscid/lib/libdiscid.0.dylib" .
 
 # Install fpcalc
 if [ -n "$FPCALC_VERSION" ]; then
-  wget "https://github.com/acoustid/chromaprint/releases/download/v$FPCALC_VERSION/chromaprint-fpcalc-$FPCALC_VERSION-macos-x86_64.tar.gz"
-  tar -xf "chromaprint-fpcalc-$FPCALC_VERSION-macos-x86_64.tar.gz"
+  FPCALC_FILENAME="chromaprint-fpcalc-$FPCALC_VERSION-macos-x86_64.tar.gz"
+  wget "https://github.com/acoustid/chromaprint/releases/download/v$FPCALC_VERSION/$FPCALC_FILENAME"
+  echo "$FPCALC_SHA256SUM  $FPCALC_FILENAME" | shasum --algorithm 256 --check --status
+  tar -xf "$FPCALC_FILENAME"
   cp "chromaprint-fpcalc-$FPCALC_VERSION-macos-x86_64/fpcalc" .
 fi
 
 # Install AcousticBrainz extractor
 if [ -n "$ABEXTRACTOR_VERSION" ]; then
-  wget "https://github.com/phw/essentia-extractor-builds/releases/download/$ABEXTRACTOR_VERSION/essentia-extractor-$ABEXTRACTOR_VERSION-macos.tar.gz"
-  tar -xf "essentia-extractor-$ABEXTRACTOR_VERSION-macos.tar.gz"
+  ABEXTRACTOR_FILENAME="essentia-extractor-$ABEXTRACTOR_VERSION-macos.tar.gz"
+  wget "https://github.com/phw/essentia-extractor-builds/releases/download/$ABEXTRACTOR_VERSION/$ABEXTRACTOR_FILENAME"
+  echo "$ABEXTRACTOR_SHA256SUM  $ABEXTRACTOR_FILENAME" | shasum --algorithm 256 --check --status
+  tar -xf "$ABEXTRACTOR_FILENAME"
   cp "essentia-extractor-$ABEXTRACTOR_VERSION-macos/streaming_extractor_music" .
 fi
