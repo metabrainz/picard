3118c6e81ef1f44d5416af29b35ed458a36b20c2
skelly37 <skelly37@protonmail.com>
2022-09-05 19:30:19 +0200

    test for no argument commands and for stripping a command from whitespaces

--- test/test_parsing_files_with_commands.py
+++ test/test_parsing_files_with_commands.py
@@ -4,18 +4,30 @@ from picard.tagger import Tagger
 
 
 class TestParsingFilesWithCommands(PicardTestCase):
+
     MOCK_FILE_CONTENTS = (
+        # should be split into 2 commands
+        "LOAD file1.mp3 file2.mp3",
+        # should be added as one
         "FROM_FILE file0.mp3",
+        "CLUSTER",
+        "  FINGERPRINT  "
+        # should be ignored
         "",
         " ",
         "\n",
         "#commented command",
-        "LOAD file1.mp3 file2.mp3",
     )
 
     def setUp(self):
         super().setUp()
         self.result = tuple(x for x in Tagger._parse_commands_from_lines(self.MOCK_FILE_CONTENTS))
+
+    def test_no_argument_command(self):
+        self.assertIn("command://CLUSTER ", self.result)
+
+    def test_no_argument_command_stripped_correctly(self):
+        self.assertIn("command://FINGERPRINT ", self.result)
 
     def test_single_argument_command(self):
         self.assertIn("command://FROM_FILE file0.mp3", self.result)
@@ -29,7 +41,7 @@ class TestParsingFilesWithCommands(PicardTestCase):
         self.assertNotIn("command://", self.result)
         # 1 FROM_FILE
         # 2 LOADs
-        self.assertEqual(len(self.result), 3)
+        self.assertEqual(len(self.result), 5)
 
     def test_commented_lines(self):
         self.assertNotIn("command://#commented command", self.result)
