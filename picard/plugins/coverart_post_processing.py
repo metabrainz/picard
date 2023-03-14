2d19a0a7cc8aa6d37655ec172d58b9f5047772e9
Pranay Pandey <pranaypandeyofficial@gmail.com>
2023-03-14 05:45:47 +0530

    Add Reshape functionality to Plugin.
    Install Pillow to add the Plugin.
    Resize the dimensions of the image if width and height are larger than MAX_DIMENSION.

--- picard/plugins/coverart_post_processing.py
+++ picard/plugins/coverart_post_processing.py
@@ -3,12 +3,14 @@ PLUGIN_AUTHOR = "Pranay"
 PLUGIN_DESCRIPTION = """
 Post Processing Features for the CoverArt Images.
 Ignore images smaller than specified width and height.
+Resize/Crop the image if larger than specified dimensions.
 """
 PLUGIN_VERSION = '0.1'
 PLUGIN_API_VERSIONS = ['2.2']
 PLUGIN_LICENSE = "GPL-2.0-or-later"
 PLUGIN_LICENSE_URL = "https://www.gnu.org/licenses/gpl-2.0.html"
 
+from PIL import Image
 from io import BytesIO
 from picard.util import imageinfo
 from picard import config, log
@@ -16,6 +18,7 @@ from picard.metadata import (register_album_metadata_processor,
                            register_track_metadata_processor)
 from picard.plugin import PluginPriority
 
+MAX_DIMENSION = 420  # Set maximum allowable dimensions of image in pixels
 MIN_DIMENSION = 400  # Set minimum allowable dimensions of image in pixels
     
 def ignore_image(img_data):
@@ -26,6 +29,30 @@ def ignore_image(img_data):
     if width < MIN_DIMENSION or height < MIN_DIMENSION:
         return True
     return False
+
+def resize_image(image_data, max_size=MAX_DIMENSION):
+    """Resize the image to max_size and center crop if larger than max_size."""
+    try:
+        with Image.open(BytesIO(image_data)) as img:
+            width, height = img.size
+            if width > max_size or height > max_size:
+                if width > height:
+                    new_width = max_size
+                    new_height = int(height * (max_size / width))
+                else:
+                    new_height = max_size
+                    new_width = int(width * (max_size / height))
+                img = img.resize((new_width, new_height), resample=Image.LANCZOS)
+                left = (new_width - max_size) / 2
+                top = (new_height - max_size) / 2
+                right = left + max_size
+                bottom = top + max_size
+                img = img.crop((left, top, right, bottom))
+            output_buffer = BytesIO()
+            img.save(output_buffer, format='JPEG')
+            return output_buffer.getvalue()
+    except Exception as ex:
+        log.error("{0}: Error: {1}".format(PLUGIN_NAME, ex,))
 
 def Track_images(album, metadata, track, release):
    try:
@@ -39,6 +66,14 @@ def Track_images(album, metadata, track, release):
                         image.imageinfo_as_string())
                     )
                     metadata.images.pop(id)
+                else:
+                    img_data_edited = resize_image(image_data)
+                    metadata.images[id].set_data(img_data_edited)
+                    
+                    log.debug("Cover art image processed: %r [%s]" % (
+                        image,
+                        image.imageinfo_as_string())
+                    )
    except Exception as ex:
         log.error("{0}: Error: {1}".format(PLUGIN_NAME, ex,))
 
