54801d97dfee909964232b6a0c99677b0409411b
Laurent Monin <github@norz.org>
2023-06-11 11:21:23 +0200

    Now that we default to 'C' locale for tests, remove redundant setups

--- test/test_coverart_utils.py
+++ test/test_coverart_utils.py
@@ -21,8 +21,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 
-import os.path
-
 from test.picardtestcase import PicardTestCase
 
 from picard.coverart.utils import (
@@ -31,17 +29,9 @@ from picard.coverart.utils import (
     translate_caa_type,
     types_from_id3,
 )
-from picard.i18n import setup_gettext
 
 
 class CaaTypeTranslationTest(PicardTestCase):
-    def setUp(self):
-        super().setUp()
-        # we are using temporary locales for tests
-        self.tmp_path = self.mktmpdir()
-        self.localedir = os.path.join(self.tmp_path, 'locale')
-        setup_gettext(self.localedir, "C")
-
     def test_translating_unknown_types_returns_input(self):
         testtype = "ThisIsAMadeUpCoverArtTypeName"
         self.assertEqual(translate_caa_type(testtype), testtype)
