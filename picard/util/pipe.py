03de33ed3d1f2cd1b5a66cf6b9639a313c3a0af2
Philipp Wolfer <ph.wolfer@gmail.com>
2023-07-04 22:16:47 +0200

    Re-introduce thread pool for pipe
    Creating a thread pool on each read / write for only a single thread is inefficient.

--- picard/util/pipe.py
+++ picard/util/pipe.py
@@ -160,6 +160,8 @@ class AbstractPipe(metaclass=ABCMeta):
 
         self.unexpected_removal = False
 
+        self.__thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=6)
+
         for path in self._paths:
             self.path = path
             for arg in self._args:
@@ -234,21 +236,20 @@ class AbstractPipe(metaclass=ABCMeta):
         if timeout_secs is None:
             timeout_secs = self.TIMEOUT_SECS
 
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            reader = executor.submit(self._reader)
-            try:
-                res = reader.result(timeout=timeout_secs)
-                if res:
-                    out = [r for r in res.split(self.MESSAGE_TO_IGNORE) if r]
-                    if out:
-                        return out
-            except concurrent.futures._base.TimeoutError:
-                # hacky way to kill the file-opening loop
-                self.send_to_pipe(self.MESSAGE_TO_IGNORE)
-            except Exception as e:
-                # https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.Future.result
-                # If the call raised an exception, this method will raise the same exception.
-                log.error("pipe reader exception: %s", e)
+        try:
+            reader = self.__thread_pool.submit(self._reader)
+            res = reader.result(timeout=timeout_secs)
+            if res:
+                out = [r for r in res.split(self.MESSAGE_TO_IGNORE) if r]
+                if out:
+                    return out
+        except concurrent.futures._base.TimeoutError:
+            # hacky way to kill the file-opening loop
+            self.send_to_pipe(self.MESSAGE_TO_IGNORE)
+        except Exception as e:
+            # https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.Future.result
+            # If the call raised an exception, this method will raise the same exception.
+            log.error("pipe reader exception: %s", e)
 
         return [self.NO_RESPONSE_MESSAGE]
 
@@ -264,22 +265,22 @@ class AbstractPipe(metaclass=ABCMeta):
         if timeout_secs is None:
             timeout_secs = self.TIMEOUT_SECS
 
-        with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
-            # we're sending only filepaths, so we have to create some kind of separator
-            # to avoid any potential conflicts and mixing the data
-            sender = executor.submit(self._sender, message + self.MESSAGE_TO_IGNORE)
-            try:
-                if sender.result(timeout=timeout_secs):
-                    return True
-            except concurrent.futures._base.TimeoutError:
-                if self.pipe_running:
-                    log.warning("Couldn't send: %r", message)
-                # hacky way to kill the sender
-                self.read_from_pipe()
-            except Exception as e:
-                # https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.Future.result
-                # If the call raised an exception, this method will raise the same exception.
-                log.error("pipe sender exception: %s", e)
+        # we're sending only filepaths, so we have to create some kind of separator
+        # to avoid any potential conflicts and mixing the data
+        try:
+            sender = self.__thread_pool.submit(
+                self._sender, message + self.MESSAGE_TO_IGNORE)
+            if sender.result(timeout=timeout_secs):
+                return True
+        except concurrent.futures._base.TimeoutError:
+            if self.pipe_running:
+                log.warning("Couldn't send: %r", message)
+            # hacky way to kill the sender
+            self.read_from_pipe()
+        except Exception as e:
+            # https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.Future.result
+            # If the call raised an exception, this method will raise the same exception.
+            log.error("pipe sender exception: %s", e)
 
         return False
 
@@ -287,6 +288,7 @@ class AbstractPipe(metaclass=ABCMeta):
         log.debug("Stopping pipe")
         self.pipe_running = False
         self.send_to_pipe(self.MESSAGE_TO_IGNORE)
+        self.__thread_pool.shutdown(wait=True, cancel_futures=True)
 
 
 class UnixPipe(AbstractPipe):
