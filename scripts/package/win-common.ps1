2a89c2edf26d9ecb0c3f3f6cb71abd5b17f9507f
Philipp Wolfer <ph.wolfer@gmail.com>
2023-09-08 09:00:06 +0200

    Use named parameters in powershell script

--- scripts/package/win-common.ps1
+++ scripts/package/win-common.ps1
@@ -33,16 +33,16 @@ Function FinalizePackage {
     $Path
   )
 
-  CodeSignBinary (Join-Path $Path picard.exe)
-  CodeSignBinary (Join-Path $Path fpcalc.exe)
-  CodeSignBinary (Join-Path $Path discid.dll)
+  CodeSignBinary (Join-Path -Path $Path -ChildPath picard.exe)
+  CodeSignBinary (Join-Path -Path $Path -ChildPath fpcalc.exe)
+  CodeSignBinary (Join-Path -Path $Path -ChildPath discid.dll)
 
   # Move all Qt5 DLLs into the main folder to avoid conflicts with system wide
   # versions of those dependencies. Since some version PyInstaller tries to
   # maintain the file hierarchy of imported modules, but this easily breaks
   # DLL loading on Windows.
   # Workaround for https://tickets.metabrainz.org/browse/PICARD-2736
-  $Qt5BinDir = (Join-Path $Path PyQt5 Qt5 bin)
-  Move-Item (Join-Path $Qt5BinDir *.dll) $Path -Force
-  Remove-Item $Qt5BinDir
+  $Qt5BinDir = (Join-Path -Path $Path -ChildPath PyQt5\Qt5\bin)
+  Move-Item -Path (Join-Path -Path $Qt5BinDir -ChildPath *.dll) -Destination $Path -Force
+  Remove-Item -Path $Qt5BinDir
 }
