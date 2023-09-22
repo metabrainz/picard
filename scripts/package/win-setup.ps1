118f5f7b660ceedfbdb861182fd2443ab1184127
Philipp Wolfer <ph.wolfer@gmail.com>
2023-09-22 14:35:26 +0200

    Build macOS and Windows packages with libdiscid 0.6.4

--- scripts/package/win-setup.ps1
+++ scripts/package/win-setup.ps1
@@ -50,7 +50,7 @@ DownloadFile -Url "http://ftp.musicbrainz.org/pub/musicbrainz/libdiscid/libdisci
   -FileName $ArchiveFile
 VerifyHash -FileName $ArchiveFile -Sha256Sum $DiscidSha256Sum
 Expand-Archive -Path $ArchiveFile -DestinationPath .\build\libdiscid -Force
-Copy-Item .\build\libdiscid\libdiscid-windows-$DiscidVersion\x64\discid.dll .
+Copy-Item .\build\libdiscid\libdiscid-$DiscidVersion-win\x64\discid.dll .
 
 $ArchiveFile = ".\build\fpcalc.zip"
 Write-Output "Downloading chromaprint-fpcalc $FpcalcVersion to $ArchiveFile..."
