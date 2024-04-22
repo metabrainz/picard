4c4114fcc92664937466a4f24c44013855e0bd5f
Philipp Wolfer <ph.wolfer@gmail.com>
2024-04-22 18:29:43 +0200

    PySide6: Rebuild resource file

--- resources/compile.py
+++ resources/compile.py
@@ -7,7 +7,7 @@
 # Copyright (C) 2013-2014, 2018 Laurent Monin
 # Copyright (C) 2014 Shadab Zafar
 # Copyright (C) 2016 Sambhav Kothari
-# Copyright (C) 2022 Philipp Wolfer
+# Copyright (C) 2022-2024 Philipp Wolfer
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -23,15 +23,14 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-
 from distutils import log
-from distutils.dep_util import newer
 from distutils.spawn import (
     DistutilsExecError,
-    find_executable,
     spawn,
 )
 import os.path
+
+from setuptools.modified import newer
 
 
 def main():
@@ -40,16 +39,11 @@ def main():
     pyfile = os.path.join(topdir, "picard", "resources.py")
     qrcfile = os.path.join(topdir, "resources", "picard.qrc")
     if newer(qrcfile, pyfile):
-        rcc = 'rcc'
-        rcc_path = find_executable(rcc, path='/usr/lib/qt6/libexec/') or find_executable(rcc)
-        if rcc_path is None:
-            log.error("%s command not found, cannot build resource file !", rcc)
-        else:
-            cmd = [rcc_path, '-g', 'python', '-o', pyfile, qrcfile]
-            try:
-                spawn(cmd, search_path=0)
-            except DistutilsExecError as e:
-                log.error(e)
+        cmd = ['pyside6-rcc', '-o', pyfile, qrcfile]
+        try:
+            spawn(cmd, search_path=0)
+        except DistutilsExecError as e:
+            log.error(e)
 
 
 if __name__ == "__main__":
