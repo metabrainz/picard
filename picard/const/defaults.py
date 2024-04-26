559f364fcc73b426ce97070949495a3e4fca12da
Laurent Monin <github@norz.org>
2024-04-26 22:41:26 +0200

    Add missing header to const/defaults.py
    Copied from const/__init__.py where most entries were before

--- picard/const/defaults.py
+++ picard/const/defaults.py
@@ -1,3 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Picard, the next-generation MusicBrainz tagger
+#
+# Copyright (C) 2007, 2014, 2016 Lukáš Lalinský
+# Copyright (C) 2014, 2019-2022 Philipp Wolfer
+# Copyright (C) 2014-2016, 2018-2021, 2024 Laurent Monin
+# Copyright (C) 2015 Ohm Patel
+# Copyright (C) 2016 Rahul Raturi
+# Copyright (C) 2016 Wieland Hoffmann
+# Copyright (C) 2016-2017 Frederik “Freso” S. Olesen
+# Copyright (C) 2017 Antonio Larrosa
+# Copyright (C) 2017 Sambhav Kothari
+# Copyright (C) 2018 Vishal Choudhary
+# Copyright (C) 2018, 2021, 2023 Bob Swift
+# Copyright (C) 2020 RomFouq
+# Copyright (C) 2021 Gabriel Ferreira
+# Copyright (C) 2021 Vladislav Karbovskii
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 import os
 
 from PyQt6 import QtCore
