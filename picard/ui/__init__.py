2ec8248ea006ea518e62bc32cbcc145296125d72
Laurent Monin <github@norz.org>
2024-05-29 22:12:21 +0200

    PicardDialog.__init__(): call parent class __init__() before anything

--- picard/ui/__init__.py
+++ picard/ui/__init__.py
@@ -182,8 +182,8 @@ class PicardDialog(QtWidgets.QDialog, PreserveGeometry):
     ready_for_display = QtCore.pyqtSignal()
 
     def __init__(self, parent=None):
-        self.tagger = QtCore.QCoreApplication.instance()
         super().__init__(parent=parent, f=self.flags)
+        self.tagger = QtCore.QCoreApplication.instance()
         self.__shown = False
         self.ready_for_display.connect(self.restore_geometry)
 
