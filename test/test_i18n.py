7afd4179aeac27f3d14efe4f88d571777c904636
Philipp Wolfer <ph.wolfer@gmail.com>
2024-08-24 16:29:56 +0200

    PICARD-2953: Use strxfrm for sorting on Windows again
    On Windows QCollator.sortKey is broken and results of wrong ordering of
    numbers, both in numeric and normal alphabetic mode.

--- test/test_i18n.py
+++ test/test_i18n.py
@@ -90,10 +90,12 @@ class TestI18n(PicardTestCase):
         self.assertTrue(sort_key('äb') < sort_key('ac'))
         self.assertTrue(sort_key('foo002') < sort_key('foo1'))
         self.assertTrue(sort_key('002 foo') < sort_key('1 foo'))
+        self.assertTrue(sort_key('1') < sort_key('C'))
         self.assertTrue(sort_key('foo1', numeric=True) < sort_key('foo002', numeric=True))
         self.assertTrue(sort_key('004', numeric=True) < sort_key('5', numeric=True))
         self.assertTrue(sort_key('0042', numeric=True) < sort_key('50', numeric=True))
         self.assertTrue(sort_key('5', numeric=True) < sort_key('0042', numeric=True))
+        self.assertTrue(sort_key('99', numeric=True) < sort_key('100', numeric=True))
 
     def test_sort_key_numbers_different_scripts(self):
         setup_gettext(localedir, 'en')
