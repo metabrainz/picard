f18c7adf8d3a141bcbe4fe061965161ac0ca47ef
Martin <spleefer90@gmail.com>
2024-12-23 10:55:56 +0100

    Add Python 3.13 to Python package metadata

--- setup.py
+++ setup.py
@@ -806,6 +806,7 @@ args = {
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
         'Operating System :: MacOS',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: POSIX :: Linux',
