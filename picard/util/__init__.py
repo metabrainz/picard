ae0315d5a3742a18213548ed0f415f47205aa797
Steve <stevilknevil@hotmail.co.uk>
2025-03-13 16:20:11 +0000

    Fail safe if the parameter passed to format_time cannot be converted to a float

--- picard/util/__init__.py
+++ picard/util/__init__.py
@@ -301,7 +301,12 @@ def samefile(path1, path2):
 
 def format_time(ms, display_zero=False):
     """Formats time in milliseconds to a string representation."""
-    ms = float(ms)
+    # Ensure that the input represents a number
+    try:
+        ms = float(ms)
+    except (TypeError, ValueError):
+        ms = 0
+    
     if ms == 0 and not display_zero:
         return "?:??"
     duration_seconds = round(ms / 1000)
