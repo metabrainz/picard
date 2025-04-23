metabrainz/picard

commit d765211120fe04429c2081b20c5b988d0b42e0d7

Replace `TAG_NAMES` dict by a generator `tag_names()`

picard/ui/metadatabox/edittagdialog.py
@@ -41,7 +41,7 @@ from picard.const import (
 from picard.const.countries import RELEASE_COUNTRIES
 from picard.i18n import gettext as _
 from picard.util import temporary_disconnect
-from picard.util.tags import TAG_NAMES
+from picard.util.tags import tag_names
 
 from picard.ui import PicardDialog
 from picard.ui.forms.ui_edittagdialog import Ui_EditTagDialog
@@ -204,7 +204,7 @@ class EditTagDialog(PicardDialog):
     def _setup_tag_combobox(self):
         """Set up the tag name combobox with supported tags."""
         self.default_tags = self._get_supported_tags()
-        visible_tags = (tn for tn in self.default_tags if not tn.startswith("~"))
+        visible_tags = (tn for tn in self.default_tags if not tn.startswith("~"))  # TODO: use TagVar.is_hidden?
 
         self.ui.tag_names.addItem("")
         self.ui.tag_names.addItems(visible_tags)
@@ -219,7 +219,7 @@ class EditTagDialog(PicardDialog):
         Returns:
             List of supported tag names
         """
-        tags = sorted(set(list(TAG_NAMES.keys()) + self.metadata_box.tag_diff.tag_names))
+        tags = sorted(set(tag_names()) + self.metadata_box.tag_diff.tag_names)
         if len(self.metadata_box.files) == 1:
             current_file = list(self.metadata_box.files)[0]
             tags = list(filter(current_file.supports_tag, tags))

picard/ui/widgets/taglisteditor.py
@@ -22,7 +22,7 @@
 
 from PyQt6 import QtWidgets
 
-from picard.util.tags import TAG_NAMES
+from picard.util.tags import tag_names
 
 from picard.ui.forms.ui_widget_taglisteditor import Ui_TagListEditor
 from picard.ui.widgets.editablelistview import (
@@ -41,8 +41,7 @@ class TagListEditor(QtWidgets.QWidget):
         model.user_sortable_changed.connect(self.on_user_sortable_changed)
         self.ui.sort_buttons.setVisible(model.user_sortable)
         list_view.setModel(model)
-        list_view.setItemDelegate(AutocompleteItemDelegate(
-            sorted(TAG_NAMES.keys())))
+        list_view.setItemDelegate(AutocompleteItemDelegate(sorted(tag_names())))
 
         selection = list_view.selectionModel()
         selection.selectionChanged.connect(self.on_selection_changed)

picard/util/tags.py
@@ -735,7 +735,11 @@ ALL_TAG_VARS = {
 
 ALL_NAME_2_TAGVAR = {str(tv): tv for tv in ALL_TAG_VARS}
 
-TAG_NAMES = {str(tv): tv.shortdesc for tv in ALL_TAG_VARS if tv.is_tag}
+
+def tag_names():
+    yield from (str(tv) for tv in ALL_TAG_VARS if tv.is_tag)
+
+
 PRESERVED_TAGS = tuple(str(tv) for tv in ALL_TAG_VARS if tv.is_preserved)
 
 # Tags that got generated in some way from the audio content.

commit ad6ed706db26825eafb6c85c88fe7b64a84cc782

Fix exception introduced in d765211120fe0
```
  File "picard/ui/metadatabox/edittagdialog.py", line 222, in _get_supported_tags
    tags = sorted(list(tag_names()) + self.metadata_box.tag_diff.tag_names)
                  ~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
TypeError: unsupported operand type(s) for +: 'set' and 'list'
```

This issue was introduced in commit d765211120fe04429c2081b20c5b988d0b42e0d7, it should have been `list()` instead of `set()`, tag names are already unique, but `tag_names()` is a generator.

picard/ui/metadatabox/edittagdialog.py
@@ -219,7 +219,7 @@ class EditTagDialog(PicardDialog):
         Returns:
             List of supported tag names
         """
-        tags = sorted(set(tag_names()) + self.metadata_box.tag_diff.tag_names)
+        tags = sorted(list(tag_names()) + self.metadata_box.tag_diff.tag_names)
         if len(self.metadata_box.files) == 1:
             current_file = list(self.metadata_box.files)[0]
             tags = list(filter(current_file.supports_tag, tags))