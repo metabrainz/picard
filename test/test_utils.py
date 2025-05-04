63f4386befac0a6b6415ee6292a10312bc0e21b4
Marethyun <21334836+Marethyun@users.noreply.github.com>
2025-05-04 15:29:25 +0200

    Fixed test case for dot stripping in filenames

--- test/test_utils.py
+++ test/test_utils.py
@@ -680,7 +680,7 @@ class TracknumAndTitleFromFilenameTest(PicardTestCase):
             (('42', 'Foo'), '0000042 Foo.mp3'),
             (('2', 'Foo'), '0000002 Foo.mp3'),
             # Tracknumber dot not in title
-            (('77', 'Foo.mp3'), '77. Foo.mp3'),
+            (('77', 'Foo'), '77. Foo.mp3'),
             ((None, '20000 Feet'), '20000 Feet.mp3'),
             ((None, '20,000 Feet'), '20,000 Feet.mp3'),
             ((None, 'Venus (Original 12" version)'), 'Venus (Original 12" version).mp3'),
