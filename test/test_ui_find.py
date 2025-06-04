0ee3bcfc973f5ea4200b093e409523d8aabf190a
Francisco Lisboa <franciscolisboa@tecnico.ulisboa.pt>
2025-06-04 13:34:16 +0100

    fix: rebase & headers
    During the rebase process, some imports had to be changed, due to the recent modification to the project's structure.

--- test/test_ui_find.py
+++ test/test_ui_find.py
@@ -1,7 +1,28 @@
+# -*- coding: utf-8 -*-
+#
+# Picard, the next-generation MusicBrainz tagger
+#
+# Copyright (C) 2025 João Sousa
+# Copyright (C) 2025 Francisco Lisboa
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
 from test.picardtestcase import PicardTestCase
 
 from picard.metadata import Metadata
-from picard.util.tags import tag_names
+from picard.tags import preserved_tag_names
 
 from picard.ui.find import FindBox
 from picard.ui.itemviews import MainPanel
@@ -13,7 +34,7 @@ class FindBoxTest(PicardTestCase):
 
     def test_tag_names_available(self):
         """Test that tag_names() returns valid tags for filtering"""
-        tags = set(tag_names())
+        tags = set(preserved_tag_names())
         self.assertIsInstance(tags, set)
         self.assertGreater(len(tags), 0)
         self.assertIn('artist', tags)
