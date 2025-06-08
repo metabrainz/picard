feaa5732aea5a6c72697b562ef7e08d4fc6cf013
Bob Swift <bswift@rsds.ca>
2025-06-08 13:12:24 -0600

    Reapply filters if filterable tags reloaded.  Add testing option.

--- picard/ui/filter.py
+++ picard/ui/filter.py
@@ -4,6 +4,7 @@
 #
 # Copyright (C) 2025 João Sousa
 # Copyright (C) 2025 Francisco Lisboa
+# Copyright (C) 2025 Bob Swift
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -34,13 +35,24 @@ from picard.tags import (
 )
 
 
+# When set to True, this will add a test button and a filter item that is not
+# normally used (TEST_TAG) to allow testing of the filterable tags reloading.
+# The TEST_TAG item will be toggled from the filterable items dialog when the
+# test button is clicked.
+TEST_RELOAD_FILTERABLE_TAGS = True
+TEST_TAG = '~filesize'
+
+
 class Filter(QtWidgets.QWidget):
 
     filterChanged = QtCore.pyqtSignal(str, list)
     filterable_tags = set()
+    instances = set()
 
     def __init__(self, parent=None):
         super().__init__(parent)
+        Filter.instances.add(self)
+        self.initializing = True
         layout = QtWidgets.QHBoxLayout(self)
         layout.setContentsMargins(2, 2, 2, 2)
 
@@ -52,8 +64,10 @@ class Filter(QtWidgets.QWidget):
         self.filter_button.clicked.connect(self._show_filter_dialog)
         layout.addWidget(self.filter_button)
 
-        self.load_filterable_tags()
         self.selected_filters = []  # Start with no filters selected
+        self.load_filterable_tags()
+        if TEST_RELOAD_FILTERABLE_TAGS:
+            Filter.filterable_tags.add('~filesize')
 
         # filter input
         self.filter_query_box = QtWidgets.QLineEdit(self)
@@ -61,6 +75,18 @@ class Filter(QtWidgets.QWidget):
         self.filter_query_box.setClearButtonEnabled(True)
         self.filter_query_box.textChanged.connect(self._query_changed)
         layout.addWidget(self.filter_query_box)
+
+        if TEST_RELOAD_FILTERABLE_TAGS:
+            # test button
+            self.test_button = QtWidgets.QPushButton('Testing On', self)
+            self.test_button.setMaximumWidth(120)
+            self.test_button.clicked.connect(self.test_tag_reload)
+            layout.addWidget(self.test_button)
+
+        self.initializing = False
+
+    def __del__(self):
+        Filter.instances.discard(self)
 
     def _show_filter_dialog(self):
         """Show dialog to select multiple filters"""
@@ -79,7 +105,7 @@ class Filter(QtWidgets.QWidget):
         checkboxes = {}
 
         # Add checkboxes for all tags
-        for tag in sorted(self.filterable_tags, key=lambda t: ALL_TAGS.display_name(t).lower()):
+        for tag in sorted(Filter.filterable_tags, key=lambda t: ALL_TAGS.display_name(t).lower()):
             checkbox = QtWidgets.QCheckBox(ALL_TAGS.display_name(str(tag)), scroll_content)
             checkbox.setChecked(str(tag) in self.selected_filters)
             checkbox.setToolTip(ALL_TAGS.display_tooltip(tag))
@@ -112,10 +138,41 @@ class Filter(QtWidgets.QWidget):
 
             self._query_changed(self.filter_query_box.text())
 
+    def test_tag_reload(self):
+        # Used for testing of the filterable tags reloading.
+        new_state = 'Off' if TEST_TAG in Filter.filterable_tags else 'On'
+        for item in Filter.instances:
+            item.test_button.setText(f'Testing {new_state}')
+        if TEST_TAG in Filter.filterable_tags:
+            self.load_filterable_tags(force=True)
+        else:
+            Filter.filterable_tags.add(TEST_TAG)
+
     @classmethod
     def load_filterable_tags(cls, force: bool = False):
-        if force or not cls.filterable_tags:
-            cls.filterable_tags = set(filterable_tag_names())
+        if cls.filterable_tags and not force:
+            return
+        old_filterable_tags = cls.filterable_tags.copy()
+        cls.filterable_tags = set(filterable_tag_names())
+        if cls.filterable_tags == old_filterable_tags:
+            return
+        for item in cls.instances:
+            item: Filter
+            item.filterable_tags_updated()
+
+    def filterable_tags_updated(self):
+        if self.initializing:
+            return
+        # TODO: Rebuild the filter selection dialog?
+
+        # Check if selected filters were removed and re-apply the filter
+        old_filters = set(self.selected_filters)
+        temp = old_filters.difference(Filter.filterable_tags)
+        if temp:
+            new = old_filters - temp
+            self.selected_filters = list(new)
+            self.set_filter_button_label(self.make_button_text(self.selected_filters))
+            self._query_changed(self.filter_query_box.text())
 
     @classmethod
     def make_button_text(cls, selected_filters):
