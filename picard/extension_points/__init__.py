10ba1e6cace76a43c7b8b96e2640b58379478e47
Philipp Wolfer <ph.wolfer@gmail.com>
2025-12-09 07:28:04 +0100

    Revert "Fix tests by not patching global ExtensionPoints"
    This reverts commit ef4a715a8e34d9649e2813fff9e42ec33e245b4d.

--- picard/extension_points/__init__.py
+++ picard/extension_points/__init__.py
@@ -91,11 +91,6 @@ class ExtensionPoint(QObject):
             # >>> #^^ no exception, after first read
             pass
 
-    def clear(self):
-        """Remove all registered modules"""
-        self.__dict.clear()
-        self.changed.emit()
-
     def __iter__(self):
         config = get_config()
         if not config:
