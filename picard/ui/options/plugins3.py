e0611b98e8aa5cc49e2bdbb614667e9c652aa5a3
Laurent Monin <github@norz.org>
2025-12-15 11:25:33 +0100

    Add debug logging when an exception occurs when loading plugins in Options

--- picard/ui/options/plugins3.py
+++ picard/ui/options/plugins3.py
@@ -150,6 +150,7 @@ class Plugins3OptionsPage(OptionsPage):
             self._show_enabled_state()
             self._update_details_button_text()  # Update button state based on plugin availability
         except Exception as e:
+            log.debug("Error loading plugins", exc_info=True)
             self._show_status(_("Error loading plugins: {}").format(str(e)))
 
     def _refresh_all(self):
