51116ba0ae5ee74114cb46420d8ce48b5214e974
Bob Swift <bswift@rsds.ca>
2026-01-28 05:00:37 -0700

    Use dictionary for test definitions.  Add subclass testing.

--- picard/config.py
+++ picard/config.py
@@ -33,7 +33,10 @@ from collections import (
     defaultdict,
     namedtuple,
 )
-from enum import Enum
+from enum import (
+    Enum,
+    IntEnum,
+)
 import os
 import shutil
 from typing import Any
@@ -48,9 +51,6 @@ from picard import (
 )
 from picard.profile import profile_groups_all_settings
 from picard.version import Version
-
-
-OptionTypeTest = namedtuple('OptionTypeTest', ['type', 'class_name'])
 
 
 class Option(QtCore.QObject):
@@ -71,19 +71,33 @@ class Option(QtCore.QObject):
 
     def _check_if_valid(self):
         """Check if the option should be sub-classed, and log a warning."""
-        tests = [
-            OptionTypeTest(str, 'TextOption'),
-            OptionTypeTest(bool, 'BoolOption'),
-            OptionTypeTest(int, 'IntOption'),
-            OptionTypeTest(float, 'FloatOption'),
-            OptionTypeTest(list, 'ListOption'),
-        ]
-        for test in tests:
-            if type(self.default) is test.type and self.__class__.__name__ != test.class_name:
-                log.warning(
-                    "Invalid Option definition for %s/%s: should be %s", self.section, self.name, test.class_name
-                )
-                break
+        tests = {
+            str: TextOption,
+            bool: BoolOption,
+            int: IntOption,
+            float: FloatOption,
+            list: ListOption,
+            tuple: ListOption,
+            dict: Option,
+        }
+
+        subclass_tests = {
+            IntEnum: IntOption,
+            QtCore.QByteArray: Option,
+        }
+
+        def _do_logging(expected):
+            log.warning("Invalid Option definition for %s/%s: should be %s", self.section, self.name, expected.__name__)
+
+        t = type(self.default)
+        if t in tests:
+            if self.__class__ != tests[t]:
+                _do_logging(tests[t])
+        else:
+            for base, expected in subclass_tests.items():
+                if issubclass(t, base) and self.__class__ != expected:
+                    _do_logging(expected)
+                    break
 
     @classmethod
     def get(cls, section, name):
