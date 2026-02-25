2fa246a4c2338bace0a1a395fe50730745734120
Philipp Wolfer <ph.wolfer@gmail.com>
2026-02-25 07:28:32 +0100

    Fix CD submenu update using all drives in non-auto-detect mode

--- picard/ui/options/cdlookup.py
+++ picard/ui/options/cdlookup.py
@@ -76,11 +76,10 @@ class CDLookupOptionsPage(OptionsPage):
         config = get_config()
         if AUTO_DETECT_DRIVES:
             device = self.ui.cd_lookup_device.currentText()
-            device_list = self._device_list
         else:
             device = self.ui.cd_lookup_device.text()
-            device_list = [device]
         config.setting['cd_lookup_device'] = device
+        device_list = get_cdrom_drives()
         self.tagger.window.update_cd_lookup_menu(device_list)
 
 
