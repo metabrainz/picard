metabrainz/picard

commit 6938bd5ace4b749f48af41b5eece078da0e0c862

Simplify code to fill preferred release countries/formats lists

picard/ui/options/releases.py
@@ -8,7 +8,7 @@
 # Copyright (C) 2013-2015, 2018-2024 Laurent Monin
 # Copyright (C) 2016-2017 Sambhav Kothari
 # Copyright (C) 2017 Suhas
-# Copyright (C) 2018-2022 Philipp Wolfer
+# Copyright (C) 2018-2022, 2024 Philipp Wolfer
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -24,8 +24,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-
-from operator import itemgetter
+from functools import partial
 
 from PyQt6 import (
     QtCore,
@@ -275,31 +274,27 @@ class ReleasesOptionsPage(OptionsPage):
 
     def _load_list_items(self, setting, source, list1, list2):
         if setting == 'preferred_release_countries':
-            source_list = [(c[0], gettext_countries(c[1])) for c in
-                           source.items()]
+            translate_func = gettext_countries
         elif setting == 'preferred_release_formats':
-            source_list = [(c[0], pgettext_attributes('medium_format', c[1])) for c
-                           in source.items()]
+            translate_func = partial(pgettext_attributes, 'medium_format')
         else:
-            source_list = [(c[0], _(c[1])) for c in source.items()]
+            translate_func = _
 
         def fcmp(x):
             return sort_key(x[1])
+
+        source_list = [(c[0], translate_func(c[1])) for c in source.items()]
         source_list.sort(key=fcmp)
         config = get_config()
         saved_data = config.setting[setting]
-        move = []
         for data, name in source_list:
             item = QtWidgets.QListWidgetItem(name)
             item.setData(QtCore.Qt.ItemDataRole.UserRole, data)
             try:
-                i = saved_data.index(data)
-                move.append((i, item))
-            except BaseException:
+                saved_data.index(data)
+                list2.addItem(item)
+            except ValueError:
                 list1.addItem(item)
-        move.sort(key=itemgetter(0))
-        for i, item in move:
-            list2.addItem(item)
 
     def _save_list_items(self, setting, list1):
         data = [

commit 342fc5a3a00d9d641fb2b81ef4114a3d93d7fe7e

PICARD-3256: Fix preferred release settings loosing sort order
This fixes a regression introduced in 6938bd5ace4b749f48af41b5eece078da0e0c862

picard/ui/options/releases.py
@@ -27,6 +27,7 @@
 
 
 from functools import partial
+from operator import itemgetter
 
 from PyQt6 import (
     QtCore,
@@ -291,6 +292,7 @@ class ReleasesOptionsPage(OptionsPage):
             return sort_key(x[1])
 
         source_list = [(c[0], translate_func(c[1])) for c in source.items()]
+        target_list = []
         source_list.sort(key=fcmp)
         config = get_config()
         saved_data = config.setting[setting]
@@ -298,10 +300,14 @@ class ReleasesOptionsPage(OptionsPage):
             item = QtWidgets.QListWidgetItem(name)
             item.setData(QtCore.Qt.ItemDataRole.UserRole, data)
             try:
-                saved_data.index(data)
-                list2.addItem(item)
+                i = saved_data.index(data)
+                target_list.append((i, item))
             except ValueError:
                 list1.addItem(item)
+
+        target_list.sort(key=itemgetter(0))
+        for _i, item in target_list:
+            list2.addItem(item)
 
     def _save_list_items(self, setting, list1):
         data = [item.data(QtCore.Qt.ItemDataRole.UserRole) for item in qlistwidget_items(list1)]