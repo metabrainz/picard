3d1a65f0beff1db7540496ed77402020d5e31448
Philipp Wolfer <ph.wolfer@gmail.com>
2026-07-06 18:33:06 +0200

    PICARD-2999: check_for_updates is False by default, user has to opt-in

--- picard/options.py
+++ picard/options.py
@@ -599,7 +599,7 @@ ListOption('setting', 'list_of_scripts', [], title=N_("Tagger scripts"), in_prof
 IntOption('persist', 'last_update_check', 0)
 BoolOption('setting', 'check_rtd_updates', False, title=N_("Check for documentation updates"), in_profile=True)
 BoolOption('setting', 'check_for_plugin_updates', False, title=N_("Check for plugin updates"), in_profile=True)
-BoolOption('setting', 'check_for_updates', True, title=N_("Check for program updates"), in_profile=True)
+BoolOption('setting', 'check_for_updates', False, title=N_("Check for program updates"), in_profile=True)
 IntOption('setting', 'update_check_days', 7, title=N_("Days between update checks"), in_profile=True)
 IntOption('setting', 'update_level', DEFAULT_PROGRAM_UPDATE_LEVEL, title=N_("Update types to check"), in_profile=True)
 IntOption('setting', 'log_verbosity', DEFAULT_LOG_LEVEL, title=N_("Log verbosity level"), in_profile=True)
