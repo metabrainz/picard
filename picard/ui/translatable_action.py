36c6db3438e5c5be46f28235b810f5268ff58636
Laurent Monin <github@norz.org>
2026-08-27 09:07:45 +0200

    ui: Add TranslatableAction subclass for dynamic retranslation
    QAction subclass that stores source strings passed to setText(),
    setToolTip(), and setStatusTip(), applies _() immediately, and
    provides a retranslateUi() method to re-apply _() after a language
    change.
    
    This follows the Qt-native pattern where retranslateUi() re-evaluates
    translations, but avoids repeating string literals by storing the
    source at set-time.

---
+++ picard/ui/translatable_action.py
@@ -0,0 +1,60 @@
+# Picard, the next-generation MusicBrainz tagger
+#
+# Copyright (C) 2026 Laurent Monin
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <https://www.gnu.org/licenses/>.
+
+
+from PyQt6.QtGui import QAction
+
+from picard.i18n import gettext as _
+
+
+class TranslatableAction(QAction):
+    """QAction subclass that supports dynamic retranslation.
+
+    All text set via setText(), setToolTip(), and setStatusTip() is treated
+    as untranslated source text (as marked with N_() at the call site).
+    The source is stored and _() is applied immediately.
+
+    Call retranslateUi() after a language change to re-apply _() to all
+    stored source strings.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._source_text = ''
+        self._source_tool_tip = ''
+        self._source_status_tip = ''
+
+    def setText(self, text):
+        self._source_text = text
+        super().setText(_(text) if text else '')
+
+    def setToolTip(self, text):
+        self._source_tool_tip = text
+        super().setToolTip(_(text) if text else '')
+
+    def setStatusTip(self, text):
+        self._source_status_tip = text
+        super().setStatusTip(_(text) if text else '')
+
+    def retranslateUi(self):
+        """Re-apply _() to all stored source strings."""
+        if self._source_text:
+            super().setText(_(self._source_text))
+        if self._source_tool_tip:
+            super().setToolTip(_(self._source_tool_tip))
+        if self._source_status_tip:
+            super().setStatusTip(_(self._source_status_tip))
