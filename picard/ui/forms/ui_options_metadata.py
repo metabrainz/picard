d4ac73a39318d5565e45a53915c2a691e0b344d6
krotka <oliwiapolonia@gmail.com>
2026-09-06 21:24:33 +0200

    Fix option from code generation

--- picard/ui/forms/ui_options_metadata.py
+++ picard/ui/forms/ui_options_metadata.py
@@ -169,7 +169,6 @@ class Ui_MetadataOptionsPage(object):
         self.retranslateUi(MetadataOptionsPage)
         self.standardize_artist_names.setCurrentIndex(-1)
         QtCore.QMetaObject.connectSlotsByName(MetadataOptionsPage)
-        MetadataOptionsPage.setTabOrder(self.force_standardize_multivalue_artist, self.standardize_instruments)
         MetadataOptionsPage.setTabOrder(self.standardize_instruments, self.standardize_vocals)
         MetadataOptionsPage.setTabOrder(self.standardize_vocals, self.convert_punctuation)
         MetadataOptionsPage.setTabOrder(self.convert_punctuation, self.release_ars)
