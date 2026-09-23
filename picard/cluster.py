metabrainz/picard

commit 6db3949d854c20116d4ed66882af69b616d0dc2e

Fix increasing memory use when creating FileList objects
A FileList instance holds references to files, but also registers to each
file's metadata_images_changed signal. This causes a reference to the
FileList instance and keeps it alive as long as any of the File instances
it hold is still alive. Furthermore it causes repeated calls to the
update_metadata_images slot even if the FileList instance is not actively
used anymore.

This badly impacts the temporary FileList instances created for the cover
art box during selection updates, but also Cluster instances that stay
in memory even after they have been removed if their files are still used.

Add an explicit clear() method that must be called once the instance is no
longer needed.

picard/cluster.py
@@ -55,6 +55,7 @@ from picard.i18n import (
 from picard.item import (
     FileListItem,
     Item,
+    ListOfMetadataItems,
 )
 from picard.matching import (
     SimMatchRelease,
@@ -105,6 +106,12 @@ class FileList(FileListItem):
                 file.metadata_images_changed.connect(self.update_metadata_images)
             self.update_metadata_images_from_children()
 
+    def clear(self):
+        """Remove all files and disconnect signals."""
+        for file in self.files:
+            file.metadata_images_changed.disconnect(self.update_metadata_images)
+        self.files = ListOfMetadataItems()
+
     def update(self, signal=True):
         pass
 
@@ -123,6 +130,10 @@ class Cluster(FileList):
         self.hide_if_empty = hide_if_empty
         self.album = related_album
         self._lookup_task = None
+
+    def clear(self):
+        super().clear()
+        self._clear_lookup_task()
 
     @property
     def album(self) -> 'Album | None':
@@ -468,7 +479,7 @@ class Cluster(FileList):
             limit=config.setting['query_limit'],
         )
 
-    def clear_lookup_task(self):
+    def _clear_lookup_task(self):
         if self._lookup_task:
             self.tagger.webservice.abort_task(self._lookup_task)
             self._lookup_task = None
@@ -507,6 +518,13 @@ class Cluster(FileList):
                 cluster_list[token].add(album, artist or various_artists, file)
 
         yield from cluster_list.values()
+
+
+class TempFileList(FileListItem):
+    """A temporary file list used for display purposes only.
+
+    If this becomes unused `clear` should be called to free up resources.
+    """
 
 
 class UnclusteredFiles(Cluster):

picard/tagger.py
@@ -1285,7 +1285,7 @@ class Tagger(QtWidgets.QApplication):
             log.debug("Removing %r", cluster)
             files = list(cluster.files)
             cluster.files = []
-            cluster.clear_lookup_task()
+            cluster.clear()
             self.remove_files(files, from_parent=False)
             self.clusters.remove(cluster)
             self.cluster_removed.emit(cluster)

picard/ui/coverartbox/__init__.py
@@ -51,7 +51,10 @@ from picard import (
     log,
     tagger_instance,
 )
-from picard.cluster import Cluster
+from picard.cluster import (
+    Cluster,
+    TempFileList,
+)
 from picard.config import get_config
 from picard.coverart.image import (
     CoverArtImage,
@@ -243,6 +246,8 @@ class CoverArtBox(QtWidgets.QGroupBox):
 
         if self.item is not item:
             self._exported_images = None
+            if isinstance(self.item, TempFileList):
+                self.item.clear()
         if self.item and hasattr(self.item, 'metadata_images_changed'):
             self.item.metadata_images_changed.disconnect(self.update_metadata)
         self.item = item

picard/ui/mainwindow/__init__.py
@@ -70,7 +70,7 @@ from picard.album import Album
 from picard.browser import addrelease
 from picard.cluster import (
     Cluster,
-    FileList,
+    TempFileList,
 )
 from picard.collection import load_user_collections
 from picard.config import (
@@ -1988,7 +1988,7 @@ class MainWindow(QtWidgets.QMainWindow, PreserveGeometry):
         elif coverart_visible and new_selection:
             # Create a temporary file list which allows changing cover art for all selected files
             files = list(iter_files_from_objects(objects))
-            obj = FileList(files)
+            obj = TempFileList(files)
 
         if coverart_visible and new_selection:
             self.cover_art_box.set_item(obj)

commit 7bc9af660433877f2c8fd85fc20b6d30f056a955

Fix cover art not shown for multiple selection
Fixes regression introduced in 6db3949d854c20116d4ed66882af69b616d0dc2e

picard/cluster.py
@@ -520,7 +520,7 @@ class Cluster(FileList):
         yield from cluster_list.values()
 
 
-class TempFileList(FileListItem):
+class TempFileList(FileList):
     """A temporary file list used for display purposes only.
 
     If this becomes unused `clear` should be called to free up resources.